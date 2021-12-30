f4f0b7ac88749529c97851f4b9295979e6199a6a
Quokkaaa <rktrkdml@gmail.com>
2021-12-30 18:08:57 +0900

    feat: Routines 열거형 및 연산프로퍼티 구현
    * String으로 반환하는 운동 루틴이름 구현

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -87,3 +87,26 @@ struct Routine {
     let name: String
     let exercises: [() -> Exercise]
 }
+
+enum Routines {
+    case uppperBodyExercise
+    case lowerBodyExercise
+    case backExercise
+    case chestExercise
+    case shoulderExercise
+    
+    var name: String {
+        switch self {
+        case .uppperBodyExercise:
+            return "상체운동"
+        case .lowerBodyExercise:
+            return "하체운동"
+        case .backExercise:
+            return "등운동"
+        case .chestExercise:
+            return "가슴운동"
+        case .shoulderExercise:
+            return "어깨운동"
+        }
+    }
+}
