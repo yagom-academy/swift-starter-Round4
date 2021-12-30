c272fc2d7f4d6f00582568cdb08ac3c2bca7c921
Quokkaaa <rktrkdml@gmail.com>
2021-12-30 17:54:55 +0900

    feat: BodyCondition 클래스 및 프로퍼티와 메서드 구현
    * uppperBodyStrength, lowerBodyStrength, muscularEndurance, fatigue 프로퍼티 구현
    * sitUp(), moveSquart(), runLong(), moveActiveRest(), pushUp()메서드 구현

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -1,12 +1,46 @@
-//
 //  main.swift
 //  CodeStarterCamp_Week4
-//
-//  Created by yagom.
+//  Created by quokka.
 //  Copyright © yagom academy. All rights reserved.
-//
 
 import Foundation
 
-print("Hello, World!")
-
+class BodyCondition {
+    var uppperBodyStrength: Int
+    var lowerBodyStrength: Int
+    var muscularEndurance: Int
+    var fatigue: Int
+    
+    init(uppperBodyStrength: Int, lowerBodyStrength: Int, muscularEndurance: Int, fatigue: Int) {
+        self.uppperBodyStrength = uppperBodyStrength
+        self.lowerBodyStrength = lowerBodyStrength
+        self.muscularEndurance = muscularEndurance
+        self.fatigue = fatigue
+    }
+    
+    func sitUp() {
+        self.uppperBodyStrength += Int.random(in: 10...20)
+        self.fatigue += Int.random(in: 10...20)
+    }
+    
+    func moveSquat() {
+        self.lowerBodyStrength += Int.random(in: 20...30)
+        self.fatigue += Int.random(in: 10...20)
+    }
+    
+    func runLong() {
+        self.muscularEndurance += Int.random(in: 20...30)
+        self.uppperBodyStrength += Int.random(in: 5...10)
+        self.lowerBodyStrength += Int.random(in: 5...10)
+        self.fatigue += Int.random(in: 20...30)
+    }
+    
+    func moveActiveRest() {
+        self.fatigue -= Int.random(in: 5...10)
+    }
+    
+    func pushUp() {
+        self.uppperBodyStrength += Int.random(in: 10...15)
+        self.fatigue += Int.random(in: 5...10)
+    }
+}
