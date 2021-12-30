e77fc5b49700d1e843c7ae917df0d09d8142d0d9
Quokkaaa <rktrkdml@gmail.com>
2021-12-30 18:06:14 +0900

    feat: Exercises 열거형 구현 및 프로퍼티 생성 및 리펙토링
    * 운동 종류와 String이름을 반환하는 연산프로퍼티 구현
    * BodyCondition내 메서드들 반환값 기입

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -18,36 +18,69 @@ class BodyCondition {
         self.fatigue = fatigue
     }
     
-    func sitUp() {
+    func sitUp() -> Exercise {
+        print(Exercises.sitUp.name)
         self.uppperBodyStrength += Int.random(in: 10...20)
         self.fatigue += Int.random(in: 10...20)
+        return Exercise(name: Exercises.sitUp.name, action: sitUp)
     }
     
-    func moveSquat() {
+    func moveSquat() -> Exercise {
+        print(Exercises.squart.name)
         self.lowerBodyStrength += Int.random(in: 20...30)
         self.fatigue += Int.random(in: 10...20)
+        return Exercise(name: Exercises.squart.name, action: moveSquat)
     }
     
-    func runLong() {
+    func runLong() -> Exercise {
+        print(Exercises.runLong.name)
         self.muscularEndurance += Int.random(in: 20...30)
         self.uppperBodyStrength += Int.random(in: 5...10)
         self.lowerBodyStrength += Int.random(in: 5...10)
         self.fatigue += Int.random(in: 20...30)
+        return Exercise(name: Exercises.runLong.name, action: runLong)
     }
     
-    func moveActiveRest() {
+    func moveActiveRest() -> Exercise {
+        print(Exercises.activeRest.name)
         self.fatigue -= Int.random(in: 5...10)
+        return Exercise(name: Exercises.activeRest.name, action: moveActiveRest)
     }
     
-    func pushUp() {
+    func pushUp() -> Exercise {
+        print(Exercises.pushUp.name)
         self.uppperBodyStrength += Int.random(in: 10...15)
         self.fatigue += Int.random(in: 5...10)
+        return Exercise(name: Exercises.pushUp.name, action: pushUp)
     }
 }
 
 struct Exercise {
     let name: String
     var action: () -> Self
+}
+
+enum Exercises {
+    case sitUp
+    case squart
+    case runLong
+    case pushUp
+    case activeRest
+    
+    var name: String {
+        switch self {
+        case .sitUp:
+            return "윗몸일으키기"
+        case .squart:
+            return "스쿼트"
+        case .runLong:
+            return "오래달리기"
+        case .pushUp:
+            return "푸쉬업"
+        case .activeRest:
+            return "동적휴식"
+        }
+    }
 }
 
 struct Routine {
