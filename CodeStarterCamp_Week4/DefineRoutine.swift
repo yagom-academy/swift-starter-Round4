2f7eba0dd23c5395bffffda78ba0ea79b1888856
Jungmo Yu <zzoo789@naver.com>
2021-12-31 21:55:26 +0900

    feat: Type정의: BodyCondition

---
+++ CodeStarterCamp_Week4/DefineRoutine.swift
@@ -0,0 +1,8 @@
+//
+//  DefineRoutine.swift
+//  CodeStarterCamp_Week4
+//
+//  Created by Jungmo Yu on 2021/12/31.
+//
+
+import Foundation
---
+++ CodeStarterCamp_Week4/Types/Routine.swift
@@ -0,0 +1,8 @@
+//
+//  Routine.swift
+//  CodeStarterCamp_Week4
+//
+//  Created by Jungmo Yu on 2021/12/31.
+//
+
+import Foundation
