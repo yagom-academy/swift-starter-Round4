7e70793d469097200f0a80a6373aeee82f7759bd
Jungmo Yu <zzoo789@naver.com>
2021-12-31 21:58:06 +0900

    feat: Define constant - Exercise & Routine

--- CodeStarterCamp_Week4/DefineRoutine.swift
+++ CodeStarterCamp_Week4/DefineRoutine.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+
+let hellRoutine: Routine = Routine(name: "hellRoutine", routine: [윗몸일으키기,
+                                                                 윗몸일으키기,
+                                                                 동적휴식,
+                                                                 스쿼트,
+                                                                 스쿼트,
+                                                                 동적휴식,
+                                                                 오래달리기,
+                                                                 오래달리기])
+
+
+let tabataRoutine: Routine = Routine(name: "타바타", routine: [윗몸일으키기,
+                                                           동적휴식,
+                                                           윗몸일으키기,
+                                                           동적휴식,
+                                                           스쿼트,
+                                                           동적휴식,
+                                                           스쿼트])
+
+let refresh: Routine = Routine(name: "리프레쉬", routine: [동적휴식,
+                                                      동적휴식,
+                                                      동적휴식,
+                                                      동적휴식])
