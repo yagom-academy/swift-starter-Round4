d3920ac21019b1c27906938498081b4699965e26
Jungmo Yu <zzoo789@naver.com>
2022-01-03 00:16:42 +0900

    refactor: Class로 타입 변경(참조타입) 및 필요 메서드 생성

--- CodeStarterCamp_Week4/Types/BodyCondition.swift
+++ CodeStarterCamp_Week4/Types/BodyCondition.swift
@@ -7,9 +7,17 @@
 
 import Foundation
 
-struct BodyCondition {
+class BodyCondition {
     var upperBodyStrength: UInt = 0
     var lowerBodyStrength: UInt = 0
     var muscularEndurance: UInt = 0
     var fatigue: Int = 0
+    
+    func printBodyCondition() {
+        print("상체근력: \(self.upperBodyStrength)")
+        print("하체근력: \(self.lowerBodyStrength)")
+        print("근지구력: \(self.muscularEndurance)")
+        print("피로도: \(self.fatigue)")
+    }
 }
+
