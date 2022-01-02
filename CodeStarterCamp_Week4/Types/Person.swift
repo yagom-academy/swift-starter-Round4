ef55b50bf23a953cc0eee2c4dc8c67fac8f7ef6a
Jungmo Yu <zzoo789@naver.com>
2022-01-03 00:17:28 +0900

    feat: Person 타입 생성(메인의 공용바디컨디션 공유)

--- CodeStarterCamp_Week4/Types/Person.swift
+++ CodeStarterCamp_Week4/Types/Person.swift
@@ -9,47 +9,22 @@ import Foundation
 
 class Person {
     var name: String
-    var bodyCondition: BodyCondition = BodyCondition()
+    var bodyCondition: BodyCondition = publicBodyCondition
     
-    init(name: String = "", bodyCondition: BodyCondition = BodyCondition()) {
+    init(name: String = "") {
         self.name = name
-        self.bodyCondition = bodyCondition
     }
     
-    func defineExercise() {
-        let 윗몸일으키기: Exercise = Exercise(name: "윗몸일으키기", action: {
-            self.bodyCondition.upperBodyStrength += UInt.random(in: 10...20)
-            self.bodyCondition.fatigue += Int.random(in: 10...20)
-        })
-        let 스쿼트: Exercise = Exercise(name: "스쿼트", action: {
-            self.bodyCondition.lowerBodyStrength += UInt.random(in: 20...30)
-            self.bodyCondition.fatigue += Int.random(in: 10...20)
-        })
-        let 오래달리기: Exercise = Exercise(name: "오래달리기", action: {
-            self.bodyCondition.muscularEndurance += UInt.random(in: 20...30)
-            self.bodyCondition.upperBodyStrength += UInt.random(in: 5...10)
-            self.bodyCondition.lowerBodyStrength += UInt.random(in: 5...10)
-            self.bodyCondition.fatigue += Int.random(in: 20...30)
-        })
-        let 동적휴식: Exercise = Exercise(name: "동적휴식", action: {
-            self.bodyCondition.fatigue -= Int.random(in: 10...20)
-
-            guard self.bodyCondition.fatigue >= 0 else {
-                self.bodyCondition.fatigue = 0
-                return
-            }
-        })
-    }
-    
-    func exercise(for set: Int, routine: Routine) {
+    func exercise(for set: Int, routine: Routine) throws {
         var routineCount = 0
         print("--------------")
-        print("\(routine.name)을 \(set)Set 시작합니다.")
+        print("\(routine.name) \(set)Set 시작합니다.")
         while routineCount < set {
             routineCount += 1
             routine.startRoutine()
         }
         print("--------------")
+        guard self.bodyCondition.fatigue < 100 else { throw FitnessError.HighFatigue }
     }
 }
 
