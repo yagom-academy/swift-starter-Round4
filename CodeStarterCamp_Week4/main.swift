a1e8550e1749acf30e0d613ec325b05a5a025fc6
JoSH0318 <jsh9870@naver.com>
2022-01-03 16:25:38 +0900

    Feat: add struct BodyCondition And Instance

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,5 +8,15 @@
 
 import Foundation
 
-print("Hello, World!")
+struct BodyCondition {
+    var upperBodyStrength = 0
+    var lowerBodyStrength = 0
+    var muscleEndurance = 0
+    var fatigue = 0
+}
+
+var personalBodyCondition: BodyCondition = .init(upperBodyStrength: 100,
+                                                 lowerBodyStrength: 100,
+                                                 muscleEndurance: 100,
+                                                 fatigue: 0)
 
