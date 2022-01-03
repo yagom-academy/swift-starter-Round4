468048406e22ee7cecf248427e89e59330c09314
Minseong-yagom <msb0987@naver.com>
2022-01-03 17:07:09 +0900

    Feat 인스턴스 생성
    BodyCondition 인스턴스 생성
    다양한 Exercise 인스턴스 생성

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -17,4 +17,26 @@ struct Routine {
     let exercises: [Exercise]
 }
 
+var Minseong = BodyCondition()
+
+let sitUp: Exercise = Exercise(name: "윗몸일으키기") {
+    Minseong.upperBodyStrength += Int.random(in: 10...20)
+    Minseong.muscleFatigue += Int.random(in: 10...20)
+}
+
+let squat: Exercise = Exercise(name: "스쿼트") {
+    Minseong.lowerBodyStrength += Int.random(in: 20...30)
+    Minseong.muscleFatigue += Int.random(in: 10...20)
+}
+
+let longDistanceRunning: Exercise = Exercise(name: "오래달리기") {
+    Minseong.muscularEndurance += Int.random(in: 20...30)
+    Minseong.upperBodyStrength += Int.random(in: 5...10)
+    Minseong.lowerBodyStrength += Int.random(in: 5...10)
+    Minseong.muscleFatigue += Int.random(in: 20...30)
+}
+
+let activeRest: Exercise = Exercise(name: "동적휴식") {
+    Minseong.muscleFatigue -= Int.random(in: 5...10)
+}
 
