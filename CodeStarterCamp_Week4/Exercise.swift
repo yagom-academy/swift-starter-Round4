971811b31616f0b8f7b3342867033e84f48655a6
marisol <marisol.develop@gmail.com>
2022-01-07 19:32:52 +0900

    refactor : Exercise 내부에 열거형 선언, BodyCondition 타입 변경

--- CodeStarterCamp_Week4/Exercise.swift
+++ CodeStarterCamp_Week4/Exercise.swift
@@ -10,38 +10,36 @@ import Foundation
 struct Exercise {
     let name: String
     let action: () -> Void
+    
+    enum NumberOfRange {
+        static let range1 = 5...10
+        static let range2 = 10...20
+        static let range3 = 20...30
+    }
 }
 
 let 윗몸일으키기: Exercise = Exercise(name: "윗몸일으키기") {
-    var randomNumber1: Int = Int.random(in: 10...20)
-    var randomNumber2: Int = Int.random(in: 10...20)
-    bodyCondition.상체근력 += randomNumber1
-    bodyCondition.피로도 += randomNumber2
+    bodyCondition.상체근력 += Int.random(in: Exercise.NumberOfRange.range1)
+    bodyCondition.피로도 += Int.random(in: Exercise.NumberOfRange.range2)
 }
 
 let 스쿼트: Exercise = Exercise(name: "스쿼트") {
-    var randomNumber1: Int = Int.random(in: 20...30)
-    var randomNumber2: Int = Int.random(in: 10...20)
-    bodyCondition.하체근력 += randomNumber1
-    bodyCondition.피로도 += randomNumber2
+    bodyCondition.하체근력 += Int.random(in: Exercise.NumberOfRange.range3)
+    bodyCondition.피로도 += Int.random(in: Exercise.NumberOfRange.range2)
 }
 
 let 오래달리기: Exercise = Exercise(name: "오래달리기") {
-    var randomNumber1: Int = Int.random(in: 20...30)
-    var randomNumber2: Int = Int.random(in: 5...10)
-    bodyCondition.근지구력 += randomNumber1
-    bodyCondition.상체근력 += randomNumber2
-    bodyCondition.하체근력 += randomNumber2
-    bodyCondition.피로도 += randomNumber1
+    bodyCondition.근지구력 += Int.random(in: Exercise.NumberOfRange.range3)
+    bodyCondition.상체근력 += Int.random(in: Exercise.NumberOfRange.range1)
+    bodyCondition.하체근력 += Int.random(in: Exercise.NumberOfRange.range1)
+    bodyCondition.피로도 += Int.random(in: Exercise.NumberOfRange.range3)
 }
 
 let 동적휴식: Exercise = Exercise(name: "동적휴식") {
-    var randomNumber: Int = Int.random(in: 5...10)
-    bodyCondition.피로도 -= randomNumber
+    bodyCondition.피로도 -= Int.random(in: Exercise.NumberOfRange.range1)
 }
 
 let 철봉매달리기: Exercise = Exercise(name: "철봉매달리기") {
-    var randomNumber: Int = Int.random(in: 20...30)
-    bodyCondition.근지구력 += randomNumber
-    bodyCondition.피로도 += randomNumber
+    bodyCondition.근지구력 += Int.random(in: Exercise.NumberOfRange.range3)
+    bodyCondition.피로도 += Int.random(in: Exercise.NumberOfRange.range1)
 }
