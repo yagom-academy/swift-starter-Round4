b5f0a1dcbb211decab906ea03c39f767084875eb
nayeon <vennie20@naver.com>
2022-01-07 11:07:37 +0900

    feat: add routine instance, run!

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -1,12 +1,2 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week4
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
-
-import Foundation
-
-print("Hello, World!")
-
+let routine = Routine(name: "vennie's Routine", person: vennie, routines: [스쿼트, 윗몸일으키기, 스쿼트, 오래달리기, 동적휴식, 스쿼트, 윗몸일으키기, 오래달리기, 동적휴식])
+routine.start()
--- CodeStarterCamp_Week4/week4.swift
+++ CodeStarterCamp_Week4/week4.swift
@@ -77,4 +77,3 @@ class Routine {
         person.printCondition()
     }
 }
-
