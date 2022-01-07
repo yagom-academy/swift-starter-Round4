e75579bdc36d59c67d28adf0a92bd23cede6782f
nayeon <vennie20@naver.com>
2022-01-07 11:05:31 +0900

    feat: add instance, exercises closures

--- CodeStarterCamp_Week4/week4.swift
+++ CodeStarterCamp_Week4/week4.swift
@@ -19,3 +19,38 @@ class BodyCondition {
         print("피로도 : \(fatigue)")
     }
 }
+
+var vennie = BodyCondition()
+
+struct Exercise {
+    let name: String
+    let action: () -> Void
+}
+
+func randomIntBetween(_ a: Int, _ b: Int) -> Int {
+    return Int.random(in: a...b)
+}
+
+let 윗몸일으키기 = Exercise(name: "윗몸일으키기", action: {
+    vennie.upperBodyStrength += randomIntBetween(10, 20)
+    vennie.fatigue += randomIntBetween(10, 20)
+})
+
+let 스쿼트 = Exercise(name: "스쿼트", action: {
+    vennie.lowerBodyStrength += randomIntBetween(20, 30)
+    vennie.fatigue += randomIntBetween(10, 20)
+})
+
+let 오래달리기 = Exercise(name: "오래달리기", action: {
+    vennie.upperBodyStrength += randomIntBetween(5, 10)
+    vennie.lowerBodyStrength += randomIntBetween(5, 10)
+    vennie.muscularEndurance += randomIntBetween(20, 30)
+    vennie.fatigue += randomIntBetween(20, 30)
+})
+
+let 동적휴식 = Exercise(name: "동적휴식", action: {
+    vennie.fatigue -= randomIntBetween(5, 10)
+    if vennie.fatigue < 0 {
+        vennie.fatigue = 0
+    }
+})
