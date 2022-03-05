aaf9b0cd9853d4ba0b6e6665fbd81dffea424f1a
doroomook <ts4683@gmail.com>
2022-03-05 20:32:28 +0900

    class Routine 추가

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -48,4 +48,49 @@ let 동적휴식: Exercise = Exercise(name: "동적휴식", action: {person in
     person.피로도 -= Int.random(in: 5...10)
 })
 
-var personA: BodyCondition = BodyCondition(상체근력: 0, 하체근력: 0, 근지구력: 0, 피로도: 0)
+class Routine {
+    let name: String
+    let exercises: [Exercise]
+    
+    init(name: String, exercises: [Exercise]) {
+        self.name = name
+        self.exercises = exercises
+    }
+    
+    func start(_ person: BodyCondition) {         //클래스를 쓸것인가 , 스트럭트와 뮤테이팅 함수를 쓸것인가?
+        print("""
+              --------------
+              \(name)을 시작합니다.
+              """)
+        
+        var exercises = exercises
+        
+        while exercises.count > 0 {
+            if let nameOfExercise = exercises.first?.name {
+                print(nameOfExercise)
+            }
+            exercises.first?.action(person)
+            exercises.removeFirst()
+        }
+        
+        printPresentCondition(person)
+    }
+}
+
+func printPresentCondition(_ person: BodyCondition) {
+    print("""
+          --------------
+          현재의 컨디션은 다음과 같습니다.
+          상체근력: \(person.상체근력)
+          하체근력: \(person.하체근력)
+          근지구력: \(person.근지구력)
+          피로도: \(person.피로도)
+          """)
+}
+
+let misterLee: BodyCondition = BodyCondition(상체근력: 0, 하체근력: 0, 근지구력: 0, 피로도: 0)
+let missKim: BodyCondition = BodyCondition(상체근력: 0, 하체근력: 0, 근지구력: 0, 피로도: 0)
+let hellRoutine: Routine = Routine(name: "hellRoutine", exercises: [윗몸일으키기, 윗몸일으키기, 동적휴식, 스쿼트, 스쿼트, 동적휴식, 오래달리기, 오래달리기])
+
+hellRoutine.start(misterLee)
+hellRoutine.start(missKim)
