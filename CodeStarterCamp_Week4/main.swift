fde9d112bbcd20293f80cdc97ee8dd98e5e306fe
cpp2swift <dkseotn123@gmail.com>
2022-03-06 17:31:45 +0900

    실제로 동작하는 실행 코드 추가
    루틴시작과 현재 몸상태를 체크 후 알려주는 코드 추가

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -79,3 +79,6 @@ struct Routine {
 }
 
 var hellRoutine = Routine(routineName: "missKim's Hell Routine", exercises: [윗몸일으키기, 윗몸일으키기, 동적휴식, 스쿼트, 스쿼트, 동적휴식, 오래달리기, 오래달리기])
+
+hellRoutine.routineStart()
+myBody.checkCurrentCondition()
