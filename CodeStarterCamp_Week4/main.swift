22f2a55a9513e85ec44448df09153e89c719bfd7
cpp2swift <dkseotn123@gmail.com>
2022-03-06 17:26:03 +0900

    클로저를 이용해 상수에 운동 종류들을 생성

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -42,3 +42,24 @@ struct Exercise {
 }
 
 var myBody = Bodycondition(upperBS: 50, lowerBS: 50, muscular: 50, fatigue: 0)
+
+let 윗몸일으키기: Exercise = Exercise(name: "윗몸일으키기", action: {
+    myBody.upperBodyStrength += randomNum(10,20)
+    myBody.fatigue += randomNum(10,20)
+})
+
+let 스쿼트: Exercise = Exercise(name: "스쿼트", action: {
+    myBody.lowerBodyStrength += randomNum(20,30)
+    myBody.fatigue += randomNum(10,20)
+})
+
+let 오래달리기: Exercise = Exercise(name: "오래달리기", action: {
+    myBody.muscularEndurance += randomNum(20,30)
+    myBody.upperBodyStrength += randomNum(5,10)
+    myBody.lowerBodyStrength += randomNum(5,10)
+    myBody.fatigue += randomNum(20,30)
+})
+
+let 동적휴식: Exercise = Exercise(name: "동적휴식", action: {
+    myBody.fatigue -= randomNum(5,10)
+})
