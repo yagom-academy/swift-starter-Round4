2fe9dd52ed165b25c4f645f0b2ca3b1c7c0b2be6
ohkyeongsik <unchain123@kakao.com>
2022-03-08 00:58:57 +0900

    feat: BodyCondition type

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,5 +8,20 @@
 
 import Foundation
 
-print("Hello, World!")
-
+struct BodyCondition {
+    var upperBodyStrength: Int
+    var lowerBodyStrength: Int
+    var muscularEndurance: Int
+    var fatigAbility: Int
+    
+    func presentCondition() {
+        print("""
+            --------------
+            현재의 컨디션은 다음과 같습니다.
+            상체근력: \(upperBodyStrength)
+            하체근력: \(lowerBodyStrength)
+            근지구력: \(muscularEndurance)
+            피로도: \(fatigAbility)
+            """)
+    }
+}
