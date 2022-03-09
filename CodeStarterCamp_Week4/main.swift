3fe59fe0e2078c37616064142611f8d6877fc26f
yeeton37 <lyeeun37@naver.com>
2022-03-09 16:01:05 +0900

    feat: BodyCondition 타입 생성

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -1,12 +1,27 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week4
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+class BodyCondition {
+    var upperBodyStrength:Int = 0
+    var lowerBodyStrength:Int = 0
+    var muscularEndurance:Int = 0
+    var fatigue:Int = 0
+
+    func sitUp() {
+        self.upperBodyStrength += Int.random(in: 10...20)
+        self.fatigue += Int.random(in: 10...20)
+    }
+    func doSquat() {
+        self.lowerBodyStrength += Int.random(in: 20...30)
+        self.fatigue += Int.random(in: 10...20)
+    }
+    func runLongtime() {
+        self.muscularEndurance += Int.random(in: 20...30)
+        self.upperBodyStrength += Int.random(in: 5...10)
+        self.lowerBodyStrength += Int.random(in: 5...10)
+        self.fatigue += Int.random(in: 20...30)
+    }
+    func doActiveRest() {
+        self.fatigue -= Int.random(in: 5...10)
+    }
+}
 
