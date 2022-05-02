b77e15980c48bf5f0dbe22633e88026677cc6c37
yuvinrho <yvrho@naver.com>
2022-05-02 17:01:09 +0900

    BodyCondition 구조체 정의

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -1,5 +1,12 @@
 import Foundation
 
+struct BodyCondition {
+    var 상체근력: Int = 0
+    var 하체근력: Int = 0
+    var 근지구력: Int = 0
+    var 피로도: Int = 0
+}
+
 struct Exercise {
         let name: String
         let action: () -> Void
