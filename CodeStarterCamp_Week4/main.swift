ed74afa55fb9c8107731b573b35104a45c2cfecd
yuvinrho <yvrho@naver.com>
2022-05-02 18:32:10 +0900

    운동구현

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -11,3 +11,23 @@ struct Exercise {
     let name: String
     let action: (inout BodyCondition) -> Void
 }
+
+let 윗몸일으키기: Exercise = Exercise(name: "윗몸일으키기") {
+    $0.상체근력 += Int.random(in: 10...20)
+    $0.피로도 += Int.random(in: 10...20)
+}
+
+let 스쿼드: Exercise = Exercise(name: "스쿼드") {
+    $0.하체근력 += Int.random(in: 20...30)
+    $0.피로도 += Int.random(in: 10...20)
+}
+
+let 오래달리기: Exercise = Exercise(name: "오래달리기") {
+    $0.근지구력 += Int.random(in: 20...30)
+    $0.상체근력 += Int.random(in: 5...10)
+    $0.하체근력 += Int.random(in: 5...10)
+    $0.피로도 += Int.random(in: 10...20)
+}
+let 동적휴식: Exercise = Exercise(name: "동적휴식") {
+    $0.피로도 -= Int.random(in: 5...10)
+}
