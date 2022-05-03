b8e83a3caa28924bd2930d0e968e1cfafac239b9
Onestar <onestar7984@gmail.com>
2022-05-04 01:24:34 +0900

    refactor: 생성된 Exercise의 클로저 내부의 BodyCondition변수 네이밍 수정.

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -91,61 +91,61 @@ struct Routine {
 }
 
 let pushUp = Exercise(name: "팔굽혀펴기") { (bodyCondition: BodyCondition) in
-    var temporaryBodyCondition = bodyCondition
-    temporaryBodyCondition.upperBodyStrength += Int.random(in: 5...15)
-    temporaryBodyCondition.tiredness += Int.random(in: 5...10)
-    return temporaryBodyCondition
+    var exerciserBodyCondition = bodyCondition
+    exerciserBodyCondition.upperBodyStrength += Int.random(in: 5...15)
+    exerciserBodyCondition.tiredness += Int.random(in: 5...10)
+    return exerciserBodyCondition
 }
 
 let chinUp = Exercise(name: "턱걸이") { (bodyCondition: BodyCondition) in
-    var temporaryBodyCondition = bodyCondition
-    temporaryBodyCondition.upperBodyStrength += Int.random(in: 15...20)
-    temporaryBodyCondition.tiredness += Int.random(in: 10...15)
-    return temporaryBodyCondition
+    var exerciserBodyCondition = bodyCondition
+    exerciserBodyCondition.upperBodyStrength += Int.random(in: 15...20)
+    exerciserBodyCondition.tiredness += Int.random(in: 10...15)
+    return exerciserBodyCondition
 }
 
 let dip = Exercise(name: "딥스") { (bodyCondition: BodyCondition) in
-    var temporaryBodyCondition = bodyCondition
-    temporaryBodyCondition.upperBodyStrength += Int.random(in: 8...14)
-    temporaryBodyCondition.tiredness += Int.random(in: 5...10)
-    return temporaryBodyCondition
+    var exerciserBodyCondition = bodyCondition
+    exerciserBodyCondition.upperBodyStrength += Int.random(in: 8...14)
+    exerciserBodyCondition.tiredness += Int.random(in: 5...10)
+    return exerciserBodyCondition
 }
 
 let sitUp = Exercise(name: "윗몸일으키기") { (bodyCondition: BodyCondition) in
-    var temporaryBodyCondition = bodyCondition
-    temporaryBodyCondition.upperBodyStrength += Int.random(in: 10...20)
-    temporaryBodyCondition.tiredness += Int.random(in: 10...20)
-    return temporaryBodyCondition
+    var exerciserBodyCondition = bodyCondition
+    exerciserBodyCondition.upperBodyStrength += Int.random(in: 10...20)
+    exerciserBodyCondition.tiredness += Int.random(in: 10...20)
+    return exerciserBodyCondition
 }
 
 let squatting = Exercise(name: "스쿼트") { (bodyCondition: BodyCondition) in
-    var temporaryBodyCondition = bodyCondition
-    temporaryBodyCondition.lowerBodyStrength += Int.random(in: 20...30)
-    temporaryBodyCondition.tiredness += Int.random(in: 10...20)
-    return temporaryBodyCondition
+    var exerciserBodyCondition = bodyCondition
+    exerciserBodyCondition.lowerBodyStrength += Int.random(in: 20...30)
+    exerciserBodyCondition.tiredness += Int.random(in: 10...20)
+    return exerciserBodyCondition
 }
 
 let lunge = Exercise(name: "런지") { (bodyCondition: BodyCondition) in
-    var temporaryBodyCondition = bodyCondition
-    temporaryBodyCondition.lowerBodyStrength += Int.random(in: 15...20)
-    temporaryBodyCondition.tiredness += Int.random(in: 7...10)
-    return temporaryBodyCondition
+    var exerciserBodyCondition = bodyCondition
+    exerciserBodyCondition.lowerBodyStrength += Int.random(in: 15...20)
+    exerciserBodyCondition.tiredness += Int.random(in: 7...10)
+    return exerciserBodyCondition
 }
 
 let longRun = Exercise(name: "오래달리기") { (bodyCondition: BodyCondition) in
-    var temporaryBodyCondition = bodyCondition
-    temporaryBodyCondition.muscleEndurance += Int.random(in: 10...20)
-    temporaryBodyCondition.upperBodyStrength += Int.random(in: 5...10)
-    temporaryBodyCondition.lowerBodyStrength += Int.random(in: 5...10)
-    temporaryBodyCondition.tiredness += Int.random(in: 10...20)
-    return temporaryBodyCondition
+    var exerciserBodyCondition = bodyCondition
+    exerciserBodyCondition.muscleEndurance += Int.random(in: 10...20)
+    exerciserBodyCondition.upperBodyStrength += Int.random(in: 5...10)
+    exerciserBodyCondition.lowerBodyStrength += Int.random(in: 5...10)
+    exerciserBodyCondition.tiredness += Int.random(in: 10...20)
+    return exerciserBodyCondition
 }
 
 let activeRest = Exercise(name: "동적휴식") { (bodyCondition: BodyCondition) in
-    var temporaryBodyCondition = bodyCondition
-    temporaryBodyCondition.tiredness -= Int.random(in: 5...10)
-    if temporaryBodyCondition.tiredness < 0 {
-        temporaryBodyCondition.tiredness = 0
+    var exerciserBodyCondition = bodyCondition
+    exerciserBodyCondition.tiredness -= Int.random(in: 5...10)
+    if exerciserBodyCondition.tiredness < 0 {
+        exerciserBodyCondition.tiredness = 0
     }
-    return temporaryBodyCondition
+    return exerciserBodyCondition
 }
