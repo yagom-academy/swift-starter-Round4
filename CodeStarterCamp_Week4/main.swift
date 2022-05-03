bcddecd2a1113bae0191fb10c3fad9293daa30be
Onestar <onestar7984@gmail.com>
2022-05-04 00:31:25 +0900

    feat: Exercise타입 정의 프로퍼티로 이름과 액션을 가짐 액션은 클로저이다. 생성되는 운동에 따라서 액션의 클로저 내용을 정의한 후 인스턴스를 생성하였다.

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -25,3 +25,68 @@ struct BodyCondition {
         self.muscleEndurance = muscleEndurance
     }
 }
+
+struct Exercise {
+    let name: String
+    let action: (BodyCondition) -> BodyCondition
+}
+
+let pushUp = Exercise(name: "팔굽혀펴기") { (bodyCondition: BodyCondition) in
+    var temporaryBodyCondition = bodyCondition
+    temporaryBodyCondition.upperBodyStrength += Int.random(in: 5...15)
+    temporaryBodyCondition.tiredness += Int.random(in: 5...10)
+    return temporaryBodyCondition
+}
+
+let chinUp = Exercise(name: "턱걸이") { (bodyCondition: BodyCondition) in
+    var temporaryBodyCondition = bodyCondition
+    temporaryBodyCondition.upperBodyStrength += Int.random(in: 15...20)
+    temporaryBodyCondition.tiredness += Int.random(in: 10...15)
+    return temporaryBodyCondition
+}
+
+let dip = Exercise(name: "딥스") { (bodyCondition: BodyCondition) in
+    var temporaryBodyCondition = bodyCondition
+    temporaryBodyCondition.upperBodyStrength += Int.random(in: 8...14)
+    temporaryBodyCondition.tiredness += Int.random(in: 5...10)
+    return temporaryBodyCondition
+}
+
+let sitUp = Exercise(name: "윗몸일으키기") { (bodyCondition: BodyCondition) in
+    var temporaryBodyCondition = bodyCondition
+    temporaryBodyCondition.upperBodyStrength += Int.random(in: 10...20)
+    temporaryBodyCondition.tiredness += Int.random(in: 10...20)
+    return temporaryBodyCondition
+}
+
+let squatting = Exercise(name: "스쿼트") { (bodyCondition: BodyCondition) in
+    var temporaryBodyCondition = bodyCondition
+    temporaryBodyCondition.lowerBodyStrength += Int.random(in: 20...30)
+    temporaryBodyCondition.tiredness += Int.random(in: 10...20)
+    return temporaryBodyCondition
+}
+
+let lunge = Exercise(name: "런지") { (bodyCondition: BodyCondition) in
+    var temporaryBodyCondition = bodyCondition
+    temporaryBodyCondition.lowerBodyStrength += Int.random(in: 15...20)
+    temporaryBodyCondition.tiredness += Int.random(in: 7...10)
+    return temporaryBodyCondition
+}
+
+let longRun = Exercise(name: "오래달리기") { (bodyCondition: BodyCondition) in
+    var temporaryBodyCondition = bodyCondition
+    temporaryBodyCondition.muscleEndurance += Int.random(in: 10...20)
+    temporaryBodyCondition.upperBodyStrength += Int.random(in: 5...10)
+    temporaryBodyCondition.lowerBodyStrength += Int.random(in: 5...10)
+    temporaryBodyCondition.tiredness += Int.random(in: 10...20)
+    return temporaryBodyCondition
+}
+
+let activeRest = Exercise(name: "동적휴식") { (bodyCondition: BodyCondition) in
+    var temporaryBodyCondition = bodyCondition
+    temporaryBodyCondition.tiredness -= Int.random(in: 5...10)
+    if temporaryBodyCondition.tiredness < 0 {
+        temporaryBodyCondition.tiredness = 0
+    }
+    return temporaryBodyCondition
+}
