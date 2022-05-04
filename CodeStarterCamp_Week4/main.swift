1c4ca5ad476ce143f311e5455956938a2fef30b1
jeongmin <lj845854@gmail.com>
2022-05-04 12:30:45 +0900

    feat : 운동을 실행하는 메서드와 컨디션을 출력하는 메서드 생성

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -12,16 +12,25 @@ let exercises: [Exercise] = [윗몸일으키기, 스쿼트, 동적휴식, 스쿼
 let hellRoutine: Routine = Routine(called: "HellRoutine", exercises: exercises)
 let misterLee: Person = Person(name: "이정민", routine: hellRoutine)
 
+func workOut(with person: Person) {
+    for exercise in person.routine.exercises {
+        print(exercise.name)
+        exercise.action(&person.condition)
+    }
+
+}
+
+func printCondition(of person: Person) {
+    print("현재의 컨디션은 다음과 같습니다.")
+    print("상체근력: \(person.condition.upperBodyStrength)")
+    print("하체근력: \(person.condition.lowerBodyStrength)")
+    print("근지구력: \(person.condition.endurancePower)")
+    print("피로도: \(person.condition.fatigue)")
+}
+
 print("--------------")
 print("\(misterLee.routine.routineName)을 시작합니다.")
-for exercise in misterLee.routine.exercises {
-    print(exercise.name)
-    exercise.action(&misterLee.condition)
-}
+workOut(with: misterLee)
 print("--------------")
-print("현재의 컨디션은 다음과 같습니다.")
-print("상체근력: \(misterLee.condition.upperBodyStrength)")
-print("하체근력: \(misterLee.condition.lowerBodyStrength)")
-print("근지구력: \(misterLee.condition.endurancePower)")
-print("피로도: \(misterLee.condition.fatigue)")
+printCondition(of: misterLee)
 
