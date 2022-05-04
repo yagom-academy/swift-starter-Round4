1df43bf4286518daeda811e28c423fc51ed7e4c2
jeongmin <lj845854@gmail.com>
2022-05-05 02:11:38 +0900

    style : main의 workOut과 showCondition을 Person타입의 메서드로 변경

--- CodeStarterCamp_Week4/Person.swift
+++ CodeStarterCamp_Week4/Person.swift
@@ -17,4 +17,27 @@ class Person {
         self.routine = routine
         self.condition = BodyCondition()
     }
+    
+    func workOut() {
+        print("""
+        --------------
+        \(self.routine.routineName)을 시작합니다.
+        """)
+        
+        for exercise in routine.exercises {
+            print(exercise.name)
+            exercise.action(&condition)
+        }
+    }
+
+    func showCondition() {
+        print("""
+        --------------
+        현재의 컨디션은 다음과 같습니다.
+        상체근력: \(self.condition.upperBodyStrength)
+        하체근력: \(self.condition.lowerBodyStrength)
+        근지구력: \(self.condition.endurancePower)
+        피로도: \(self.condition.fatigue)
+        """)
+    }
 }
--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -12,25 +12,5 @@ let exercises: [Exercise] = [sitUp, squat, activeRest, squat, benchPress, active
 let hellRoutine: Routine = Routine(called: "HellRoutine", exercises: exercises)
 let misterLee: Person = Person(name: "이정민", routine: hellRoutine)
 
-func workOut(with person: Person) {
-    for exercise in person.routine.exercises {
-        print(exercise.name)
-        exercise.action(&person.condition)
-    }
-
-}
-
-func printCondition(of person: Person) {
-    print("현재의 컨디션은 다음과 같습니다.")
-    print("상체근력: \(person.condition.upperBodyStrength)")
-    print("하체근력: \(person.condition.lowerBodyStrength)")
-    print("근지구력: \(person.condition.endurancePower)")
-    print("피로도: \(person.condition.fatigue)")
-}
-
-print("--------------")
-print("\(misterLee.routine.routineName)을 시작합니다.")
-workOut(with: misterLee)
-print("--------------")
-printCondition(of: misterLee)
-
+misterLee.workOut()
+misterLee.showCondition()
