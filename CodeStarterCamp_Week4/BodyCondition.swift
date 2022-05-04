ffbe06661ef82842bde098d8285966595a665de4
jeongmin <lj845854@gmail.com>
2022-05-04 12:05:38 +0900

    add : BodyCondition 타입 생성

---
+++ CodeStarterCamp_Week4/BodyCondition.swift
@@ -0,0 +1,15 @@
+//
+//  BodyCondition.swift
+//  CodeStarterCamp_Week4
+//
+//  Created by 이정민 on 2022/05/04.
+//
+
+import Foundation
+
+struct BodyCondition {
+    var upperBodyStrength: Int = 0
+    var lowerBodyStrength: Int = 0
+    var endurancePower: Int = 0
+    var fatigue: Int = 0
+}
