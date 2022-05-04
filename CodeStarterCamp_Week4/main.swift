2f53291e29b21419e573719dc5425e445a2adf54
jeongmin <lj845854@gmail.com>
2022-05-04 12:07:51 +0900

    add : 실행 시나리오 추가

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,5 +8,20 @@
 
 import Foundation
 
-print("Hello, World!")
+let exercises: [Exercise] = [윗몸일으키기, 스쿼트, 동적휴식, 스쿼트, 벤치프레스, 동적휴식, 오래달리기, 오래달리기]
+let hellRoutine: Routine = Routine(called: "HellRoutine", exercises: exercises)
+let misterLee: Person = Person(name: "이정민",routine: hellRoutine)
+
+print("--------------")
+print("\(misterLee.routine.routineName)을 시작합니다.")
+for exercise in misterLee.routine.exercises {
+    print(exercise.name)
+    exercise.action(&misterLee.condition)
+}
+print("--------------")
+print("현재의 컨디션은 다음과 같습니다.")
+print("상체근력: \(misterLee.condition.upperBodyStrength)")
+print("하체근력: \(misterLee.condition.lowerBodyStrength)")
+print("근지구력: \(misterLee.condition.endurancePower)")
+print("피로도: \(misterLee.condition.fatigue)")
 
