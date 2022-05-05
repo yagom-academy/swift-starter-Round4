eae082ea63f407ddf4265a97c946187b8010d071
jeongmin <lj845854@gmail.com>
2022-05-06 07:50:55 +0900

    feat : register, enterName, setTargetCondition 메서드 기능 추가
    FitnessCenter에 등록을 할 수 있게하는 register메서드와
    등록할 때 필요한 이름, 목표 바디컨디션을 입력 받는 메서드 추가

--- CodeStarterCamp_Week4/FitnessCenter.swift
+++ CodeStarterCamp_Week4/FitnessCenter.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class FitnessCenter {
-    let targetBodyCondition: BodyCondition? = nil
+    var targetBodyCondition: BodyCondition? = nil
     var member: Person? = nil
     var routineList: [Routine]
     
@@ -16,20 +16,39 @@ class FitnessCenter {
         self.routineList = routineList
     }
     
-    func open() {
-        enroll()
-        setTargetCondition()
+    func register() {
+        do {
+            try enterName()
+            try setTargetCondition()
+        } catch FitnessCenterContingency.unexpectedInputValue {
+            print("인식할 수 없는 값입니다")
+        } catch {
+            print(error)
+        }
     }
     
-    func enroll() {
+    private func enterName() throws {
+        print("안녕하세요. 야곰 피트니스 센터입니다. 회원님의 이름은 무엇인가요?")
         
+        guard let memberName = readLine() else {
+            throw FitnessCenterContingency.unexpectedInputValue
+        }
+        
+        self.member = Person(name: memberName)
     }
     
-    func setTargetCondition() {
+    private func setTargetCondition() throws {
+        print("운동 목표치를 순서대로 알려주세요. 예시) 130 120 150 (상체근력:130,하체근력:120,근지구력:150)")
         
-    }
-    
-    func selectRoutine() {
+        guard let inputValue = readLine() else {
+            throw FitnessCenterContingency.unexpectedInputValue
+        }
         
+        let receivedTargetCondition = inputValue.split(separator: " ").map({ Int($0) })
+        if let upperCondition = receivedTargetCondition[0], let lowerCondition = receivedTargetCondition[1], let enduranceCondition = receivedTargetCondition[2] {
+            targetBodyCondition?.upperBodyStrength = upperCondition
+            targetBodyCondition?.lowerBodyStrength = lowerCondition
+            targetBodyCondition?.endurancePower = enduranceCondition
+        }
     }
 }
