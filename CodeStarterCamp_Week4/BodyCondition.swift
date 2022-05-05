2ba253e138cf3b416320003430d8d00ea0ccb780
JAY-Winter <rubat0@naver.com>
2022-05-05 20:33:44 +0900

    feat: BodyCondition 모델 구현

---
+++ CodeStarterCamp_Week4/BodyCondition.swift
@@ -0,0 +1,15 @@
+//
+//  BodyCondition.swift
+//  CodeStarterCamp_Week4
+//
+//  Created by JAEHYEON on 2022/05/05.
+//
+
+import Foundation
+
+struct BodyCondition {
+    var 상체근력: Int = 0
+    var 하체근력: Int = 0
+    var 근지구력: Int = 0
+    var 피로도: Int = 0
+}
---
+++ CodeStarterCamp_Week4/Exercise.swift
@@ -0,0 +1,8 @@
+//
+//  Exercise.swift
+//  CodeStarterCamp_Week4
+//
+//  Created by JAEHYEON on 2022/05/05.
+//
+
+import Foundation
