51a144f7eac37f372860c1e0f7f437bb1860b019
jeongmin <lj845854@gmail.com>
2022-05-06 06:46:01 +0900

    add : FitnessCenterContigency enum 정의
    Error 값을 가짐

---
+++ CodeStarterCamp_Week4/FitnessCenterContingency.swift
@@ -0,0 +1,12 @@
+//
+//  FitnessCenterContingency.swift
+//  CodeStarterCamp_Week4
+//
+//  Created by 이정민 on 2022/05/06.
+//
+
+import Foundation
+
+enum FitnessCenterContingency: Error {
+    case unexpectedInputValue
+}
