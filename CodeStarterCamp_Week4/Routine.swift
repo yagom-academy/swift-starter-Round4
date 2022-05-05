3792ee3762a05ca06caf9d297f2099b7aaf65d8b
JAY-Winter <rubat0@naver.com>
2022-05-05 20:34:21 +0900

    feat: Routine 모델 구현

--- CodeStarterCamp_Week4/Routine.swift
+++ CodeStarterCamp_Week4/Routine.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct Routine {
+    var name: String
+    var exercises: [Exercise] = []
+    
+    func startRoutine(_ exercises: [Exercise]) {
+        print("""
+    --------------
+    \(self.name)을 시작합니다.
+    """)
+        
+        for exercise in exercises {
+            print(exercise.name)
+            exercise.action()
+        }
+        printBodyCondition(myBodyCondition)
+    }
+    
+    func printBodyCondition(_ body: BodyCondition) {
+        print("""
+    --------------
+    현재의 컨디션은 다음과 같습니다.
+    상체근력: \(body.상체근력)
+    하체근력: \(body.하체근력)
+    근지구력: \(body.근지구력)
+    피로도: \(body.피로도)
+    """)
+    }
+}
