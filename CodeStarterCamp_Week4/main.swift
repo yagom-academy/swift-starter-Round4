4f53bc5fc606b8efc3f34e3de58232d61840b11d
ssyoun4092 <ssyoun4092@naver.com>
2022-05-06 14:17:25 +0900

    Routine 타입 생성

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -1,11 +1,17 @@
 import Foundation
 
 struct BodyCondition {
-    var upperBodyStrength: Int = 0
-    var lowerBodyStrength: Int = 0
-    var muscleEndurance: Int = 0
-    var fatigue: Int = 0
+    var upperBodyStrength: Int
+    var lowerBodyStrength: Int
+    var muscleEndurance: Int
+    var fatigue: Int
+
+    func createRandomStrengthPoint(between range: ClosedRange<Int>) -> Int {
+        return Int.random(in: range)
+    }
 }
+
+var myBodyCondition = BodyCondition(upperBodyStrength: 0, lowerBodyStrength: 0, muscleEndurance: 0, fatigue: 0)
 
 struct Exercise {
     let name: String
@@ -13,32 +19,65 @@ struct Exercise {
 }
 
 let sitUp = Exercise(name: "윗몸일으키기") {
-    var bodyCondition = BodyCondition()
-
-    bodyCondition.upperBodyStrength += Int.random(in: 10...20)
-    bodyCondition.fatigue += Int.random(in: 10...20)
+    myBodyCondition.upperBodyStrength += myBodyCondition.createRandomStrengthPoint(between: 10...20)
+    myBodyCondition.fatigue += myBodyCondition.createRandomStrengthPoint(between: 10...20)
 }
 
 let squat = Exercise(name: "스쿼트") {
-    var bodyCondition = BodyCondition()
-
-    bodyCondition.lowerBodyStrength += Int.random(in: 20...30)
-    bodyCondition.fatigue += Int.random(in: 10...20)
+    myBodyCondition.lowerBodyStrength += myBodyCondition.createRandomStrengthPoint(between: 20...30)
+    myBodyCondition.fatigue += myBodyCondition.createRandomStrengthPoint(between: 10...20)
 }
 
 let runningLongTime = Exercise(name: "오래 달리기") {
-    var bodyCondition = BodyCondition()
-
-    bodyCondition.muscleEndurance += Int.random(in: 20...30)
-    bodyCondition.upperBodyStrength += Int.random(in: 5...10)
-    bodyCondition.lowerBodyStrength += Int.random(in: 5...10)
-    bodyCondition.fatigue += Int.random(in: 20...30)
+    myBodyCondition.muscleEndurance += myBodyCondition.createRandomStrengthPoint(between: 20...30)
+    myBodyCondition.upperBodyStrength += myBodyCondition.createRandomStrengthPoint(between: 5...10)
+    myBodyCondition.lowerBodyStrength += myBodyCondition.createRandomStrengthPoint(between: 5...10)
+    myBodyCondition.fatigue += myBodyCondition.createRandomStrengthPoint(between: 20...30)
 }
 
 let activeRest = Exercise(name: "동적휴식") {
-    var bodyCondition = BodyCondition()
-
-    bodyCondition.fatigue -= Int.random(in: 5...10)
+    myBodyCondition.fatigue -= myBodyCondition.createRandomStrengthPoint(between: 5...10)
 }
 
+struct Routine {
+    let name: String
+    let routineOrder: [Exercise]
+
+    func startRoutine() {
+        print("--------------")
+        print("\(name)을 시작합니다.")
+        calculateBodyConditionStrengthPoints()
+        printRoutineNames()
+        print("--------------")
+    }
+
+    func calculateBodyConditionStrengthPoints() {
+        for index in 0..<routineOrder.count {
+            routineOrder[index].action()
+        }
+    }
+
+    func printRoutineNames() {
+        for index in 0..<routineOrder.count {
+            print("\(routineOrder[index].name)")
+        }
+    }
+
+    func printBodyConditionPoints(in bodyCondition: BodyCondition) {
+        var condition: [Int] = []
+        condition.append(bodyCondition.upperBodyStrength)
+        condition.append(bodyCondition.lowerBodyStrength)
+        condition.append(bodyCondition.muscleEndurance)
+        condition.append(bodyCondition.fatigue)
+        print("현재의 컨디션은 다음과 같습니다.")
+        for index in 0..<condition.count {
+            print(condition[index])
+        }
+    }
+}
+
+
+var myRoutine = Routine(name: "My Routine", routineOrder: [sitUp, squat, runningLongTime, activeRest])
+myRoutine.startRoutine()
+myRoutine.printBodyConditionPoints(in: myBodyCondition)
 
