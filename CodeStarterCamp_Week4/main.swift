a906ef03a0fb658e9faa6aae5a19398aecbac24d
delight010 <81559026+delight010@users.noreply.github.com>
2022-05-06 18:48:35 +0900

    - Exercise 값들 생성

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,5 +8,33 @@
 
 import Foundation
 
-print("Hello, World!")
+let 윗몸일으키기: Exercise = Exercise(name: "윗몸일으키기",
+                                action: { (person: BodyCondition) in
+    person.upperBodyStrength += Int.random(in: 10...20)
+    person.fatigue += Int.random(in: 10...20)
+})
 
+let 스쿼트: Exercise = Exercise(name: "스쿼트",
+                             action: { (person: BodyCondition) in
+    person.lowerBodyStrength += Int.random(in: 20...30)
+    person.fatigue += Int.random(in: 10...20)
+})
+
+let 오래달리기: Exercise = Exercise(name: "오래달리기",
+                               action: { (person: BodyCondition) in
+    person.muscularEndurance += Int.random(in: 20...30)
+    person.upperBodyStrength += Int.random(in: 5...10)
+    person.lowerBodyStrength += Int.random(in: 5...10)
+    person.fatigue += Int.random(in: 20...30)
+})
+
+let 동적휴식: Exercise = Exercise(name: "동적휴식",
+                              action: { (person: BodyCondition) in
+    person.fatigue -= Int.random(in: 5...10)
+})
+
+let 줄넘기: Exercise = Exercise(name: "줄넘기",
+                             action: { (person: BodyCondition) in
+    person.muscularEndurance += Int.random(in: 5...15)
+    person.fatigue += Int.random(in: 5...10)
+})
