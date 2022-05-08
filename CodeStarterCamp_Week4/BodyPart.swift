b2233ff640435acc6eee2f80c8abb512dfd5433f
wolframhwang <hzw94@naver.com>
2022-05-09 06:06:28 +0900

    [feat] Mission4: Step2: 몸 부위에 따른 Enumerate 생성

---
+++ CodeStarterCamp_Week4/BodyPart.swift
@@ -0,0 +1,14 @@
+//
+//  BodyPart.swift
+//  CodeStarterCamp_Week4
+//
+//  Created by 황지웅 on 2022/05/09.
+//
+
+import Foundation
+
+enum BodyPart: String {
+    case upperBody = "상체근력"
+    case lowerBody = "하체근력"
+    case muscularEndurance = "근지구력"
+}
