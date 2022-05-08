eb41b4a3b92b365df916c78947aa293bf0c113a1
wolframhwang <hzw94@naver.com>
2022-05-09 06:58:59 +0900

    [feat] Mission4: Step2: Person 추가 반영
    1, Person은 이름과 bodyCondition을 저장 프로퍼티로 소유합니다.
    2. 저장 프로퍼티의 정보는 개인정보에 해당하므로 전부 private처리합니다.
    3. 또한, 필요한 정보에 대해 획득 할 수 있게, 연산 프로퍼티를 만들어 두었습니다.
    4. 신체 성장에 관한 책임을 소유하고있고, 그것을 바디 컨디션에 또 전달할수 있습니다.
    5. 피로도 관리에 대한 부분 역시 마찬가지로 전달하는 책임이 있습니다.
    6. 피로도 를 넘어섰거나, 목표치를 이뤘는지 확인하기 위한 인터페이스적 역할 역시 겸하고있습니다.

--- CodeStarterCamp_Week4/Person.swift
+++ CodeStarterCamp_Week4/Person.swift
@@ -7,3 +7,51 @@
 
 import Foundation
 
+class Person {
+    private var name: String
+    private var bodyCondition: BodyCondition = BodyCondition(upperBody: 0, lowerBody: 0, muscularEndurance: 0, fatigue: 0)
+    
+    var getName: String {
+        get {
+            return name
+        }
+    }
+    
+    var myFatigue: Int {
+        get {
+            return bodyCondition.myFatigue
+        }
+    }
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func growthBody(bodyPart: BodyPart, overLimitValue: Int, underLimitValue: Int) {
+        bodyCondition.growthBody(bodyPart: bodyPart,
+                                 overLimitValue: overLimitValue,
+                                 underLimitValue: underLimitValue)
+    }
+    
+    func manageFatigue(overLimitValue: Int, underLimitValue: Int, restOfTraining: RestOrTraining) {
+        bodyCondition.manageFatigue(overLimitValue: overLimitValue,
+                                    underLimitValue: underLimitValue,
+                                    restOfTraining: restOfTraining)
+    }
+    
+    func checkOverExercise() -> Bool {
+        return bodyCondition.checkFatigue()
+    }
+    
+    func checkGoal(goal: BodyCondition) -> Bool {
+        return bodyCondition.isGetGoal(goal: goal)
+    }
+    
+    func exercise(for set: Int, routine: Routine) {
+        routine.run(person: self, for: set)
+    }
+    
+    func printStatus() {
+        bodyCondition.printStatus()
+    }
+}
