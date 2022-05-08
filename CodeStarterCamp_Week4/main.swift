4ebb0ee1f7b00294cfbab9e83778b871fa529b83
jsa0224 <puccav@naver.com>
2022-05-08 14:00:51 +0900

    main 소스 파일 - 각 객체의 인스턴스 생성

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,5 +8,12 @@
 
 import Foundation
 
-print("Hello, World!")
+var person = BodyCondition(upperBodyStrength: 0, lowerBodyStrength: 0, muscularEndurance: 0, fatigue: 0)
+
+let exercise: Array<Exercise> = [sitUp, sitUp, sitUp, activeRest, deadLift, deadLift, deadLift, activeRest, plank, plank, plank, activeRest, barbellRow, barbellRow, barbellRow, activeRest, cablePullover, cablePullover, cablePullover, activeRest, longRun]
+
+var hellRoutine = Routine(routineName: "지옥의 헬스루틴", exercises: exercise)
+
+hellRoutine.startRoutine() // 지옥의 헬스루틴 시작
+person.checkYourCondition() // 인바디 측정 
 
