c458b970d89ccce82145b4a156e667c9fb8e5472
wolframhwang <hzw94@naver.com>
2022-05-09 06:36:59 +0900

    [feat] Mission4: Step2: Exercise 에 필요한 관리 요소에 대한 집합체 반영

---
+++ CodeStarterCamp_Week4/ExerciseSet/ResrOrTraining.swift
@@ -0,0 +1,13 @@
+//
+//  ResrOrTraining.swift
+//  CodeStarterCamp_Week4
+//
+//  Created by 황지웅 on 2022/05/09.
+//
+
+import Foundation
+
+enum RestOrTraining {
+    case rest
+    case training
+}
