09593e4b6b629c0beca8d40d8b6995b1df472d44
MANSU.J <seunghyun.jung.84@gmail.com>
2022-05-08 17:38:16 +0900

    Feat: 운동 타입
    - 운동 인스턴스 생성을 위한 운동타입

--- CodeStarterCamp_Week4/Exercise.swift
+++ CodeStarterCamp_Week4/Exercise.swift
@@ -1,8 +1,27 @@
-//
-//  Exercise.swift
-//  CodeStarterCamp_Week4
-//
-//  Created by SH.Jung on 2022/05/08.
-//
-
 import Foundation
+
+struct Exercise {
+	let name: String
+	let action: () -> Void
+}
+
+let 윗몸일으키기: Exercise = Exercise(name: "윗몸일으키기", action: {
+	myBodyCondition.upperMicle += Int.random(in: 10...20)
+	myBodyCondition.fatigue += Int.random(in: 10...20)
+})
+
+let 스쿼트: Exercise = Exercise(name: "스쿼트", action: {
+	myBodyCondition.lowerMicle += Int.random(in: 20...30)
+	myBodyCondition.fatigue += Int.random(in: 10...20)
+})
+
+let 오래달리기: Exercise = Exercise(name: "오래달리기", action: {
+	myBodyCondition.upperMicle += Int.random(in: 5...10)
+	myBodyCondition.lowerMicle += Int.random(in: 5...10)
+	myBodyCondition.endurance += Int.random(in: 20...30)
+	myBodyCondition.fatigue += Int.random(in: 20...30)
+})
+
+let 동적휴식: Exercise = Exercise(name: "동적휴식", action: {
+	myBodyCondition.fatigue -= Int.random(in: 10...20)
+})
