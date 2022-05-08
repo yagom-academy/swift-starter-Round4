06581017d6aa1b70070289cda61c983c44320f59
hansm <hanmix2003@naver.com>
2022-05-09 05:58:42 +0900

    Feat: 각 타입의 인스턴스를 생성하고 운동루틴 배열을 할당한 후 운동루틴 시작과 bodyCondition의 값을 확인하는 코드 추가
    - BodyCondition 인스턴스: 상체근력, 하체근력, 근지구력, 피로도의 초기값을 0으로 하는 인스턴스
    - todayRoutine 인스턴스: 운동 루틴의 이름과 운동 루틴을 빈 배열로 가지고 있는 인스턴스
    - sitUp, squat, longRunning, activeRest 인스턴스: 운동의 이름과 운동 루틴 실행 후 bodyCondition의 값을 바꾸는 클로저를 가지고 있는 인스턴스
    - 운동 루틴을 배열에 담아서 추가
    - 운동 루틴을 실행하는 함수 호출
    - 운동 후 바뀐 bodyCondition을 출력하는 함수 호출

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
+let myBodyCondition: BodyCondition = BodyCondition(upperBodyMuscleStrength: 0, lowerBodyMuscleStrength: 0, muscularEndurance: 0, tiredness: 0)
+var todayRoutine: Routine = Routine(name: "전신 근육 박살내기 루틴", routineArray: [])
+
+let sitUp: Exercise = Exercise(name: "윗몸일으키기", action: {
+    (bodyCondition: BodyCondition) in
+    bodyCondition.upperBodyMuscleStrength += bodyCondition.selectRandomStatusIndex(min: 10, max: 20)
+    bodyCondition.tiredness += bodyCondition.selectRandomStatusIndex(min: 10, max: 20)
+})
+
+let squat: Exercise = Exercise(name: "스쿼트", action: { (bodyCondition: BodyCondition) in
+    bodyCondition.lowerBodyMuscleStrength += bodyCondition.selectRandomStatusIndex(min: 20, max: 30)
+    bodyCondition.tiredness += bodyCondition.selectRandomStatusIndex(min: 10, max: 20)
+})
+
+let longRunning: Exercise = Exercise(name: "오래달리기", action: { (bodyCondition: BodyCondition) in
+    bodyCondition.upperBodyMuscleStrength += bodyCondition.selectRandomStatusIndex(min: 5, max: 10)
+    bodyCondition.lowerBodyMuscleStrength += bodyCondition.selectRandomStatusIndex(min: 5, max: 10)
+    bodyCondition.muscularEndurance += bodyCondition.selectRandomStatusIndex(min: 20, max: 30)
+    bodyCondition.tiredness += bodyCondition.selectRandomStatusIndex(min: 20, max: 30)
+})
+
+let activeRest: Exercise = Exercise(name: "동적휴식", action: { (bodyCondition: BodyCondition) in
+    bodyCondition.tiredness -= bodyCondition.selectRandomStatusIndex(min: 5, max: 10)
+})
+
+todayRoutine.routineArray = [sitUp, sitUp, activeRest, squat, squat, activeRest, longRunning, longRunning]
+
+todayRoutine.startExerciseRoutine()
+myBodyCondition.printMucleStatus()
+
 
