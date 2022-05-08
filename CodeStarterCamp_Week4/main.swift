60a5fd3bb7e109fe6f1bcf0545f7602f800c4f8c
delight010 <81559026+delight010@users.noreply.github.com>
2022-05-09 00:08:06 +0900

    - Exercise 인스턴스명 변경 - 인스턴스 생성 부분, 메소드실행부분 구분

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,19 +8,19 @@
 
 import Foundation
 
-let 윗몸일으키기: Exercise = Exercise(name: "윗몸일으키기",
+let sitUp: Exercise = Exercise(name: "윗몸일으키기",
                                 action: { (person: BodyCondition) in
     person.upperBodyStrength += Int.random(in: 10...20)
     person.fatigue += Int.random(in: 10...20)
 })
 
-let 스쿼트: Exercise = Exercise(name: "스쿼트",
+let squat: Exercise = Exercise(name: "스쿼트",
                              action: { (person: BodyCondition) in
     person.lowerBodyStrength += Int.random(in: 20...30)
     person.fatigue += Int.random(in: 10...20)
 })
 
-let 오래달리기: Exercise = Exercise(name: "오래달리기",
+let longRun: Exercise = Exercise(name: "오래달리기",
                                action: { (person: BodyCondition) in
     person.muscularEndurance += Int.random(in: 20...30)
     person.upperBodyStrength += Int.random(in: 5...10)
@@ -28,22 +28,22 @@ let 오래달리기: Exercise = Exercise(name: "오래달리기",
     person.fatigue += Int.random(in: 20...30)
 })
 
-let 동적휴식: Exercise = Exercise(name: "동적휴식",
+let dynamicBreak: Exercise = Exercise(name: "동적휴식",
                               action: { (person: BodyCondition) in
     person.fatigue -= Int.random(in: 5...10)
 })
 
-let 줄넘기: Exercise = Exercise(name: "줄넘기",
+let jumpRope: Exercise = Exercise(name: "줄넘기",
                              action: { (person: BodyCondition) in
     person.muscularEndurance += Int.random(in: 5...15)
     person.fatigue += Int.random(in: 5...10)
 })
 
-let hellRoutine = Routine(name: "지옥 루틴", exercises: [윗몸일으키기, 윗몸일으키기, 동적휴식, 스쿼트, 스쿼트, 동적휴식, 오래달리기, 오래달리기])
-let lightRoutine = Routine(name: "가벼운 루틴", exercises: [윗몸일으키기, 동적휴식, 줄넘기, 스쿼트, 동적휴식])
+let hellRoutine = Routine(name: "지옥 루틴", exercises: [sitUp, sitUp, dynamicBreak, squat, squat, dynamicBreak, longRun, longRun])
+let lightRoutine = Routine(name: "가벼운 루틴", exercises: [sitUp, dynamicBreak, jumpRope, squat, dynamicBreak])
 
 let kimYuna = BodyCondition(name: "김연아", upperBodyStrength: 0, lowerBodyStrength: 0, muscularEndurance: 0, fatigue: 0)
-kimYuna.exercise(routine: hellRoutine)
-
 let songGain = BodyCondition(name: "송가인", upperBodyStrength: 0, lowerBodyStrength: 0, muscularEndurance: 0, fatigue: 0)
+
+kimYuna.exercise(routine: hellRoutine)
 songGain.exercise(routine: lightRoutine)
