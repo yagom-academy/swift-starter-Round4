11f31662ba98ffd7a22c046dc92e0be3b708f91f
MANSU.J <seunghyun.jung.84@gmail.com>
2022-05-09 15:44:44 +0900

    Refactor: 운동 타입
    - 오타 수정
    - 클로저를 후행클로저로 반영

--- CodeStarterCamp_Week4/Exercise.swift
+++ CodeStarterCamp_Week4/Exercise.swift
@@ -5,23 +5,23 @@ struct Exercise {
 	let action: () -> Void
 }
 
-let 윗몸일으키기: Exercise = Exercise(name: "윗몸일으키기", action: {
-	myBodyCondition.upperMicle += Int.random(in: 10...20)
+let 윗몸일으키기: Exercise = Exercise(name: "윗몸일으키기") {
+	myBodyCondition.upperMuscle += Int.random(in: 10...20)
 	myBodyCondition.fatigue += Int.random(in: 10...20)
-})
+}
 
-let 스쿼트: Exercise = Exercise(name: "스쿼트", action: {
-	myBodyCondition.lowerMicle += Int.random(in: 20...30)
+let 스쿼트: Exercise = Exercise(name: "스쿼트") {
+	myBodyCondition.lowerMuscle += Int.random(in: 20...30)
 	myBodyCondition.fatigue += Int.random(in: 10...20)
-})
+}
 
-let 오래달리기: Exercise = Exercise(name: "오래달리기", action: {
-	myBodyCondition.upperMicle += Int.random(in: 5...10)
-	myBodyCondition.lowerMicle += Int.random(in: 5...10)
+let 오래달리기: Exercise = Exercise(name: "오래달리기") {
+	myBodyCondition.upperMuscle += Int.random(in: 5...10)
+	myBodyCondition.lowerMuscle += Int.random(in: 5...10)
 	myBodyCondition.endurance += Int.random(in: 20...30)
 	myBodyCondition.fatigue += Int.random(in: 20...30)
-})
+}
 
-let 동적휴식: Exercise = Exercise(name: "동적휴식", action: {
+let 동적휴식: Exercise = Exercise(name: "동적휴식") {
 	myBodyCondition.fatigue -= Int.random(in: 10...20)
-})
+}
