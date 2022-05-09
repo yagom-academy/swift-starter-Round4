e55a70d83e66091f0f45ad4fb8dbf09b51e852bb
jeongmin <lj845854@gmail.com>
2022-05-10 08:14:33 +0900

    refactor : routineList 상수로 변경

--- CodeStarterCamp_Week4/FitnessCenter.swift
+++ CodeStarterCamp_Week4/FitnessCenter.swift
@@ -10,7 +10,7 @@ import Foundation
 class FitnessCenter {
     var targetBodyCondition = BodyCondition()
     var member: Person?
-    var routineList: [Routine]
+    let routineList: [Routine]
     
     init(routineList: [Routine]) {
         self.routineList = routineList
