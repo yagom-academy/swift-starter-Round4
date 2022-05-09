69f1ca39b4b562c8b9cd54b7deede1cba274ce3d
홍승주 <hongseungju@hongseungjuui-MacBookAir.local>
2022-05-09 19:09:06 +0900

    main수정본

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -10,6 +10,7 @@ import Foundation
 
 var person = BodyCondition(upperBodyMuscleStrength: 0, lowerBodyMuscleStrength: 0, muscleEndurance: 0, fatigue: 0)
 let exercise: Array<Exercise> = [sitUp, activeRest, yoga]
-var aa = Routine(routineName: "hellroutine", exercises: exercise)
-aa.startRoutine()
+var personRoutine = Routine(routineName: "hellroutine", exercises: exercise)
+
+personRoutine.printRoutine()
 person.checkBodyCondition()
