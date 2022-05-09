9e1b176b62bfdcf0cde0716a6c8c09b65c15ab5f
hansm <hanmix2003@naver.com>
2022-05-09 18:09:15 +0900

    Refactor: 각 운동의 클로저 축약형으로 수정
    - Exercise 타입의 action 프로퍼티는 BodyCondition 타입을 매개변수로 가지고 있으므로 클로저 축약형의 인자($0)를 받아서 구현함
    - 클로저 축약문법으로 표현하여 가독성 향상을 유도함

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -10,22 +10,13 @@ import Foundation
 
 let myBodyCondition: BodyCondition = BodyCondition(upperBodyMuscleStrength: 0, lowerBodyMuscleStrength: 0, muscularEndurance: 0, tiredness: 0)
 
-let sitUp: Exercise = Exercise(name: "윗몸일으키기", action: {
-    (bodyCondition: BodyCondition) in
-    bodyCondition.changeStatusAfterSitup()
-})
+let sitUp: Exercise = Exercise(name: "윗몸일으키기", action: { $0.changeStatusAfterSitup() })
 
-let squat: Exercise = Exercise(name: "스쿼트", action: { (bodyCondition: BodyCondition) in
-    bodyCondition.changeStatusAfterSquat()
-})
+let squat: Exercise = Exercise(name: "스쿼트", action: { $0.changeStatusAfterSquat() })
 
-let longRunning: Exercise = Exercise(name: "오래달리기", action: { (bodyCondition: BodyCondition) in
-    bodyCondition.changeStatusAfterlongRunning()
-})
+let longRunning: Exercise = Exercise(name: "오래달리기", action: { $0.changeStatusAfterlongRunning() })
 
-let activeRest: Exercise = Exercise(name: "동적휴식", action: { (bodyCondition: BodyCondition) in
-    bodyCondition.changeStatusAfterActiveRest()
-})
+let activeRest: Exercise = Exercise(name: "동적휴식", action: { $0.changeStatusAfterActiveRest() })
 
 let todayRoutine: Routine = Routine(name: "전신 근육 박살내기 루틴", routineArray: [sitUp, sitUp, activeRest, squat, squat, activeRest, longRunning, longRunning])
 
