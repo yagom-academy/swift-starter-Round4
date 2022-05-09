8b5eea429716f450bd492d8734e0b177cfe60ac9
MANSU.J <seunghyun.jung.84@gmail.com>
2022-05-09 15:43:26 +0900

    Refactor: 바디컨디션 타입
    - 오타 수정
    - 함수명 기능에 맞게 변경

--- CodeStarterCamp_Week4/BodyCondition.swift
+++ CodeStarterCamp_Week4/BodyCondition.swift
@@ -1,17 +1,17 @@
 import Foundation
 
 class BodyCondition {
-	var upperMicle: Int = 0
-	var lowerMicle: Int = 0
+	var upperMuscle: Int = 0
+	var lowerMuscle: Int = 0
 	var endurance: Int = 0
 	var fatigue: Int = 0
 	
-	func yourConsition() {
+	func printCondition() {
 	print("""
 		--------------
 		현재의 컨디션은 다음과 같습니다.
-		상체근력: \(upperMicle)
-		하체근력: \(lowerMicle)
+		상체근력: \(upperMuscle)
+		하체근력: \(lowerMuscle)
 		근지구력: \(endurance)
 		피로도: \(fatigue)
 		""")
