7b474c8dba9be9cb9b89aae5b7ac4bbef2249577
jeongmin <lj845854@gmail.com>
2022-05-09 09:29:03 +0900

    refactor : LocalizedError를 이용한 메세지 출력

--- CodeStarterCamp_Week4/Error.swift
+++ CodeStarterCamp_Week4/Error.swift
@@ -16,6 +16,25 @@ enum FitnessCenterContingency: Error {
     case notEnoughToTargetError
 }
 
+extension FitnessCenterContingency: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .unRecognizedInputError:
+            return "인식할 수 없는 값입니다."
+        case .stringToIntConvertError:
+            return "정수로 변환할 수 없습니다."
+        case .invalidNumberOfValuesError:
+            return "너무 적거나 많은 값을 입력했습니다."
+        case .notExistMemberError:
+            return "회원님이 존재하지 않습니다."
+        case .exhaustedMemberError:
+            return "회원님이 탈진했습니다."
+        case .notEnoughToTargetError:
+            return "목표치에 도달하지 못했습니다"
+        }
+    }
+}
+
 enum ErrorValue {
     static let stringToIntConvertErrorValue = -1
 }
