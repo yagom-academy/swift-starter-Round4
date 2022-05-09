249cdcc4017ed163b9e0bd095448c6ee1d68947a
hansm <hanmix2003@naver.com>
2022-05-09 17:44:40 +0900

    Refactor: Exercise 인스턴스의 클로저 부분을 메소드 체이닝으로 가독성 향상
    - 운동과 휴식을 한 후 변화하는 근력량과 피로도를 임의의 값과 연산하는 함수를 클로저 안에 메소드 체이닝으로 작성하여 가독성 향상을 유도함.

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -12,24 +12,19 @@ let myBodyCondition: BodyCondition = BodyCondition(upperBodyMuscleStrength: 0, l
 
 let sitUp: Exercise = Exercise(name: "윗몸일으키기", action: {
     (bodyCondition: BodyCondition) in
-    bodyCondition.upperBodyMuscleStrength += bodyCondition.selectRandomStatusIndex(min: 10, max: 20)
-    bodyCondition.tiredness += bodyCondition.selectRandomStatusIndex(min: 10, max: 20)
+    bodyCondition.changeStatusAfterSitup()
 })
 
 let squat: Exercise = Exercise(name: "스쿼트", action: { (bodyCondition: BodyCondition) in
-    bodyCondition.lowerBodyMuscleStrength += bodyCondition.selectRandomStatusIndex(min: 20, max: 30)
-    bodyCondition.tiredness += bodyCondition.selectRandomStatusIndex(min: 10, max: 20)
+    bodyCondition.changeStatusAfterSquat()
 })
 
 let longRunning: Exercise = Exercise(name: "오래달리기", action: { (bodyCondition: BodyCondition) in
-    bodyCondition.upperBodyMuscleStrength += bodyCondition.selectRandomStatusIndex(min: 5, max: 10)
-    bodyCondition.lowerBodyMuscleStrength += bodyCondition.selectRandomStatusIndex(min: 5, max: 10)
-    bodyCondition.muscularEndurance += bodyCondition.selectRandomStatusIndex(min: 20, max: 30)
-    bodyCondition.tiredness += bodyCondition.selectRandomStatusIndex(min: 20, max: 30)
+    bodyCondition.changeStatusAfterlongRunning()
 })
 
 let activeRest: Exercise = Exercise(name: "동적휴식", action: { (bodyCondition: BodyCondition) in
-    bodyCondition.tiredness -= bodyCondition.selectRandomStatusIndex(min: 5, max: 10)
+    bodyCondition.changeStatusAfterActiveRest()
 })
 
 let todayRoutine: Routine = Routine(name: "전신 근육 박살내기 루틴", routineArray: [sitUp, sitUp, activeRest, squat, squat, activeRest, longRunning, longRunning])
