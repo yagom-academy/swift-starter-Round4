d7cfbdccef1da14b5895981939b1e7a5845794e6
delight010 <81559026+delight010@users.noreply.github.com>
2022-05-10 18:48:20 +0900

    - InputError 추가

---
+++ CodeStarterCamp_Week4/Error.swift
@@ -0,0 +1,14 @@
+//
+//  Error.swift
+//  CodeStarterCamp_Week4
+//
+//  Created by Seong A Oh on 2022/05/10.
+//
+
+import Foundation
+
+enum InputError: Error {
+    case empty
+    case outOfValue
+    case invaildValue
+}
