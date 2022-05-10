1712c3ebe5423ca3f111ddc2807aa569aecdc20c
delight010 <81559026+delight010@users.noreply.github.com>
2022-05-10 18:51:15 +0900

    - FitnessCenter 이름 입력 메서드 추가 - FitnessCenter 운동하기 메서드 추가

--- CodeStarterCamp_Week4/FitnessCenter.swift
+++ CodeStarterCamp_Week4/FitnessCenter.swift
@@ -23,4 +23,28 @@ class FitnessCenter {
                   member: .init(name: "", bodyCondition: .init(upperBodyStrength: 0, lowerBodyStrength: 0, muscularEndurance: 0, fatigue: 0)),
                   routine: routine)
     }
+    
+    func exercise() {
+        do {
+            self.member?.name = try inputName()
+        } catch InputError.empty {
+            print("값을 입력하지않았습니다. 값을 입력해주세요")
+        } catch InputError.invaildValue {
+            print("입력할 수 없는 값입니다.")
+        } catch InputError.outOfValue {
+            print("값의 갯수가 적거나 큽니다.")
+        } catch {
+            
+        }
+    }
+    
+    func inputName() throws -> String {
+        print("안녕하세요. 야곰 피트니스 센터입니다. 회원님의 이름은 무엇인가요?")
+        let inputName: String = readLine()!
+        guard inputName.isEmpty == false else {
+            throw InputError.empty
+        }
+        
+        return inputName
+    }
 }
