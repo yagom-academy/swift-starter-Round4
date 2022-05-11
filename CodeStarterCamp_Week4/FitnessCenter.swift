84166ba8bde164dbcb2b9899ba6f051ba728517f
hansm <hanmix2003@naver.com>
2022-05-12 04:32:46 +0900

    Feat: FitnessCenter 타입 정의
    - cafeName 프로퍼티: 인스턴스 생성 시 이름 속성을 할당할 수 있는 프로퍼티
    - goalBodyCondition 프로퍼티: 운동 목표치를 할당하기 위한 프로퍼티로 초기값은 0
    - member 프로퍼티: 운동을 진행할 회원을 정의하는 프로퍼티로 옵셔널이므로 초기값은 nil
    - routineList 프로퍼티: 운동 루틴들을 배열로서 가지는 프로퍼티
    - inputToInt throw 함수: 사용자 입력값으로 숫자를 받고 옵셔널 문자열을 옵셔널 정수로 바꿨을 때 unwrapping 하여 입력값을 리턴함.
    조건을 만족하지 않을 경우 notInt 에러를 throw 함
    - inputToString throw 함수: 사용자 입력값으로 문자열을 받고, 입력값이 비어있지 않고, 입력값에 빈칸이 없을 때 입력값이 문자열인 경우 unwrapping한 다음 입력값을 리턴함
    조건을 만족하지 않은 경우 notString 에러를 throw 함
    - proceedExercise throw 함수: 운동 루틴의 종류와 횟수를 사용자 입력값으로 받아 시도하고 운동 루틴 후의 목표치 값을 출력하는 함수를 시도함
    1. 운동 목표치에 도달하지 못 하는 에러를 받는 경우 현재 컨디션 상태를 출력함
    2. 회원이 도망가는 에러를 받는 경우 다시 회원이 도망가는 에러를 throw 함
    - printAfterRoutineResult throw 함수: 회원이 운동 목표치에 도달할 경우 성공 안내문을 출력하고 현재 컨디션 상태를 출력함
    - registerMember 함수: inputToString 함수의 리턴값을 받아 회원을 등록함
    컨디션 초기값은 0이다
    notString 에러를 받을 경우 안내문을 출력함
    그외의 에러일 경우 에러메시지를 출력함
    - selectGoalBodyCondition 함수: 운동 목표치와 현재 피로도 상태를 사용자 입력값으로 받음
    notInt 에러를 받는 경우 안내문을 출력함
    그외 에러일 경우 에러메시지를 출력함
    - chooseRountine 함수: 루틴 배열 중 하나를 선택하고 해당 루틴을 리턴함
    선택한 루틴의 index가 배열 안에 없는 경우 invaildRoutine 에러를 throw 함
    invaildRoutine 에러를 받는 경우 안내문을 출력함
    notInt 에러를 받는 경우 안내문을 출력함
    그외 에러일 경우 에러메시지를 출력함
    - setCountOfRoutine 함수: 루틴을 몇 번 반복할지를 사용자 입력값으로 받아 그 값을 리턴함
    notInt 에러를 받는 경우 안내문을 출력함
    그외 에러일 경우 에러메시지를 출력함
    - startForTotalRoutine 함수: 전체 진행 상황을 호출함
    runAwayMember 에러를 받는 경우 회원의 피로도와 회원이 도망갔다는 안내문을 출력함
    그외 에러일 경우 에러메시지를 출력함

---
+++ CodeStarterCamp_Week4/FitnessCenter.swift
@@ -0,0 +1,195 @@
+//
+//  FitnessCenter.swift
+//  CodeStarterCamp_Week4
+//
+//  Created by mingmac on 2022/05/10.
+//
+
+import Foundation
+
+class FitnessCenter {
+    let centerName: String
+    var goalBodyCondition: BodyCondition = BodyCondition(upperBodyMuscleStrength: 0, lowerBodyMuscleStrength: 0, muscularEndurance: 0, tiredness: 0)
+    var member: Person?
+    let routineList: [Routine]
+    
+    init(centerName: String, routineList: [Routine]) {
+        self.centerName = centerName
+        self.routineList = routineList
+    }
+    
+    func inputToInt() throws -> Int {
+        guard let inputInt = readLine(), let inputInt = Int(inputInt) else {
+            throw InputError.notInt
+        }
+        return inputInt
+    }
+    
+    func inputToString() throws -> String {
+        guard let inputName = readLine(), inputName.isEmpty == false, inputName.contains(" ") == false else {
+            throw InputError.notString
+            }
+        for inputValue in inputName {
+            guard inputValue.isLetter else {
+                throw InputError.notString
+            }
+        }
+        return inputName
+    }
+    
+    func proceedExercise() throws {
+        if let member = member {
+            while true {
+                do {
+                    try member.exercise(routineArray: chooseRountine(from: routineList), for: setCountOfRoutine(), overTiredness: goalBodyCondition.tiredness)
+                    try printAfterRoutineResult()
+                    break
+                } catch FitnessCenterError.unreachedGoal {
+                    print("--------------------")
+                    print("목표치에 도달하지 못 했습니다. 현재 \(member.name)님의 컨디션은 다음과 같습니다.")
+                    member.bodyCondition.printMucleStatus()
+                } catch FitnessCenterError.runAwayMember {
+                    throw FitnessCenterError.runAwayMember
+                } catch {
+                    print("에러메시지를 확인해주세요. \(error)")
+                    break
+                }
+            }
+        }
+        
+    }
+    
+    func printAfterRoutineResult() throws {
+        if let member = member {
+            guard member.bodyCondition.upperBodyMuscleStrength >= goalBodyCondition.upperBodyMuscleStrength,
+                  member.bodyCondition.lowerBodyMuscleStrength >= goalBodyCondition.lowerBodyMuscleStrength,
+                  member.bodyCondition.muscularEndurance >= goalBodyCondition.muscularEndurance else {
+                throw FitnessCenterError.unreachedGoal
+            }
+            print("--------------------")
+            print("성공입니다! 현재 \(member.name)님의 컨디션은 다음과 같습니다.")
+            member.bodyCondition.printMucleStatus()
+        }
+    }
+    
+    func registerMember() {
+        print("안녕하세요 \(centerName) 피트니스 센터 입니다. 회원님의 이름을 알려주세요.")
+        while true {
+            do {
+                member = Person(name: try inputToString(), bodyCondition: BodyCondition(upperBodyMuscleStrength: 0, lowerBodyMuscleStrength: 0, muscularEndurance: 0, tiredness: 0))
+                break
+            } catch InputError.notString {
+                print("한글 또는 영문으로 입력해주세요.")
+            } catch {
+                print("에러메시지를 확인해주세요. \(error)")
+            }
+        }
+    }
+    
+    func selectGoalBodyCondition() {
+        print("--------------------")
+        print("운동 목표치를 순서대로 알려주세요. 예시) 상체근력: 130, 하체근력: 120, 근지구력: 150")
+        while true {
+            do {
+                print("상체근력 : ", terminator: "")
+                try goalBodyCondition.upperBodyMuscleStrength += inputToInt()
+                break
+            } catch InputError.notInt {
+                print("숫자로 입력해주세요.")
+            } catch {
+                print("에러메시지를 확인해주세요. \(error)")
+            }
+        }
+        while true {
+            do {
+                print("하체근력 : ", terminator: "")
+                try goalBodyCondition.lowerBodyMuscleStrength += inputToInt()
+                break
+            } catch InputError.notInt {
+                print("숫자로 입력해주세요.")
+            } catch {
+                print("에러메시지를 확인해주세요. \(error)")
+            }
+        }
+        while true {
+            do {
+                print("근지구력 : ", terminator: "")
+                try goalBodyCondition.muscularEndurance += inputToInt()
+                break
+            } catch InputError.notInt {
+                print("숫자로 입력해주세요.")
+            } catch {
+                print("에러메시지를 확인해주세요. \(error)")
+            }
+        }
+        while true {
+            print("현재 피로도를 0에서 100 사이의 숫자로 알려주세요!")
+            do {
+                print("현재 피로도 : ", terminator: "")
+                if let member = member {
+                    try member.bodyCondition.tiredness += inputToInt()
+                    break
+                }
+            } catch InputError.notInt {
+                print("숫자로 입력해주세요.")
+            } catch {
+                print("에러메시지를 확인해주세요. \(error)")
+            }
+        }
+    }
+    
+    func chooseRountine(from routineList: [Routine]) -> Routine {
+        while true {
+            do {
+                print("--------------------")
+                print("몇 번째 루틴으로 하시겠어요?")
+                for (index, routine) in routineList.enumerated() {
+                    print("\(index + 1). \(routine.name)")
+                }
+                let index = try inputToInt() - 1
+                
+                guard routineList.indices.contains(index) else {
+                    throw FitnessCenterError.invaildRoutine
+                }
+                return routineList[index]
+            } catch FitnessCenterError.invaildRoutine {
+                print("선택한 루틴이 없습니다.")
+            } catch InputError.notInt {
+                print("숫자로 입력해주세요.")
+            } catch {
+                print("에러메시지를 확인해주세요. \(error)")
+            }
+        }
+    }
+    
+    func setCountOfRoutine() -> Int {
+        while true {
+            do {
+                print("--------------------")
+                print("몇 세트 반복 하시겠어요?")
+                let setCounter = try inputToInt()
+                return setCounter
+            } catch InputError.notInt {
+                print("숫자로 입력해주세요.")
+            } catch {
+                print("에러메시지를 확인해주세요. \(error)")
+            }
+        }
+    }
+    
+    func startForTotalRoutine() {
+        registerMember()
+        selectGoalBodyCondition()
+            do {
+                try proceedExercise()
+            } catch FitnessCenterError.runAwayMember {
+                print("--------------------")
+                if let member = member {
+                print("\(member.name)님의 피로도가 \(member.bodyCondition.tiredness)입니다. 회원님이 도망갔습니다.")
+                }
+            } catch {
+                print("에러메시지를 확인해주세요. \(error)")
+            }
+        
+    }
+}
