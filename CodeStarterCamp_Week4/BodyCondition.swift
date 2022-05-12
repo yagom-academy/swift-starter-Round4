040d9dd3acc08562135175758b2cc790c3a349fc
Wonbi <wonbi@Wonbiui-MacBookPro.local>
2022-05-12 14:07:14 +0900

    Feat: 루틴생성, 함수실행

--- CodeStarterCamp_Week4/BodyCondition.swift
+++ CodeStarterCamp_Week4/BodyCondition.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class BodyCondition {
+    var upperBodyMuscleStrength: Int = 0
+    var lowerBodyMuscleStrength: Int = 0
+    var musclerEndurance: Int = 0
+    var tiredness: Int = 0
+    
+    func checkCondition() {
+        print("""
+              --------------
+              현재의 컨디션은 다음과 같습니다.
+              상체근력: \(upperBodyMuscleStrength)
+              하체근력: \(lowerBodyMuscleStrength)
+              근지구력: \(musclerEndurance)
+              피로도: \(tiredness)
+              """)
+    }
+}
--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,5 +8,38 @@
 
 import Foundation
 
-print("Hello, World!")
+let sitUp = Exercise(name: "윗몸일으키기") {bodyCondition in
+    bodyCondition.upperBodyMuscleStrength += Int.random(in: 10...20)
+    bodyCondition.tiredness += Int.random(in: 10...20)
+}
+let squats = Exercise(name: "스쿼트") {bodyCondition in
+    bodyCondition.lowerBodyMuscleStrength += Int.random(in: 20...30)
+    bodyCondition.tiredness += Int.random(in: 10...20)
+}
+let deadLift = Exercise(name: "데드리프트") {bodyCondition in
+    bodyCondition.upperBodyMuscleStrength += Int.random(in: 10...20)
+    bodyCondition.lowerBodyMuscleStrength += Int.random(in: 10...20)
+    bodyCondition.tiredness += Int.random(in: 10...20)
+}
+let benchPress = Exercise(name: "벤치프레스") {bodyCondition in
+    bodyCondition.upperBodyMuscleStrength += Int.random(in: 20...30)
+    bodyCondition.tiredness += Int.random(in: 10...20)
+}
+let runningLongTime = Exercise(name: "오래달리기") {bodyCondition in
+    bodyCondition.upperBodyMuscleStrength += Int.random(in: 5...10)
+    bodyCondition.lowerBodyMuscleStrength += Int.random(in: 10...20)
+    bodyCondition.musclerEndurance += Int.random(in: 20...30)
+    bodyCondition.tiredness += Int.random(in: 10...20)
+}
+let activeRest = Exercise(name: "동적휴식") {bodyCondition in
+    bodyCondition.tiredness -= Int.random(in: 10...20)
+}
+
+let hellRoutine = Routine(name: "지옥의루틴", routineArray: [squats, squats, activeRest, benchPress, benchPress, deadLift, deadLift, activeRest, sitUp, sitUp, activeRest, runningLongTime])
+
+var wonbi = BodyCondition()
+hellRoutine.printRotineList()
+hellRoutine.startRoutine(affecting: wonbi)
+wonbi.checkCondition()
+
 
