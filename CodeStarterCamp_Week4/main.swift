a0e007b0e0b6bf9f685ee9e5340b996c479b90d2
OneE <top9712@daum.net>
2022-05-13 00:45:00 +0900

    feat: exercise 추가

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -31,3 +31,26 @@ class BodyCondition {
             """)
     }
 }
+
+let bodyCondition: BodyCondition = BodyCondition()
+
+let sitUp: Exercise = Exercise(name: "윗몸일으키기", action: {
+    bodyCondition.upperBodyStrength += Int.random(in: 10...20)
+    bodyCondition.fatigue += Int.random(in: 10...20)
+})
+
+let squatt: Exercise = Exercise(name: "스쿼트", action: {
+    bodyCondition.lowerBodyStrength += Int.random(in: 10...20)
+    bodyCondition.fatigue += Int.random(in: 10...20)
+})
+
+let longDistanceRunning: Exercise = Exercise(name: "오래달리기", action: {
+    bodyCondition.muscleEndurance += Int.random(in: 20...30)
+    bodyCondition.upperBodyStrength += Int.random(in: 5...10)
+    bodyCondition.lowerBodyStrength += Int.random(in: 5...10)
+    bodyCondition.fatigue += Int.random(in: 20...30)
+})
+
+let activeRest: Exercise = Exercise(name: "동적휴식", action: {
+    bodyCondition.fatigue -= Int.random(in: 5...10)
+})
