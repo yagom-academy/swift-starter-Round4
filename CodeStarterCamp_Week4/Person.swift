e65272e37ba1ec5f9200d122b96c06c4f576b5ea
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-05-12 16:35:46 +0900

    Person 소스 파일 - doExercise() 추가

--- CodeStarterCamp_Week4/Person.swift
+++ CodeStarterCamp_Week4/Person.swift
@@ -15,4 +15,18 @@ struct Person {
         self.name = name
         self.bodyCondition = bodyCondition
     }
+    
+    func doExercise(for set: Int, routine: Routine) {
+        var currentNumber = 1
+        while currentNumber <= set {
+            if self.bodyCondition.fatigue >= 100 {
+                print("\(self.name)님의 피로도가 \(self.bodyCondition.fatigue)입니다. 회원님이 도망갔습니다.")
+                break
+            }
+            print("--------------")
+            print("\(routine.routineName)을 \(currentNumber)set시작합니다.")
+            bodyCondition.exercise(routine: routine)
+            currentNumber += 1
+        }
+    }
 }
