8c2fa87f9f4a121ff97cfe2a7dc6b9fa509642a8
suyeon <higewnag@naver.com>
2022-05-12 12:20:36 +0900

    add: main 첫번째 commit

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,6 +8,11 @@
 
 import Foundation
 
+let exercises = [sitUp, letBreak, squat, squat, letBreak, longRunning, longRunning]
+let bodyCondition = BodyCondition(upperBodyStrength: 0, lowerBodyStrength: 0, muscularEndurance: 0, fatigue: 0)
+let hellRoutine = Routine(name: "hellRoutine", exercises: exercises)
 
+hellRoutine.routineStart()
+bodyCondition.yourCondition()
 
 
