6274359fe32d642b66b0306feba449a221d35d0e
Wonbi <wonbi@Wonbiui-MacBookPro.local>
2022-05-12 22:57:11 +0900

    Feat: Exercise타입 수정

--- CodeStarterCamp_Week4/Exercise.swift
+++ CodeStarterCamp_Week4/Exercise.swift
@@ -7,7 +7,65 @@
 
 import Foundation
 
-struct Exercise {
-    let name: String
-    let action: (_ bodyCondition: BodyCondition) -> Void
+enum Exercise {
+    case sitUp
+    case squats
+    case deadLift
+    case benchPress
+    case runningLongTime
+    case activeRest
+    
+    var name: String {
+        switch self {
+        case .sitUp:
+            return "윗몸일으키기"
+        case .squats:
+            return "스쿼트"
+        case .deadLift:
+            return "데드리프트"
+        case .benchPress:
+            return "벤치프레스"
+        case .runningLongTime:
+            return "오래달리기"
+        case .activeRest:
+            return "동적휴식"
+        }
+    }
+    
+    var action: (BodyCondition) -> () {
+        switch self {
+        case .sitUp:
+            return {bodyCondition in
+                bodyCondition.upperBodyMuscleStrength += Int.random(in: 10...20)
+                bodyCondition.tiredness += Int.random(in: 10...20)
+            }
+        case .squats:
+            return {bodyCondition in
+                bodyCondition.lowerBodyMuscleStrength += Int.random(in: 20...30)
+                bodyCondition.tiredness += Int.random(in: 10...20)
+            }
+        case .deadLift:
+            return {bodyCondition in
+                bodyCondition.upperBodyMuscleStrength += Int.random(in: 10...20)
+                bodyCondition.lowerBodyMuscleStrength += Int.random(in: 10...20)
+                bodyCondition.tiredness += Int.random(in: 10...20)
+            }
+        case .benchPress:
+            return {bodyCondition in
+                bodyCondition.upperBodyMuscleStrength += Int.random(in: 20...30)
+                bodyCondition.tiredness += Int.random(in: 10...20)
+            }
+        case .runningLongTime:
+            return {bodyCondition in
+                bodyCondition.upperBodyMuscleStrength += Int.random(in: 5...10)
+                bodyCondition.lowerBodyMuscleStrength += Int.random(in: 10...20)
+                bodyCondition.musclerEndurance += Int.random(in: 20...30)
+                bodyCondition.tiredness += Int.random(in: 10...20)
+            }
+        case .activeRest:
+            return {bodyCondition in
+                bodyCondition.tiredness -= Int.random(in: 10...20)
+            }
+        }
+    }
 }
--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,32 +8,12 @@
 
 import Foundation
 
-let sitUp = Exercise(name: "윗몸일으키기") {bodyCondition in
-    bodyCondition.upperBodyMuscleStrength += Int.random(in: 10...20)
-    bodyCondition.tiredness += Int.random(in: 10...20)
-}
-let squats = Exercise(name: "스쿼트") {bodyCondition in
-    bodyCondition.lowerBodyMuscleStrength += Int.random(in: 20...30)
-    bodyCondition.tiredness += Int.random(in: 10...20)
-}
-let deadLift = Exercise(name: "데드리프트") {bodyCondition in
-    bodyCondition.upperBodyMuscleStrength += Int.random(in: 10...20)
-    bodyCondition.lowerBodyMuscleStrength += Int.random(in: 10...20)
-    bodyCondition.tiredness += Int.random(in: 10...20)
-}
-let benchPress = Exercise(name: "벤치프레스") {bodyCondition in
-    bodyCondition.upperBodyMuscleStrength += Int.random(in: 20...30)
-    bodyCondition.tiredness += Int.random(in: 10...20)
-}
-let runningLongTime = Exercise(name: "오래달리기") {bodyCondition in
-    bodyCondition.upperBodyMuscleStrength += Int.random(in: 5...10)
-    bodyCondition.lowerBodyMuscleStrength += Int.random(in: 10...20)
-    bodyCondition.musclerEndurance += Int.random(in: 20...30)
-    bodyCondition.tiredness += Int.random(in: 10...20)
-}
-let activeRest = Exercise(name: "동적휴식") {bodyCondition in
-    bodyCondition.tiredness -= Int.random(in: 10...20)
-}
+let sitUp:Exercise = .sitUp
+let squats:Exercise = .squats
+let deadLift:Exercise = .deadLift
+let benchPress:Exercise = .benchPress
+let runningLongTime:Exercise = .runningLongTime
+let activeRest:Exercise = .activeRest
 
 let hellRoutine = Routine(name: "지옥의루틴", routineArray: [squats, squats, activeRest, benchPress, benchPress, deadLift, deadLift, activeRest, sitUp, sitUp, activeRest, runningLongTime])
 
