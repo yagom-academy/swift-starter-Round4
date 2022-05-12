06bb16035f3fbb86302befe63d54cf6e41203298
Wonbi <wonbi@Wonbiui-MacBookPro.local>
2022-05-12 14:04:07 +0900

    Feat: Exercise타입 추가

---
+++ CodeStarterCamp_Week4/Exercise.swift
@@ -0,0 +1,13 @@
+//
+//  Exercise.swift
+//  CodeStarterCamp_Week4
+//
+//  Created by Wonbi on 2022/05/12.
+//
+
+import Foundation
+
+struct Exercise {
+    let name: String
+    let action: (_ bodyCondition: BodyCondition) -> Void
+}
