6e97ae04dd2ef6705d9ea8b654d0ca850048f6ca
hansm <hanmix2003@naver.com>
2022-05-13 06:20:47 +0900

    Refactor: Routine 타입 내 startExercise 함수의 기능을 삭제
    - startExercise 함수의 기능을 Person 타입의 exercise 함수와 통합함

--- CodeStarterCamp_Week4/Routine.swift
+++ CodeStarterCamp_Week4/Routine.swift
@@ -10,14 +10,4 @@ import Foundation
 struct Routine {
     let name: String
     var routineArray: Array<Exercise>
- 
-    func startExercise(with bodyConditon: BodyCondition, overTiredness: Int) throws {
-        for exercise in routineArray {
-            print("\(exercise.name)")
-            exercise.action(bodyConditon)
-            if bodyConditon.tiredness > overTiredness {
-                throw FitnessCenterError.runAwayMember
-            }
-        }
-    }
 }
