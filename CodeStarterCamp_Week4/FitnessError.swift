1bca1b70cd267c06a15ae6b716301a10a61db295
홍승주 <hongseungju@hongseungjuui-MacBookAir.local>
2022-05-13 21:10:06 +0900

    FitnessError

--- CodeStarterCamp_Week4/FitnessError.swift
+++ CodeStarterCamp_Week4/FitnessError.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+enum FitnessError: String, Error {
+    case overFatigue = "피로도가 100을 넘었습니다."
+    case noGoal
+    case wrongInput = "올바른 입력을 해주세요"
+    case emptyMember = "회원이 아닙니다."
+    case etcError = "기타에러"
+}
