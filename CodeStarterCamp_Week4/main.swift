82689b4065f589f7ba84617acc87a7da4b880dff
홍승주 <hongseungju@hongseungjuui-MacBookAir.local>
2022-05-13 21:11:17 +0900

    main

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -7,10 +7,23 @@
 //
 
 import Foundation
+/*
 
-var person = BodyCondition(upperBodyMuscleStrength: 0, lowerBodyMuscleStrength: 0, muscleEndurance: 0, fatigue: 0)
 let exercise: Array<Exercise> = [sitUp, activeRest, yoga]
 var personRoutine = Routine(routineName: "hellroutine", exercises: exercise)
 
-personRoutine.printRoutine()
 person.checkBodyCondition()
+ */
+let hellRoutineExercise: Array<Exercise> = [sitUp, sitUp, activeRest, legRaise, legRaise, activeRest, burpee, burpee]
+var hellRoutine = Routine(routineName: "hellRoutine", exercises: hellRoutineExercise)
+
+let ohMyGodRoutineExercise: Array<Exercise> = [sitUp, squats, run, sitUp, squats, run, activeRest, legRaise, legRaise, burpee, run, activeRest, yoga]
+var ohMyGodRoutine = Routine(routineName: "ohMyGodRoutine", exercises: ohMyGodRoutineExercise)
+
+let person = BodyCondition(upperBodyMuscleStrength: 10, lowerBodyMuscleStrength: 10, muscleEndurance: 10, fatigue: 3)
+
+var yagom = Person(name: "yagom", bodyCondition: person)
+
+var yagomFitnessCenter = FitnessCenter(name: "야곰", routine: [hellRoutine, ohMyGodRoutine])
+
+try yagomFitnessCenter.runRoutine()
