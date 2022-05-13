fe30b063e2c0df0d68dc889fda9aa447d1d0af50
홍승주 <hongseungju@hongseungjuui-MacBookAir.local>
2022-05-13 21:09:18 +0900

    Person

--- CodeStarterCamp_Week4/Person.swift
+++ CodeStarterCamp_Week4/Person.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+class Person {
+    let name: String
+    var bodyCondition: BodyCondition
+    
+    init(name: String, bodyCondition: BodyCondition) {
+        self.name = name
+        self.bodyCondition = bodyCondition
+    }
+    
+    func exercise(for set: Int, routine: Routine) throws {
+        print("\(routine.routineName)을 \(set)set 시작합니다.")
+        for _ in 1...set {
+            for number in 0..<routine.exercises.count {
+                routine.exercises[number].action()
+                if bodyCondition.fatigue >= 100 {
+                    throw FitnessError.overFatigue
+                }
+                print(routine.exercises[number].name)
+            }
+        }
+    }
+}
