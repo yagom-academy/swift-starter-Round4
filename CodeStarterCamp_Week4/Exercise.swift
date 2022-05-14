ca760d9ee67bdb48a6df15bacc92c15fdca62207
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-05-14 17:58:48 +0900

    Exercise 소스 파일 - 운동 인스턴스 코드 수정

--- CodeStarterCamp_Week4/Exercise.swift
+++ CodeStarterCamp_Week4/Exercise.swift
@@ -8,54 +8,54 @@
 import Foundation
 
 struct Exercise {
-        let name: String
-        let action: (BodyCondition) -> Void
+    let name: String
+    let action: (BodyCondition) -> Void
 }
 
 // 상체운동
-let sitUp = Exercise(name: "윗몸일으키기") { (person: BodyCondition) in
+let sitUp = Exercise(name: "윗몸일으키기") { person in
     person.upperBodyStrength += Int.random(in: 10...20)
     person.fatigue += Int.random(in: 10...20)
 }
 
-let deadLift = Exercise(name: "데드리프트") {(person: BodyCondition) in
+let deadLift = Exercise(name: "데드리프트") { person in
     person.upperBodyStrength += Int.random(in: 10...20)
     person.lowerBodyStrength += Int.random(in: 10...20)
     person.fatigue += Int.random(in: 10...20)
 }
 
-let plank = Exercise(name: "플랭크") { (person: BodyCondition) in
+let plank = Exercise(name: "플랭크") { person in
     person.upperBodyStrength += Int.random(in: 10...20)
     person.fatigue += Int.random(in: 10...20)
 }
 
-let barbellRow = Exercise(name: "바벨로우") { (person: BodyCondition) in
+let barbellRow = Exercise(name: "바벨로우") { person in
     person.upperBodyStrength += Int.random(in: 10...20)
     person.fatigue += Int.random(in: 10...20)
 }
 
-let cablePullover = Exercise(name: "케이블 풀오버") { (person: BodyCondition) in
+let cablePullover = Exercise(name: "케이블 풀오버") { person in
     person.upperBodyStrength += Int.random(in: 10...20)
     person.fatigue += Int.random(in: 10...20)
 }
 
 // 하체운동
-let squat = Exercise(name: "스쿼트") { (person: BodyCondition) in
+let squat = Exercise(name: "스쿼트") { person in
     person.lowerBodyStrength += Int.random(in: 20...30)
     person.fatigue += Int.random(in: 10...20)
 }
 
-let lunge = Exercise(name: "런지") { (person: BodyCondition) in
+let lunge = Exercise(name: "런지") { person in
     person.lowerBodyStrength += Int.random(in: 20...30)
     person.muscularEndurance += Int.random(in: 10...15)
 }
 
-let legExtension = Exercise(name: "레그 익스텐션") { (person: BodyCondition) in
+let legExtension = Exercise(name: "레그 익스텐션") { person in
     person.lowerBodyStrength += Int.random(in: 20...30)
 }
 
 // 유산소운동
-let longRun = Exercise(name: "오래달리기") { (person: BodyCondition) in
+let longRun = Exercise(name: "오래달리기") { person in
     person.muscularEndurance += Int.random(in: 20...30)
     person.upperBodyStrength += Int.random(in: 5...10)
     person.lowerBodyStrength += Int.random(in: 5...10)
@@ -63,6 +63,6 @@ let longRun = Exercise(name: "오래달리기") { (person: BodyCondition) in
 }
 
 // 동적휴식
-let activeRest = Exercise(name: "동적휴식") { (person: BodyCondition) in
+let activeRest = Exercise(name: "동적휴식") { person in
     person.fatigue -= Int.random(in: 5...10)
 }
