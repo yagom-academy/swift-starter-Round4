5158ba2f16c3a21cda6d582b46928b7d77071033
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-05-15 15:25:12 +0900

    Person 소스 파일 수정 - 이니셜라이저 삭제, doExercise() 메서드 수정

--- CodeStarterCamp_Week4/Person.swift
+++ CodeStarterCamp_Week4/Person.swift
@@ -11,22 +11,15 @@ struct Person {
     var name: String
     var bodyCondition: BodyCondition
     
-    init(name: String, bodyCondition: BodyCondition) {
-        self.name = name
-        self.bodyCondition = bodyCondition
-    }
-    
     func doExercise(for set: Int, routine: Routine) {
-        var currentNumber = 1
-        while currentNumber <= set {
-            if self.bodyCondition.fatigue >= 100 {
-                print("\(self.name)님의 피로도가 \(self.bodyCondition.fatigue)입니다. 회원님이 도망갔습니다.")
-                break
-            }
+        for round in 1...set {
+                guard bodyCondition.fatigue < 100 else {
+                    print("\(name)님의 피로도가 \(bodyCondition.fatigue)입니다. 회원님이 도망갔습니다.")
+                    break
+                }
             print("--------------")
-            print("\(routine.routineName)을 \(currentNumber)set시작합니다.")
+            print("\(routine.routineName)을 \(round)set시작합니다.")
             bodyCondition.exercise(routine: routine)
-            currentNumber += 1
         }
     }
 }
