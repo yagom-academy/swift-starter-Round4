4665cc7e8e88e459961ee52a2187e9a2c77f7fb8
OneE <top9712@daum.net>
2022-05-19 18:41:09 +0900

    feat: bodyCondition 사람별로 설정하고 수치 다르게 하기 완성

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -1,34 +1,27 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week4
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
 import Foundation
 
-class BodyCondition {
+struct BodyCondition {
     var upperBodyStrength: Int
     var lowerBodyStrength: Int
     var muscleEndurance: Int
     var fatigue: Int
-
-    init(upperBodyStrength: Int = 0, lowerBodyStrength: Int = 0, muscleEndurance: Int = 0, fatigue: Int = 0) {
+    
+    init(upperBodyStrength: Int, lowerBodyStrength: Int, muscleEndurance: Int, fatigue: Int) {
         self.upperBodyStrength = upperBodyStrength
         self.lowerBodyStrength = lowerBodyStrength
         self.muscleEndurance = muscleEndurance
         self.fatigue = fatigue
     }
     
-    func nowCondition(myRoutine: Routine) {
-        myRoutine.tellMyExerciseRoutine()
+    func tellCondition(afterRoutine: Routine) {
+        afterRoutine.doExercise(by: self)
         print("""
             -----------------------
             현재의 컨디션은 다음과 같습니다.
-            상체근력: \(self.upperBodyStrength)
-            하체근력: \(self.lowerBodyStrength)
-            근지구력: \(self.muscleEndurance)
-            피로도: \(self.fatigue)
+            상체근력: \(bodyCondition.upperBodyStrength)
+            하체근력: \(bodyCondition.lowerBodyStrength)
+            근지구력: \(bodyCondition.muscleEndurance)
+            피로도: \(bodyCondition.fatigue)
             """)
     }
 }
@@ -48,7 +41,6 @@ class Routine {
     }
     
     func tellMyExerciseRoutine() {
-        self.doExercise()
         print("""
             -----------------------
             \(self.name)을 시작합니다.
@@ -60,7 +52,8 @@ class Routine {
         }
     }
     
-    func doExercise() {
+    func doExercise(by who: BodyCondition) {
+        tellMyExerciseRoutine()
         for didExercise in exercise {
             let exercised = didExercise.action
             exercised()
@@ -68,29 +61,32 @@ class Routine {
     }
 }
 
-let bodyCondition: BodyCondition = BodyCondition()
+var bodyCondition:BodyCondition = BodyCondition(upperBodyStrength: 0, lowerBodyStrength: 0, muscleEndurance: 0, fatigue: 0)
 
-let sitUp: Exercise = Exercise(name: "윗몸일으키기", action: {
+let sitUp = Exercise(name: "윗몸일으키기", action: {
     bodyCondition.upperBodyStrength += Int.random(in: 10...20)
     bodyCondition.fatigue += Int.random(in: 10...20)
 })
 
-let squatt: Exercise = Exercise(name: "스쿼트", action: {
+let squatt = Exercise(name: "스쿼트", action: {
     bodyCondition.lowerBodyStrength += Int.random(in: 10...20)
     bodyCondition.fatigue += Int.random(in: 10...20)
 })
 
-let longDistanceRunning: Exercise = Exercise(name: "오래달리기", action: {
+let longDistanceRunning = Exercise(name: "오래달리기", action: {
     bodyCondition.muscleEndurance += Int.random(in: 20...30)
     bodyCondition.upperBodyStrength += Int.random(in: 5...10)
     bodyCondition.lowerBodyStrength += Int.random(in: 5...10)
     bodyCondition.fatigue += Int.random(in: 20...30)
 })
 
-let activeRest: Exercise = Exercise(name: "동적휴식", action: {
+let activeRest = Exercise(name: "동적휴식", action: {
     bodyCondition.fatigue -= Int.random(in: 5...10)
 })
 
-let hellRoutine: Routine = Routine(name: "Hell-Routine", exercise: [sitUp, sitUp, squatt, squatt, activeRest, longDistanceRunning, longDistanceRunning])
+let hellRoutine = Routine(name: "Hell-Routine", exercise: [sitUp, sitUp, squatt, squatt, activeRest, longDistanceRunning, longDistanceRunning])
+var oneECondition: BodyCondition = BodyCondition(upperBodyStrength: 10, lowerBodyStrength: 30, muscleEndurance: 40, fatigue: 10)
 
-bodyCondition.nowCondition(myRoutine: hellRoutine)
+bodyCondition.tellCondition(afterRoutine: hellRoutine)
+bodyCondition = oneECondition
+oneECondition.tellCondition(afterRoutine: hellRoutine)
