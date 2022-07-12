4adb6aaea552b753c9045bf2dbd95d4a17a87e10
seoyounghan <happyunbd364@naver.com>
2022-07-13 01:19:57 +0900

    add step1 Bodycondition.changeCondition

--- CodeStarterCamp_Week4/step1.swift
+++ CodeStarterCamp_Week4/step1.swift
@@ -7,20 +7,30 @@
 
 import Foundation
 
-struct BodyCondition {
-    var upperBodyStrength: Int
-    var lowerBodyStrength: Int
-    var muscularEndurance: Int
-    var fatigue: Int
+class BodyCondition {
+    var upperBodyStrength: Int = 0
+    var lowerBodyStrength: Int = 0
+    var muscularEndurance: Int = 0
+    var fatigue: Int = 0
     
+    func changeCondition(_ conditionName: String, degree: Int) {
+        switch (conditionName) {
+        case "상체근력": self.upperBodyStrength += degree
+        case "하체근력": self.lowerBodyStrength += degree
+        case "근지구력": self.muscularEndurance += degree
+        case "피로도": self.fatigue += degree
+        default:
+            <#code#>
+        }
+    }
 }
 
 struct Activity {
-        let name: String
-        let action: (BodyCondition) -> Void
+    let name: String
+    let action: (BodyCondition) -> Void
 }
 
-
-let 윗몸일으키기: Activity = Activity(name: "윗몸일으키기", action: { bodyCondition in
-        //BodyCondition에 영향을 주는 클로저 내용
+let 윗몸일으키기: Activity = Activity(name: "윗몸일으키기", action: { (bodyCondition: BodyCondition) in
+    bodyCondition.changeCondition("상체근력", degree: Int.random(in: 10...20))
+    bodyCondition.changeCondition("하체근력", degree: Int.random(in: 10...20))
 })
