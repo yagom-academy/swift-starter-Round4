80524cbc3cad0c31f9e0c00bbc1ec4c99c9e61c3
jonghancha <jonghancha@gmail.com>
2022-07-13 17:41:08 +0900

    feat: 랜덤숫자생성함수 추가, 클로저 생략, 활동시작용 함수 추가시작

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,27 +8,50 @@
 
 import Foundation
 
-struct BodyCondition {
+class BodyCondition {
     var upperBodyStrength: Int
     var lowerBodyStrength: Int
     var muscularEndurance: Int
     var fatigue: Int
     
-    mutating func buildUpperBodyStrength(from low: Int, to high: Int) {
-        let randomNumber: Int
-        randomNumber = Int.random(in: low...high)
-        print("randomNumber : + \(randomNumber)")
+    init(upperBodyStrength: Int, lowerBodyStrength: Int, muscularEndurance: Int, fatigue: Int) {
+        self.upperBodyStrength = upperBodyStrength
+        self.lowerBodyStrength = lowerBodyStrength
+        self.muscularEndurance = muscularEndurance
+        self.fatigue = fatigue
+    }
+    
+    func buildUpperBodyStrength(_ randomNumber: Int) {
         self.upperBodyStrength += randomNumber
-        print("current upperBOdyStrength : \(self.upperBodyStrength)")
+        print("상체근력이 \(randomNumber) 상승합니다.")
     }
-    mutating func buildLowerBodyStrength() {
-        
+    func buildLowerBodyStrength(_ randomNumber: Int) {
+        self.lowerBodyStrength += randomNumber
+        print("하체근력이 \(randomNumber) 상승합니다.")
     }
-    mutating func buildMuscularEndurance() {
-        
+    func buildMuscularEndurance(_ randomNumber: Int) {
+        self.muscularEndurance += randomNumber
+        print("근지구력이 \(randomNumber) 상승합니다.")
     }
-    mutating func reduceFatigue() {
-        
+    func increaseFatigue(_ randomNumber: Int) {
+        self.fatigue += randomNumber
+        print("피로도가 \(randomNumber) 상승합니다.")
+    }
+    func reduceFatigue(_ randomNumber: Int) {
+        self.fatigue -= randomNumber
+        print("피로도가 \(randomNumber) 하락합니다.")
+    }
+    
+    func showCurrentCondition() {
+        print("""
+------------------------
+현재의 컨디션은 다음과 같습니다.
+상체근력: \(upperBodyStrength)
+하체근력: \(lowerBodyStrength)
+근지구력: \(muscularEndurance)
+피로도: \(fatigue)
+------------------------
+""")
     }
 }
 
@@ -39,15 +62,37 @@ struct Activity {
 }
 var myBodyCondition: BodyCondition = BodyCondition(upperBodyStrength: 50, lowerBodyStrength: 50, muscularEndurance: 50, fatigue: 30)
 
-
+func random(_ from: Int, _ to: Int) -> Int {
+    return Int.random(in: from...to)
+}
 //예시 활동
-let 윗몸일으키기: Activity = Activity(name: "윗몸일으키기",
-                                                         action: { bodyCondition in
-    print("-----------START STEP1-----------\n")
-    print("<<<<<<In 윗몸일으키기>>>>>>")
-        //BodyCondition에 영향을 주는 클로저 내용
-    myBodyCondition.buildUpperBodyStrength(from: 10, to: 20)
-    myBodyCondition.reduceFatigue()
+let 윗몸일으키기: Activity = Activity(name: "윗몸일으키기") {
+    $0.buildUpperBodyStrength(random(10, 20))
+    $0.increaseFatigue(random(10,20))
+    $0.showCurrentCondition()
+}
+
+let 스쿼트: Activity = Activity(name: "스쿼트") {
+    $0.buildLowerBodyStrength(random(20, 30))
+    $0.increaseFatigue(random(10, 20))
+    $0.showCurrentCondition()
+}
+let 오래달리기: Activity = Activity(name: "오래달리기") {
+    $0.buildMuscularEndurance(random(20, 30))
+    $0.buildUpperBodyStrength(random(5, 10))
+    $0.buildLowerBodyStrength(random(5, 10))
+    $0.increaseFatigue(random(20, 30))
+    $0.showCurrentCondition()
+}
+let 동적휴식: Activity = Activity(name: "동적휴식") {
+    $0.buildLowerBodyStrength(random(5, 10))
+    $0.showCurrentCondition()
+}
+
+func workOut(_ activity: Activity) {
     
-})
+}
+
+
 윗몸일으키기.action(myBodyCondition)
+스쿼트.action(myBodyCondition)
