33f5867aed44d95301c468d2dde672d1c2ae756f
codesparrow12 <kanghd97@gmail.com>
2022-07-14 17:41:09 +0900

    윗몸일으키기, 스쿼트, 오래달리기, 동적휴식 구현

--- step1/main.swift
+++ step1/main.swift
@@ -17,3 +17,46 @@ struct Activity {
     let name: String
     let action: (BodyCondition) -> Void
 }
+
+let sitUp: Activity = Activity(name: "윗몸일으키기", action: { bodyCondition in
+    let randomUpperBodyStrengthGrowth = Int.random(in: 10...20)
+    let randomFatigueGrowth = Int.random(in: 10...20)
+    print("""
+    상체근력이 \(randomUpperBodyStrengthGrowth) 상승합니다.
+    피로도가 \(randomFatigueGrowth) 상승합니다.
+    """)
+    codeSparrow.upperBodyStrength += randomUpperBodyStrengthGrowth
+    codeSparrow.fatigue += randomFatigueGrowth
+})
+let squat: Activity = Activity(name: "스쿼트", action: { bodyCondition in
+    let randomLowerBodyStrengthGrowth = Int.random(in: 20...30)
+    let randomFatigueGrowth = Int.random(in: 10...20)
+    print("""
+    하체근력이 \(randomLowerBodyStrengthGrowth) 상승합니다.
+    피로도가 \(randomFatigueGrowth) 상승합니다.
+    """)
+    codeSparrow.lowerBodyStrength += randomLowerBodyStrengthGrowth
+    codeSparrow.fatigue += randomFatigueGrowth
+})
+let running: Activity = Activity(name: "오래달리기", action: { bodyCondition in
+    let randomMuscularEnduranceGrowth = Int.random(in: 20...30)
+    let randomUpperBodyStrengthGrowth = Int.random(in: 5...10)
+    let randomLowerBodyStrengthGrowth = Int.random(in: 5...10)
+    let randomFatigueGrowth = Int.random(in: 20...30)
+    print("""
+    근지구력이 \(randomMuscularEnduranceGrowth) 상승합니다.
+    상체근력이 \(randomUpperBodyStrengthGrowth) 상승합니다.
+    하체근력이 \(randomLowerBodyStrengthGrowth) 상승합니다.
+    피로도가 \(randomFatigueGrowth) 상승합니다.
+    """)
+    codeSparrow.muscularEndurance += randomMuscularEnduranceGrowth
+    codeSparrow.upperBodyStrength += randomUpperBodyStrengthGrowth
+    codeSparrow.lowerBodyStrength += randomLowerBodyStrengthGrowth
+    codeSparrow.fatigue += randomFatigueGrowth
+})
+let activeRest: Activity = Activity(name: "동적휴식", action: { bodyCondition in
+    let randomFatigueReduction = Int.random(in: 5...10)
+    print("피로도가 \(randomFatigueReduction) 하락합니다.")
+    codeSparrow.fatigue -= randomFatigueReduction
+})
+
