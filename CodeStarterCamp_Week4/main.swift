65804fa7170e2e4661b82206d6b6f6213444103c
jerry <gwd0311@naver.com>
2022-07-14 14:22:17 +0900

    STEP2

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,10 +8,19 @@
 
 import Foundation
 
+enum RoutineError: Error {
+    case fatigueOverflow
+    case wrongInput
+}
+
 class BodyCondition {
-    var 상체근력: Int = 0 {
+    
+}
+
+class 상체근력: BodyCondition {
+    var value: Int = 0 {
         didSet(oldNum) {
-            let num = self.상체근력 - oldNum
+            let num = self.value - oldNum
             if num > 0 {
                 print("상체근력이 \(num) 상승합니다.")
             } else if num == 0 {
@@ -22,9 +31,15 @@ class BodyCondition {
         }
     }
     
-    var 하체근력: Int = 0 {
+    func adjustValue(_ minimumValue: Int, _ maximumValue: Int) {
+        self.value += Int.random(in: minimumValue...maximumValue)
+    }
+}
+
+class 하체근력: BodyCondition {
+    var value: Int = 0 {
         didSet(oldNum) {
-            let num = self.하체근력 - oldNum
+            let num = self.value - oldNum
             if num > 0 {
                 print("하체근력이 \(num) 상승합니다.")
             } else if num == 0 {
@@ -34,10 +49,16 @@ class BodyCondition {
             }
         }
     }
-    
-    var 근지구력: Int = 0 {
+
+    func adjustValue(_ minimumValue: Int, _ maximumValue: Int) {
+        self.value += Int.random(in: minimumValue...maximumValue)
+    }
+}
+
+class 근지구력: BodyCondition {
+    var value: Int = 0 {
         didSet(oldNum) {
-            let num = self.근지구력 - oldNum
+            let num = self.value - oldNum
             if num > 0 {
                 print("근지구력이 \(num) 상승합니다.")
             } else if num == 0 {
@@ -47,10 +68,16 @@ class BodyCondition {
             }
         }
     }
-    
-    var 피로도: Int = 0 {
+            
+    func adjustValue(_ minimumValue: Int, _ maximumValue: Int) {
+        self.value += Int.random(in: minimumValue...maximumValue)
+    }
+}
+
+class 피로도: BodyCondition {
+    var value: Int = 0 {
         didSet(oldNum) {
-            let num = self.피로도 - oldNum
+            let num = self.value - oldNum
             if num > 0 {
                 print("피로도가 \(num) 상승합니다.")
             } else if num == 0 {
@@ -61,58 +88,49 @@ class BodyCondition {
         }
     }
     
-    func 상체근력수치변경(_ num: Int) {
-        상체근력 += num
-    }
-    func 하체근력수치변경(_ num: Int) {
-        하체근력 += num
-    }
-    func 근지구력수치변경(_ num: Int) {
-        근지구력 += num
-    }
-    func 피로도수치변경(_ num: Int) {
-        피로도 += num
-    }
-    
-    func printCondition() {
-        print("""
-        현재의 컨디션은 다음과 같습니다.
-        상체근력: \(상체근력)
-        하체근력: \(하체근력)
-        근지구력: \(근지구력)
-        피로도: \(피로도)
-        --------------
-        """)
+    func adjustValue(_ minimumValue: Int, _ maximumValue: Int) {
+        self.value += Int.random(in: minimumValue...maximumValue)
     }
 }
 
 struct Activity {
-        let name: String
-        let action: (BodyCondition) -> Void
+    let name: String
+    let action: (BodyCondition) -> Void
 }
 
-var bodyCondition = BodyCondition()
-
-let 윗몸일으키기: Activity = Activity(name: "윗몸일으키기", action: { bodyCondition in
-    bodyCondition.상체근력수치변경(Int.random(in: 10...20))
-    bodyCondition.피로도수치변경(Int.random(in: 10...20))
-})
-
-let 스쿼트: Activity = Activity(name: "스쿼트", action: { bodyCondition in
-    bodyCondition.하체근력수치변경(Int.random(in: 20...30))
-    bodyCondition.피로도수치변경(Int.random(in: 10...20))
-})
-
-let 오래달리기: Activity = Activity(name: "오래달리기", action: { bodyCondition in
-    bodyCondition.근지구력수치변경(Int.random(in: 20...30))
-    bodyCondition.상체근력수치변경(Int.random(in: 5...10))
-    bodyCondition.하체근력수치변경(Int.random(in: 5...10))
-    bodyCondition.피로도수치변경(Int.random(in: 20...30))
-})
-
-let 동적휴식: Activity = Activity(name: "동적휴식", action: { bodyCondition in
-    bodyCondition.피로도수치변경(Int.random(in: (-10)...(-5)))
-})
+class Routine {
+    let name: String
+    let activities: Array<Activity>
+    var fatigueOverflow: Bool {
+        return fatigue.value > 100
+    }
+    
+    init(_ name: String, _ activities: Array<Activity>) {
+        self.name = name
+        self.activities = activities
+    }
+    
+    func doRoutine() throws {
+        let input = readLine() ?? ""
+        
+        guard Int(input) ?? 0 > 0 else {
+            throw RoutineError.wrongInput
+        }
+        print("--------------")
+        for i in 1...(Int(input) ?? 1) {
+            print("\(i) 번째 \(self.name)을(를) 시작합니다.")
+            for act in self.activities {
+                print("<<\(act.name)을(를) 시작합니다>>")
+                act.action(bodyCondition)
+                print("--------------")
+                guard fatigueOverflow == false else {
+                    throw RoutineError.fatigueOverflow
+                }
+            }
+        }
+        printCondition()
+    }
+}
 
 func doActivity(_ act: Activity) {
     print("<<\(act.name)을(를) 시작합니다>>")
@@ -120,8 +138,60 @@ func doActivity(_ act: Activity) {
     print("--------------")
 }
 
-doActivity(윗몸일으키기)
-doActivity(스쿼트)
-doActivity(오래달리기)
-doActivity(동적휴식)
-bodyCondition.printCondition()
+func printCondition() {
+    print("""
+    현재의 컨디션은 다음과 같습니다.
+    상체근력: \(upperBodyStrength.value)
+    하체근력: \(lowerBodyStrength.value)
+    근지구력: \(muscularEndurance.value)
+    피로도: \(fatigue.value)
+    --------------
+    """)
+}
+
+func doRoutineCourse(_ routine: Routine) throws {
+    print("루틴을 몇 번 반복할까요?")
+    while true {
+        do {
+            try routine.doRoutine()
+            return
+        } catch RoutineError.fatigueOverflow {
+            print("피로도가 100 이상입니다. 루틴을 중단합니다.")
+            printCondition()
+            return
+        } catch RoutineError.wrongInput {
+            print("잘못된 입력 형식입니다. 다시 입력해주세요.")
+        }
+    }
+}
+
+var bodyCondition = BodyCondition()
+var upperBodyStrength = 상체근력()
+var lowerBodyStrength = 하체근력()
+var muscularEndurance = 근지구력()
+var fatigue = 피로도()
+
+let 윗몸일으키기: Activity = Activity(name: "윗몸일으키기", action: { bodyCondition in
+    upperBodyStrength.adjustValue(10, 20)
+    fatigue.adjustValue(10, 20)
+})
+
+let 스쿼트: Activity = Activity(name: "스쿼트", action: { bodyCondition in
+    lowerBodyStrength.adjustValue(20, 30)
+    fatigue.adjustValue(10, 20)
+})
+
+let 오래달리기: Activity = Activity(name: "오래달리기", action: { bodyCondition in
+    muscularEndurance.adjustValue(20, 30)
+    upperBodyStrength.adjustValue(5, 10)
+    lowerBodyStrength.adjustValue(5, 10)
+    fatigue.adjustValue(20, 30)
+})
+
+let 동적휴식: Activity = Activity(name: "동적휴식", action: { bodyCondition in
+    fatigue.adjustValue(-10, -5)
+})
+
+var routine = Routine("hellRoutine", [윗몸일으키기, 스쿼트, 오래달리기, 동적휴식])
+
+try doRoutineCourse(routine)
