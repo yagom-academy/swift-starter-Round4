ad4a145318e3333cefa12853c64527b1e40e078c
sunny <maengseon-a@maengseon-aui-MacBookAir.local>
2022-07-18 20:28:53 +0900

    함수내용->프로퍼티감시자, 클로저 반환타입수정(->Void)

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -3,9 +3,9 @@ import Foundation
 var sunnyBodyCondition: BodyCondition = BodyCondition(upeerBodyStrength: 100, lowerBodyStrength: 100, muscularEndurance: 100, fatigue: 10)
 var hyeokBodyCondition: BodyCondition = BodyCondition(upeerBodyStrength: 200, lowerBodyStrength: 200, muscularEndurance: 200, fatigue: 20)
 
-sunnyBodyCondition = sitUp.action(sunnyBodyCondition)
-sunnyBodyCondition = squats.action(sunnyBodyCondition)
-sunnyBodyCondition = longRun.action(sunnyBodyCondition)
-sunnyBodyCondition = activeRest.action(sunnyBodyCondition)
-hyeokBodyCondition = sitUp.action(hyeokBodyCondition)
+sitUp.action(sunnyBodyCondition)
+squats.action(sunnyBodyCondition)
+longRun.action(sunnyBodyCondition)
+activeRest.action(sunnyBodyCondition)
+sitUp.action(hyeokBodyCondition)
 
