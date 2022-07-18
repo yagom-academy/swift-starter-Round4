dc861b42cdb87fa87dc89ece22b82e4312a64915
codesparrow12 <kanghd97@gmail.com>
2022-07-18 13:11:06 +0900

    Activity.action의 반환값을 BodyCondition으로 변경

--- step1/main.swift
+++ step1/main.swift
@@ -15,10 +15,10 @@ struct BodyCondition {
 }
 struct Activity {
     let name: String
-    let action: (BodyCondition) -> Void
+    let action: (BodyCondition) -> BodyCondition
 }
 
-let sitUp: Activity = Activity(name: "윗몸일으키기", action: { bodyCondition in
+let sitUp: Activity = Activity(name: "윗몸일으키기", action: { bodyCondition -> BodyCondition in
     let randomUpperBodyStrengthGrowth = Int.random(in: 10...20)
     let randomFatigueGrowth = Int.random(in: 10...20)
     print("""
@@ -30,9 +30,10 @@ let sitUp: Activity = Activity(name: "윗몸일으키기", action: { bodyConditi
     var currentFatigue = bodyCondition.fatigue
     currentUpperBodyStrength += randomUpperBodyStrengthGrowth
     currentFatigue += randomFatigueGrowth
+    return bodyCondition
 })
 
-let squat: Activity = Activity(name: "스쿼트", action: { bodyCondition in
+let squat: Activity = Activity(name: "스쿼트", action: { bodyCondition -> BodyCondition in
     let randomLowerBodyStrengthGrowth = Int.random(in: 20...30)
     let randomFatigueGrowth = Int.random(in: 10...20)
     print("""
@@ -44,9 +45,10 @@ let squat: Activity = Activity(name: "스쿼트", action: { bodyCondition in
     var currentFatigue = bodyCondition.fatigue
     currentLowerBodyStrength += randomLowerBodyStrengthGrowth
     currentFatigue += randomFatigueGrowth
+    return bodyCondition
 })
 
-let running: Activity = Activity(name: "오래달리기", action: { bodyCondition in
+let running: Activity = Activity(name: "오래달리기", action: { bodyCondition -> BodyCondition in
     let randomMuscularEnduranceGrowth = Int.random(in: 20...30)
     let randomUpperBodyStrengthGrowth = Int.random(in: 5...10)
     let randomLowerBodyStrengthGrowth = Int.random(in: 5...10)
@@ -62,23 +64,27 @@ let running: Activity = Activity(name: "오래달리기", action: { bodyConditio
     var currentUpperBodyStrength = bodyCondition.upperBodyStrength
     var currentLowerBodyStrength = bodyCondition.lowerBodyStrength
     var currentFatigue = bodyCondition.fatigue
+    
     currentMuscularEndurance += randomMuscularEnduranceGrowth
     currentUpperBodyStrength += randomUpperBodyStrengthGrowth
     currentLowerBodyStrength += randomLowerBodyStrengthGrowth
     currentFatigue += randomFatigueGrowth
+    return bodyCondition
 })
 
-let activeRest: Activity = Activity(name: "동적휴식", action: { bodyCondition in
+let activeRest: Activity = Activity(name: "동적휴식", action: { bodyCondition -> BodyCondition in
     let randomFatigueReduction = Int.random(in: 5...10)
     print("피로도가 \(randomFatigueReduction) 하락합니다.")
     
     var currentFatigue = bodyCondition.fatigue
     currentFatigue -= randomFatigueReduction
+    return bodyCondition
 })
 
-func workOut(_ activity: Activity) {
+func workOut(_ activity: Activity, _ bodyCondition: BodyCondition) {
     print("<<\(activity.name)을(를) 시작합니다>>")
-    activity.action(codeSparrow)
+    activity.action(bodyCondition)
+    
     print("--------------")
 }
 
@@ -95,3 +101,5 @@ func checkBodyCondition(_ name: BodyCondition) {
 
 var codeSparrow: BodyCondition = BodyCondition(upperBodyStrength: 70, lowerBodyStrength: 50, muscularEndurance: 30, fatigue: 0)
 
+workOut(sitUp, codeSparrow)
+checkBodyCondition(codeSparrow)
