f3822f859a070d178394254680b5517b687f130e
Gundy <korea1880@naver.com>
2022-07-19 18:20:02 +0900

    메소드 내용 변경

--- Activity.swift
+++ Activity.swift
@@ -11,19 +11,23 @@ struct Activity {
     let name: String
     let action: (BodyCondition) -> Void
     
-    func growth(minimum: Int, maximum: Int, current: Int) -> Int {
-        guard maximum > minimum  else { return 0 }
-            return Int.random(in: minimum...maximum) - (current / (5000 / maximum))
+    func upperGrowth(condition: BodyCondition, min: Int, max: Int) {
+        condition.upperBodyStrength += Int.random(in: min...max) - (condition.upperBodyStrength / (5000 / max))
     }
     
-    func changeFatigue(minimum: Int, maximum: Int, rest: Bool = false, current: Int) -> Int {
-        guard maximum > minimum else { return 0 }
-        var fatigue = current / 10
-        if rest == false {
-            fatigue += Int.random(in: minimum...maximum)
+    func lowerGrowth(condition: BodyCondition, min: Int, max: Int) {
+        condition.lowerBodyStrength += Int.random(in: min...max) - (condition.lowerBodyStrength / (5000 / max))
+    }
+    
+    func enduranceGrowth(condition: BodyCondition, min: Int, max: Int) {
+        condition.muscularEndurance += Int.random(in: min...max) - (condition.muscularEndurance / (10000 / max))
+    }
+    
+    func changeFatigue(condition: BodyCondition, min: Int, max: Int, rest: Bool = false) {
+        if rest != true {
+            condition.fatigue += Int.random(in: min...max) + (condition.fatigue / 10)
         } else {
-            fatigue -= Int.random(in: minimum...maximum)
+            condition.fatigue += ((-1)*(Int.random(in: min...max))) + (condition.fatigue / 20)
         }
-        return fatigue
     }
 }
--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -11,24 +11,24 @@ import Foundation
 let gundy: BodyCondition = .init(name: "건디")
 
 let sitUp: Activity = .init(name: "윗몸일으키기", action: {
-    $0.upperBodyStrength += sitUp.growth(minimum: 10, maximum: 20, current: $0.upperBodyStrength)
-    $0.fatigue += sitUp.changeFatigue(minimum: 10, maximum: 20, current: $0.fatigue)
+    sitUp.upperGrowth(condition: $0, min: 10, max: 20)
+    sitUp.changeFatigue(condition: $0, min: 10, max: 20)
 })
 
 let squat: Activity = .init(name: "스쿼트", action: {
-    $0.lowerBodyStrength += squat.growth(minimum: 20, maximum: 30, current: $0.lowerBodyStrength)
-    $0.fatigue += squat.changeFatigue(minimum: 10, maximum: 20, current: $0.fatigue)
+    squat.lowerGrowth(condition: $0, min: 20, max: 30)
+    squat.changeFatigue(condition: $0, min: 10, max: 20)
 })
 
 let longDistanceRunning: Activity = .init(name: "오래달리기", action: {
-    $0.upperBodyStrength += longDistanceRunning.growth(minimum: 5, maximum: 10, current: $0.upperBodyStrength)
-    $0.lowerBodyStrength += longDistanceRunning.growth(minimum: 5, maximum: 10, current: $0.lowerBodyStrength)
-    $0.muscularEndurance += longDistanceRunning.growth(minimum: 20, maximum: 30, current: $0.muscularEndurance / 2)
-    $0.fatigue += longDistanceRunning.changeFatigue(minimum: 20, maximum: 30, current: $0.fatigue)
+    longDistanceRunning.upperGrowth(condition: $0, min: 5, max: 10)
+    longDistanceRunning.lowerGrowth(condition: $0, min: 5, max: 10)
+    longDistanceRunning.enduranceGrowth(condition: $0, min: 20, max: 30)
+    longDistanceRunning.changeFatigue(condition: $0, min: 20, max: 30)
 })
 
 let activeRest: Activity = .init(name: "동적휴식", action: {
-    $0.fatigue += activeRest.changeFatigue(minimum: 5, maximum: 10, rest: true, current: $0.fatigue)
+    activeRest.changeFatigue(condition: $0, min: 5, max: 10, rest: true)
 })
 
 gundy.doActivity(sitUp)
