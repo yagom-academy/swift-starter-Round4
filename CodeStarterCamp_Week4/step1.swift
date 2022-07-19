cf582e8f6f10dce205770af94a1500a534e10be9
seoyounghan <happyunbd364@naver.com>
2022-07-19 15:44:27 +0900

    edit switch

--- CodeStarterCamp_Week4/step1.swift
+++ CodeStarterCamp_Week4/step1.swift
@@ -8,42 +8,48 @@
 import Foundation
 
 class BodyCondition {
+    enum Conditions {
+        case 상체근력
+        case 하체근력
+        case 근지구력
+        case 피로도
+    }
+    
     var upperBodyStrength: Int = 0 {
         didSet {
-            self.printChangedCondition("상체근력", distance: upperBodyStrength - oldValue)
+            self.printChangedCondition("상체근력", degree: upperBodyStrength - oldValue)
         }
     }
     var lowerBodyStrength: Int = 0 {
         didSet {
-            self.printChangedCondition("하체근력", distance: lowerBodyStrength - oldValue)
+            self.printChangedCondition("하체근력", degree: lowerBodyStrength - oldValue)
         }
     }
     var muscularEndurance: Int = 0 {
         didSet {
-            self.printChangedCondition("근지구력", distance: muscularEndurance - oldValue)
+            self.printChangedCondition("근지구력", degree: muscularEndurance - oldValue)
         }
     }
     var fatigue: Int = 0 {
         didSet {
-            self.printChangedCondition("피로도", distance: fatigue - oldValue)
+            self.printChangedCondition("피로도", degree: fatigue - oldValue)
         }
     }
     
-    func printChangedCondition(_ conditionName: String, distance: Int) {
-        if distance > 0 {
-            print("\(conditionName)이(가) \(distance) 상승합니다.")
+    func printChangedCondition(_ conditionName: String, degree: Int) {
+        if degree > 0 {
+            print("\(conditionName)이(가) \(degree) 상승합니다.")
         } else {
-            print("\(conditionName)이(가) \((-1) * distance) 하락합니다.")
+            print("\(conditionName)이(가) \((-1) * degree) 하락합니다.")
         }
     }
     
-    func changeCondition(_ conditionName: String, degree: Int) {
-        switch (conditionName) {
-        case "상체근력": self.upperBodyStrength += degree
-        case "하체근력": self.lowerBodyStrength += degree
-        case "근지구력": self.muscularEndurance += degree
-        case "피로도": self.fatigue += degree
-        default: break
+    func changeCondition(_ condition: Conditions, degree: Int) {
+        switch condition {
+        case .상체근력: self.upperBodyStrength += degree
+        case .하체근력: self.lowerBodyStrength += degree
+        case .근지구력: self.muscularEndurance += degree
+        case .피로도: self.fatigue += degree
         }
     }
     
@@ -66,40 +72,3 @@ struct Activity {
         print("<<\(self.name)을(를) 시작합니다>>")
     }
 }
-
-let 윗몸일으키기: Activity = Activity(name: "윗몸일으키기", action: { (bodyCondition: BodyCondition) in
-    윗몸일으키기.printStartActivity()
-    
-    bodyCondition.changeCondition("상체근력", degree: Int.random(in: 10...20))
-    bodyCondition.changeCondition("하체근력", degree: Int.random(in: 10...20))
-    
-    bodyCondition.printTotalCondition()
-})
-
-let 스쿼트: Activity = Activity(name: "스쿼트", action: { (bodyCondition: BodyCondition) in
-    스쿼트.printStartActivity()
-    
-    bodyCondition.changeCondition("하체근력", degree: Int.random(in: 20...30))
-    bodyCondition.changeCondition("피로도", degree: Int.random(in: 10...20))
-    
-    bodyCondition.printTotalCondition()
-})
-
-let 오래달리기: Activity = Activity(name: "오래달리기", action: { (bodyCondition: BodyCondition) in
-    오래달리기.printStartActivity()
-    
-    bodyCondition.changeCondition("근지구력", degree: Int.random(in: 20...30))
-    bodyCondition.changeCondition("상체", degree: Int.random(in: 5...10))
-    bodyCondition.changeCondition("하체", degree: Int.random(in: 5...10))
-    bodyCondition.changeCondition("피로도", degree: Int.random(in: 20...30))
-    
-    bodyCondition.printTotalCondition()
-})
-
-let 동적휴식: Activity = Activity(name: "동적휴식", action: { (bodyCondition: BodyCondition) in
-    동적휴식.printStartActivity()
-    
-    bodyCondition.changeCondition("피로도", degree: Int.random(in: -10 ...  -5))
-    
-    bodyCondition.printTotalCondition()
-})
