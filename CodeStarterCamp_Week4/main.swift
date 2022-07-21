a34cd857511c81c39938f343e5064b8a0cce096d
Yena <yena@Yenaui-MacBookAir.local>
2022-07-21 22:51:36 +0900

    변수/상수 이름 수정

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -10,10 +10,10 @@ import Foundation
 
 class BodyCondition {
     var name: String
-    var 상체근력: Int
-    var 하체근력: Int
-    var 근지구력: Int
-    var 피로도: Int
+    var upperBodyMuscleStrength: Int
+    var lowerBodyMuscleStrength: Int
+    var muscularEndurance: Int
+    var tiredness: Int
     
     var activity: Activity? {
         didSet {
@@ -26,21 +26,21 @@ class BodyCondition {
         }
     }
     
-    init(name: String, 상체근력: Int = 0, 하체근력: Int = 0, 근지구력: Int = 0, 피로도: Int = 0) {
+    init(name: String, upperBodyMuscleStrength: Int = 0, lowerBodyMuscleStrength: Int = 0, muscularEndurance: Int = 0, tiredness: Int = 0) {
         self.name = name
-        self.상체근력 = 상체근력
-        self.하체근력 = 하체근력
-        self.근지구력 = 근지구력
-        self.피로도 = 피로도
+        self.upperBodyMuscleStrength = upperBodyMuscleStrength
+        self.lowerBodyMuscleStrength = lowerBodyMuscleStrength
+        self.muscularEndurance = muscularEndurance
+        self.tiredness = tiredness
     }
     
     func printCondition() {
         print("--------------")
         print("현재 컨디션은 다음과 같습니다.")
-        print("상체근력: \(상체근력)")
-        print("하체근력: \(하체근력)")
-        print("근지구력: \(근지구력)")
-        print("피로도: \(피로도)")
+        print("상체근력: \(upperBodyMuscleStrength)")
+        print("하체근력: \(lowerBodyMuscleStrength)")
+        print("근지구력: \(muscularEndurance)")
+        print("피로도: \(tiredness)")
         print("--------------")
     }
 }
@@ -52,75 +52,75 @@ struct Activity {
 
 let 윗몸일으키기: Activity = Activity(name: "윗몸일으키기") { (bodyCondition: BodyCondition) -> Void in
     
-    let 상체근력 = Int.random(in: 10...20)
-    let 피로도 = Int.random(in: 10...20)
+    let upperBodyMuscleStrength = Int.random(in: 10...20)
+    let tiredness = Int.random(in: 10...20)
     
-    print("상체근력이 \(상체근력) 상승합니다.")
-    print("피로도가 \(피로도) 상승합니다.")
+    print("상체근력이 \(upperBodyMuscleStrength) 상승합니다.")
+    print("피로도가 \(tiredness) 상승합니다.")
     
-    bodyCondition.상체근력 += 상체근력
-    bodyCondition.피로도 += 피로도
+    bodyCondition.upperBodyMuscleStrength += upperBodyMuscleStrength
+    bodyCondition.tiredness += tiredness
 }
 
 let 스쿼트: Activity = Activity(name: "스쿼트") { (bodyCondition: BodyCondition) -> Void in
     
-    let 하체근력 = Int.random(in: 20...30)
-    let 피로도 = Int.random(in: 10...20)
+    let lowerBodyMuscleStrength = Int.random(in: 20...30)
+    let tiredness = Int.random(in: 10...20)
     
-    print("하체근력이 \(하체근력) 상승합니다.")
-    print("피로도가 \(피로도) 상승합니다.")
+    print("하체근력이 \(lowerBodyMuscleStrength) 상승합니다.")
+    print("피로도가 \(tiredness) 상승합니다.")
     
-    bodyCondition.하체근력 += 하체근력
-    bodyCondition.피로도 += 피로도
+    bodyCondition.lowerBodyMuscleStrength += lowerBodyMuscleStrength
+    bodyCondition.tiredness += tiredness
 }
 
 let 오래달리기: Activity = Activity(name: "오래달리기") { (bodyCondition: BodyCondition) -> Void in
     
-    let 근지구력 = Int.random(in: 20...30)
-    let 상체근력 = Int.random(in: 5...10)
-    let 하체근력 = Int.random(in: 5...10)
-    let 피로도 = Int.random(in: 20...30)
+    let muscularEndurance = Int.random(in: 20...30)
+    let upperBodyMuscleStrength = Int.random(in: 5...10)
+    let lowerBodyMuscleStrength = Int.random(in: 5...10)
+    let tiredness = Int.random(in: 20...30)
     
-    print("근지구력이 \(근지구력) 상승합니다.")
-    print("상체근력이 \(상체근력) 상승합니다.")
-    print("하체근력이 \(하체근력) 상승합니다.")
-    print("피로도가 \(피로도) 상승합니다.")
+    print("근지구력이 \(muscularEndurance) 상승합니다.")
+    print("상체근력이 \(upperBodyMuscleStrength) 상승합니다.")
+    print("하체근력이 \(lowerBodyMuscleStrength) 상승합니다.")
+    print("피로도가 \(tiredness) 상승합니다.")
     
-    bodyCondition.근지구력 += 근지구력
-    bodyCondition.상체근력 += 상체근력
-    bodyCondition.하체근력 += 하체근력
-    bodyCondition.피로도 += 피로도
+    bodyCondition.muscularEndurance += muscularEndurance
+    bodyCondition.upperBodyMuscleStrength += upperBodyMuscleStrength
+    bodyCondition.lowerBodyMuscleStrength += lowerBodyMuscleStrength
+    bodyCondition.tiredness += tiredness
 }
 
 let 동적휴식: Activity = Activity(name: "동적휴식") {(bodyCondition: BodyCondition) -> Void in
     
-    let 피로도 = Int.random(in: 5...10)
+    let tiredness = Int.random(in: 5...10)
     
-    print("피로도가 \(피로도) 감소합니다.")
+    print("피로도가 \(tiredness) 감소합니다.")
     
-    bodyCondition.피로도 -= 피로도
+    bodyCondition.tiredness -= tiredness
 }
 
 let 스트레칭: Activity = Activity(name: "스트레칭") {(bodyCondition: BodyCondition) -> Void in
     
-    let 상체근력 = Int.random(in: 1...5)
-    let 하체근력 = Int.random(in: 1...5)
-    let 피로도 = Int.random(in: 1...5)
+    let upperBodyMuscleStrength = Int.random(in: 1...5)
+    let lowerBodyMuscleStrength = Int.random(in: 1...5)
+    let tiredness = Int.random(in: 1...5)
     
-    print("상체근력이 \(상체근력) 상승합니다.")
-    print("하체근력이 \(하체근력) 상승합니다.")
-    print("피로도가 \(피로도) 감소합니다.")
+    print("상체근력이 \(upperBodyMuscleStrength) 상승합니다.")
+    print("하체근력이 \(lowerBodyMuscleStrength) 상승합니다.")
+    print("피로도가 \(tiredness) 감소합니다.")
     
-    bodyCondition.상체근력 += 상체근력
-    bodyCondition.하체근력 += 하체근력
-    bodyCondition.피로도 -= 피로도
+    bodyCondition.upperBodyMuscleStrength += upperBodyMuscleStrength
+    bodyCondition.lowerBodyMuscleStrength += lowerBodyMuscleStrength
+    bodyCondition.tiredness -= tiredness
 }
 
 var dasanBodyCondition = BodyCondition(name: "다산")
 dasanBodyCondition.activity = 윗몸일으키기
 dasanBodyCondition.printCondition()
 
-var namuBodyCondition = BodyCondition(name: "나무", 상체근력: 50, 하체근력: 50, 근지구력: 50, 피로도: 10)
+var namuBodyCondition = BodyCondition(name: "나무", upperBodyMuscleStrength: 50, lowerBodyMuscleStrength: 50, muscularEndurance: 50, tiredness: 10)
 namuBodyCondition.activity = 스트레칭
 namuBodyCondition.printCondition()
 
