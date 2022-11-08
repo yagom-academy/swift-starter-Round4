8920789f967169d450572d18feed9a7c86287c7c
500beckwon <kjsx60@gmail.com>
2022-11-08 20:13:29 +0900

    Activity 제작

--- CodeStarterCamp_Week4/Activity.swift
+++ CodeStarterCamp_Week4/Activity.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct Activity {
+    var name: String
+    let action: (BodyCondition) -> Void
+    
+    func startExercise() {
+        print("<<\(name)을(를) 시작합니다>>")
+    }
+}
