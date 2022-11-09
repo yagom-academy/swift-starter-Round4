d3158115055cceef3e21ad281d98ab1729a74708
500beckwon <kjsx60@gmail.com>
2022-11-09 22:04:46 +0900

    전용 Error 제작

--- CodeStarterCamp_Week4/ActivityError.swift
+++ CodeStarterCamp_Week4/ActivityError.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+enum ActivityError: Error {
+    case overFatigue
+    case onlyInteger
+    case unknown
+    
+    var errorMessage: String {
+        switch self {
+        case .overFatigue:
+            return "피로도가 100 이상입니다. 루틴을 중단합니다."
+        case .onlyInteger:
+            return "양의 정수만 입력 가능합니다. 다시 입력해주세요."
+        case .unknown:
+            return "잘못된 입력 형식입니다. 다시 입력해주세요."
+        }
+    }
+}
+
+
