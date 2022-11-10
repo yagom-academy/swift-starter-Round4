0b70deae0270daa8dde70714f2aba2347b0e4101
ROWAN <kimkj93@naver.com>
2022-11-10 19:30:20 +0900

    WEEK4_STEP2

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,26 +8,27 @@
 
 import Foundation
 
-struct BodyCondition {
-    var upperBodyStrength = 80 {
+// MARK: STEP1 활동 만들기
+class BodyCondition {
+    var upperBodyStrength = 0 {
         didSet(oldValue) {
             print("상체근력이 \(self.upperBodyStrength - oldValue) 상승합니다.")
         }
     }
     
-    var lowerBodyStrength = 80 {
+    var lowerBodyStrength = 0 {
         didSet(oldValue) {
             print("하체근력이 \(self.lowerBodyStrength - oldValue) 상승합니다.")
         }
     }
     
-    var muscularEndurance = 50 {
+    var muscularEndurance = 0 {
         didSet(oldValue) {
             print("근지구력이 \(self.muscularEndurance - oldValue) 상승합니다.")
         }
     }
     
-    var fatigue = 20 {
+    var fatigue = 0 {
         didSet(oldValue) {
             if fatigue > 0, fatigue >= oldValue {
                 print("피로도가 \(self.fatigue - oldValue) 상승합니다.")
@@ -51,26 +52,26 @@ struct BodyCondition {
     """)
     }
     
-    mutating func addRandomIntUpperStrength(from a: Int, to b: Int) {
+    func addRandomIntUpperStrength(from a: Int, to b: Int) {
         self.upperBodyStrength += Int.random(in: a...b)
     }
     
-    mutating func addRandomIntLowerStrength(from a: Int, to b: Int) {
+    func addRandomIntLowerStrength(from a: Int, to b: Int) {
         self.lowerBodyStrength += Int.random(in: a...b)
     }
     
-    mutating func addRandomIntMuscularEndurance(from a: Int, to b: Int) {
+    func addRandomIntMuscularEndurance(from a: Int, to b: Int) {
         self.muscularEndurance += Int.random(in: a...b)
     }
     
-    mutating func addRandomIntFatigue(from a: Int, to b: Int) {
+    func addRandomIntFatigue(from a: Int, to b: Int) {
         self.fatigue += Int.random(in: a...b)
     }
 }
 
 struct Activity {
     let name: String
-    let action: (inout BodyCondition) -> Void
+    let action: (BodyCondition) -> Void
 }
 
 let sitUp = Activity(name: "윗몸일으키키", action: { bodyCondition in
@@ -104,23 +105,77 @@ let dynamicBreak = Activity(name: "동적휴식", action: { bodyCondition in
 
 let climbing = Activity(name: "클라이밍", action: { bodyCondition in
     print("\(climbing.name)을(를) 시작합니다>>")
-    bodyCondition.addRandomIntUpperStrength(from: 15, to: 30)
+    bodyCondition.addRandomIntUpperStrength(from: 5, to: 10)
     bodyCondition.addRandomIntLowerStrength(from: 5, to: 10)
-    bodyCondition.addRandomIntMuscularEndurance(from: 20, to: 30)
-    bodyCondition.addRandomIntFatigue(from: 10, to: 20)
+    bodyCondition.addRandomIntMuscularEndurance(from: 10, to: 20)
+    bodyCondition.addRandomIntFatigue(from: 5, to: 10)
     print("--------------")
 })
 
-var rowan = BodyCondition()
-sitUp.action(&rowan)
-rowan.checkCondition()
+let rowan = BodyCondition()
 
-climbing.action(&rowan)
-running.action(&rowan)
-dynamicBreak.action(&rowan)
-rowan.checkCondition()
 
-for _ in 1...11 {
-    dynamicBreak.action(&rowan)
+// MARK: STEP2 야곰 피트니스, 지옥의 헬스 트레이닝
+enum RoutineError: Error{
+    case overFatigueLimit
+    case wrongInput
+    case overRoutineLimit
 }
-rowan.checkCondition()
+
+let ordinalNumber: Array<String> = ["첫", "두", "세", "네", "다섯", "여섯", "일곱", "여덟", "아홉", "열"]
+
+struct Routine {
+    let name: String
+    let activities: Array<Activity> = [sitUp, dynamicBreak, climbing]
+    let member: BodyCondition
+    
+    mutating func repeatRoutine(_ bodyCondition: BodyCondition) throws {
+        var roundUInt: UInt?
+        if let round = readLine() {
+            roundUInt = UInt(round)
+            guard let checkRound = roundUInt, type(of: checkRound) == UInt.self else {
+                throw RoutineError.wrongInput
+            }
+        }
+        print("--------------")
+        
+        if let range = roundUInt {
+            for round in 0...range-1 {
+                guard round < 10 else {
+                    throw RoutineError.overRoutineLimit
+                }
+                
+                let number = Int(round)
+                print("\(ordinalNumber[number]) 번째 \(self.name)을(를) 시작합니다.")
+                for count in 0...self.activities.count-1 {
+                    self.activities[count].action(self.member)
+                    guard self.member.fatigue < 100 else {
+                        throw RoutineError.overFatigueLimit
+                    }
+                }
+            }
+            bodyCondition.checkCondition()
+        }
+    }
+}
+
+var hellRoutine = Routine(name: "hellRoutine", member: rowan)
+
+func startRoutine() {
+    print("루틴을 몇 번 반복할까요?")
+    do {
+        try hellRoutine.repeatRoutine(hellRoutine.member)
+    } catch RoutineError.overFatigueLimit {
+        print("피로도가 100 이상입니다. 루틴을 중단합니다.")
+        hellRoutine.member.checkCondition()
+    } catch RoutineError.wrongInput {
+        print("잘못된 입력 형식입니다. 다시 입력해주세요.")
+    } catch RoutineError.overRoutineLimit {
+        print("루틴 반복 횟수 최대치 10회를 초과했습니다. 루틴을 중단합니다.")
+        hellRoutine.member.checkCondition()
+    } catch {
+        print("예상치 못한 에러가 발생했습니다. \(error)")
+    }
+}
+
+startRoutine()
