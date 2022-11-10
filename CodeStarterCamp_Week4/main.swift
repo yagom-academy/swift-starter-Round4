de40d06de40476235221c76fcf60d5fe1f24e4f9
siwol <siwolssi@gmail.com>
2022-11-10 15:42:31 +0900

    STEP 1 수정

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct BodyCondition {
+class BodyCondition {
     var 상체근력: Int = 0 {
         didSet(oldValue) {
             print("상체근력이 \(상체근력 - oldValue) 상승합니다.")
@@ -37,7 +37,7 @@ struct BodyCondition {
         }
     }
     
-    func currentBodyCondition() {
+    func nowBodyCondition() {
         print("""
                 --------------
                 현재의 컨디션은 다음과 같습니다.
@@ -50,51 +50,10 @@ struct BodyCondition {
     }
 }
 
-struct Activitiy {
-    let name: String
-    let action: (inout BodyCondition) -> Void
-}
+var myBodyConditon = BodyCondition()
 
-let 윗몸일으키기: Activitiy = Activitiy(name: "윗몸일으키기", action: { bodyCondition in
-    print("<<\(윗몸일으키기.name)을(를) 시작합니다>>")
-    bodyCondition.상체근력 += Int.random(in: 10...20)
-    bodyCondition.피로도 += Int.random(in: 10...20)
-    bodyCondition.currentBodyCondition()
-})
-
-let 스쿼트: Activitiy = Activitiy(name: "스쿼트", action: { bodyCondition in
-    print("<<\(스쿼트.name)을(를) 시작합니다>>")
-    bodyCondition.하체근력 += Int.random(in: 20...30)
-    bodyCondition.피로도 += Int.random(in: 10...20)
-    bodyCondition.currentBodyCondition()
-})
-
-let 런지: Activitiy = Activitiy(name: "런지", action: { bodyCondition in
-    print("<<\(런지.name)을(를) 시작합니다>>")
-    bodyCondition.하체근력 += Int.random(in: 10...20)
-    bodyCondition.피로도 += Int.random(in: 10...20)
-    bodyCondition.currentBodyCondition()
-})
-
-let 오래달리기: Activitiy = Activitiy(name: "오래달리기", action: { bodyCondition in
-    print("<<\(오래달리기.name)을(를) 시작합니다>>")
-    bodyCondition.근지구력 += Int.random(in: 20...30)
-    bodyCondition.상체근력 += Int.random(in: 5...10)
-    bodyCondition.하체근력 += Int.random(in: 5...10)
-    bodyCondition.피로도 += Int.random(in: 20...30)
-    bodyCondition.currentBodyCondition()
-})
-
-let 동적휴식: Activitiy = Activitiy(name: "동적휴식", action: { bodyCondition in
-    print("<<\(동적휴식.name)을(를) 시작합니다>>")
-    bodyCondition.피로도 -= Int.random(in: 5...10)
-    bodyCondition.currentBodyCondition()
-})
-
-var exercise = BodyCondition()
-
-윗몸일으키기.action(&exercise)
-스쿼트.action(&exercise)
-런지.action(&exercise)
-오래달리기.action(&exercise)
-동적휴식.action(&exercise)
+윗몸일으키기.action(myBodyConditon)
+스쿼트.action(myBodyConditon)
+런지.action(myBodyConditon)
+오래달리기.action(myBodyConditon)
+동적휴식.action(myBodyConditon)
