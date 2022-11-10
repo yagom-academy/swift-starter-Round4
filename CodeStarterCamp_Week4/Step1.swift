1e955d8f7ba05d34611821437676e6f71e2f01c0
kimseongj <ksj8277@naver.com>
2022-11-10 10:36:20 +0900

    Week4 Step1

--- CodeStarterCamp_Week4/Step1.swift
+++ CodeStarterCamp_Week4/Step1.swift
@@ -1,5 +1,4 @@
 import Foundation
-
 class BodyCondition {
     var upperBodyStrength: Int
     var legStrength: Int
@@ -17,14 +16,17 @@ class BodyCondition {
 var bodyCondition = BodyCondition(upperBodyStrength: 40, legStrength: 40, muscularEndurance: 40, tiredness: 15)
 
 struct Activity {
-    var name: String {
-        willSet {
-            print("<<\(name)을(를) 시작합니다.>>")
-        }
-    }
+    var name: String
+    
     let action: (BodyCondition) -> Void
 }
 
+enum ActivityType: String {
+    case situp = "윗몸일으키기"
+    case squart = "스쿼트"
+    case running = "오래달리기"
+    case dynamicRest = "동적휴식"
+}
 
 let situp: Activity = Activity(name: "윗몸일으키기", action: {(bodyCondtion) in
     var plusUpperBodyStrength = Int.random(in: 10...20)
@@ -33,6 +35,7 @@ let situp: Activity = Activity(name: "윗몸일으키기", action: {(bodyCondtio
     bodyCondition.upperBodyStrength += plusUpperBodyStrength
     bodyCondition.tiredness += plusTiredness
     
+    print("<<\(situp.name)을(를) 시작합니다.>>")
     print("상체근력이 \(plusUpperBodyStrength) 상승합니다.")
     print("피로도가 \(plusTiredness) 상승합니다.")
     print("--------------")
@@ -45,6 +48,7 @@ let squart: Activity = Activity(name: "스쿼트", action: {(bodyCondition) in
     bodyCondition.legStrength += plusLegStrength
     bodyCondition.tiredness += plusTiredness
     
+    print("<<\(squart.name)을(를) 시작합니다.>>")
     print("하체근력이 \(plusLegStrength) 상승합니다.")
     print("피로도가 \(plusTiredness) 상승합니다.")
     print("--------------")
@@ -59,6 +63,7 @@ let running: Activity = Activity(name: "오래달리기", action: {(bodyConditio
     bodyCondition.legStrength += plusLegStrength
     bodyCondition.tiredness += plusTiredness
     
+    print("<<\(running.name)을(를) 시작합니다.>>")
     print("상체근력이 \(plusUpperBodyStrength) 상승합니다.")
     print("하체근력이 \(plusLegStrength) 상승합니다.")
     print("피로도가 \(plusTiredness) 상승합니다.")
@@ -66,14 +71,30 @@ let running: Activity = Activity(name: "오래달리기", action: {(bodyConditio
     
 })
 
-let rest: Activity = Activity(name: "오래달리기", action: {(bodyCondition) in
+let dynamicRest: Activity = Activity(name: "동적휴식", action: {(bodyCondition) in
     var minusTiredness = Int.random(in: 5...10)
     
     bodyCondition.tiredness -= minusTiredness
     
+    print("<<\(dynamicRest.name)을(를) 시작합니다.>>")
     print("피로도가 \(minusTiredness) 감소합니다.")
     print("--------------")
 })
 
+func printCondtion() {
+    print("""
+        현재의 컨디션은 다음과 같습니다.
+        상체근력: \(bodyCondition.upperBodyStrength)
+        하체근력: \(bodyCondition.legStrength)
+        근지구력: \(bodyCondition.muscularEndurance)
+        피로도: \(bodyCondition.tiredness)
+        --------------
+        """)
+}
 
 
+
+func printResult(activity: Activity) {
+    activity.action(bodyCondition)
+    printCondtion()
+}
--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -1,5 +1,7 @@
 
 import Foundation
 
-situp.action(bodyCondition)
+printResult(activity: squart)
+printCondtion()
+
 
