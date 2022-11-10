e74f40943603d317b43f67866c39e34c7b3d87bf
hyemory <cjstksla73@gmail.com>
2022-11-10 15:28:23 +0900

    STEP 2 리뷰 후 수정

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -22,20 +22,20 @@ var myCondition: BodyCondition = BodyCondition(upperBodyStrength: 0, lowerBodySt
 // MARK: - STEP2 호출
 var myRoutine: Routine = Routine(name: "hyemorysRoutine")
 
-func executeRoutine() {
+func executeRoutine(routine: Routine, by bodyCondition: BodyCondition) {
     do {
-        try myRoutine.repeatRoutine(by: myCondition)
+        try routine.repeatRoutine(by: bodyCondition)
     } catch RoutineError.invalidInput {
         print("잘못된 입력 형식입니다. 다시 입력해주세요.")
-        executeRoutine()
+        executeRoutine(routine: routine, by: bodyCondition)
     } catch RoutineError.fatigueLevelLimit {
         print("피로도가 100이상입니다. 루틴을 중단합니다.")
-        myCondition.informBodyCondition()
+        bodyCondition.informBodyCondition()
     } catch {
         print(error)
     }
 }
 
 print("루틴을 몇 번 반복할까요?")
-executeRoutine()
+executeRoutine(routine: myRoutine, by: myCondition)
 
