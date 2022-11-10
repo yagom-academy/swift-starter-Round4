0de7875d6c09da08ba76fa3ea6d8cb7987d281c7
hyemory <cjstksla73@gmail.com>
2022-11-10 15:22:22 +0900

    STEP 2 리뷰 후 수정

--- CodeStarterCamp_Week4/step2.swift
+++ CodeStarterCamp_Week4/step2.swift
@@ -9,22 +9,30 @@ import Foundation
 
 
 enum Numbers: UInt {
-    case first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth, eleventh, twelfth
+    case first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth
     
     var numbers: String {
         switch self {
-        case .first: return "첫"
-        case .second: return "두"
-        case .third: return "세"
-        case .fourth: return "네"
-        case .fifth: return "다섯"
-        case .sixth: return "여섯"
-        case .seventh: return "일곱"
-        case .eighth: return "여덟"
-        case .ninth: return "아홈"
-        case .tenth: return "열"
-        case .eleventh: return "열한"
-        case .twelfth: return "열두"
+        case .first:
+            return "첫"
+        case .second:
+            return "두"
+        case .third:
+            return "세"
+        case .fourth:
+            return "네"
+        case .fifth:
+            return "다섯"
+        case .sixth:
+            return "여섯"
+        case .seventh:
+            return "일곱"
+        case .eighth:
+            return "여덟"
+        case .ninth:
+            return "아홈"
+        case .tenth:
+            return "열"
         }
     }
 }
@@ -37,36 +45,35 @@ enum RoutineError: Error {
 
 
 struct Routine {
-    let routineName: String
-    var activities: [Activity] = [sitUp, activeRest, squat, activeRest, longRunning]
+    let name: String
+    var activities: [Activity] = [longRunning, activeRest, squat]
     
     func repeatSet() throws -> UInt {
-        print("루틴을 몇 번 반복할까요?")
         let setsNumber: String? = readLine()
         print("--------------")
         
-        guard let setsNumber = setsNumber else {
+        guard let inputNumber = setsNumber, inputNumber != "0" else {
             throw RoutineError.invalidInput
         }
         
-        guard let setsNumber = UInt(setsNumber) else {
+        guard let positiveSetsNumber = UInt(inputNumber) else {
             throw RoutineError.invalidInput
         }
         
-        return setsNumber
+        return positiveSetsNumber - 1
     }
     
     func repeatRoutine(by bodyCondition: BodyCondition) throws {
-        let setsNumber: UInt = try repeatSet()
+        let positiveSetsNumber: UInt = try repeatSet()
         
-        for sets in 0...setsNumber {
-            print("\(Numbers(rawValue: sets)?.numbers ?? "?")번째 \(routineName)을(를) 시작합니다")
+        for sets in 0...positiveSetsNumber {
+            guard bodyCondition.fatigueLevel < 100 else {
+                throw RoutineError.fatigueLevelLimit
+            }
+            
+            print("\(Numbers(rawValue: sets)?.numbers ?? "\(sets + 1)")번째 \(name)을(를) 시작합니다")
             
             for activitiy in activities {
-                guard bodyCondition.fatigueLevel < 100 else {
-                    throw RoutineError.fatigueLevelLimit
-                }
-                
                 workOut(activity: activitiy, by: bodyCondition)
                 print("--------------")
             }
