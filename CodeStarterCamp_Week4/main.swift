172bbb2c0dd0243dcd75321e27900a03dad32d70
gyeong <okbamtol@naver.com>
2022-11-18 18:56:04 +0900

    step1 수정

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -16,7 +16,6 @@ class BodyCondition {
             print("하체근력이 \(lowerStrength - oldValue) 상승합니다.")
         }
     }
-    
     var muscularEndurance: Int = 0 {
         didSet(oldValue) {
             print("근지구력이 \(muscularEndurance - oldValue) 상승합니다.")
@@ -24,18 +23,24 @@ class BodyCondition {
     }
     var fatigue: Int = 0 {
         didSet(oldValue) {
-            print("피로도가 \(fatigue - oldValue) 만큼 변합니다.")
+            if fatigue > oldValue {
+                print("피로도가 \(fatigue - oldValue) 상승합니다.")
+            }else{
+                print("피로도가 \(fatigue - oldValue) 하락합니다.")
+            }
         }
     }
     
     func presentCondition() {
-        print("--------------")
-        print("현재의 컨디션은 다음과 같습니다.")
-        print("상체근력: \(upperStrength)")
-        print("하체근력: \(lowerStrength)")
-        print("근지구력: \(muscularEndurance)")
-        print("피로도: \(fatigue)")
-        print("--------------")
+        print("""
+        "--------------"
+        "현재의 컨디션은 다음과 같습니다."
+        "상체근력: \(upperStrength)"
+        "하체근력: \(lowerStrength)"
+        "근지구력: \(muscularEndurance)"
+        "피로도: \(fatigue)"
+        "--------------"
+        """)
     }
 }
 
@@ -45,19 +50,16 @@ struct Activity {
 }
 
 let sitUp: Activity = Activity(name: "윗몸일으키기", action: { bodyCondition in
-    print("<<\(sitUp.name)을(를) 시작합니다>>")
     bodyCondition.upperStrength += Int.random(in: 10...20)
     bodyCondition.fatigue += Int.random(in: 10...20)
 })
 
 let squat: Activity = Activity(name: "스쿼트", action: { bodyCondition in
-    print("<<\(squat.name)을(를) 시작합니다>>")
     bodyCondition.lowerStrength += Int.random(in: 20...30)
     bodyCondition.fatigue += Int.random(in: 10...20)
 })
 
 let longRunning: Activity = Activity(name: "오래달리기", action: { bodyCondition in
-    print("<<\(longRunning.name)을(를) 시작합니다>>")
     bodyCondition.upperStrength += Int.random(in: 5...10)
     bodyCondition.lowerStrength += Int.random(in: 5...10)
     bodyCondition.muscularEndurance += Int.random(in: 20...30)
@@ -65,7 +67,6 @@ let longRunning: Activity = Activity(name: "오래달리기", action: { bodyCond
 })
 
 let dynamicBreak: Activity = Activity(name: "동적휴식", action: { bodyCondition in
-    print("<<\(dynamicBreak.name)을(를) 시작합니다>>")
     if bodyCondition.fatigue == 0 {
         print("현재 피로도는 0이기 때문에 변화가 없습니다")
     }else{
@@ -73,10 +74,15 @@ let dynamicBreak: Activity = Activity(name: "동적휴식", action: { bodyCondit
     }
 })
 
+func executeActivity(condition: BodyCondition, activity: Activity) {
+    print("<<\(activity.name)을(를) 시작합니다>>")
+    activity.action(condition)
+}
+
 var myBody: BodyCondition = BodyCondition()
 
-sitUp.action(myBody)
-squat.action(myBody)
-longRunning.action(myBody)
-dynamicBreak.action(myBody)
+executeActivity(condition: myBody, activity: sitUp)
+executeActivity(condition: myBody, activity: squat)
+executeActivity(condition: myBody, activity: longRunning)
+executeActivity(condition: myBody, activity: dynamicBreak)
 myBody.presentCondition()
