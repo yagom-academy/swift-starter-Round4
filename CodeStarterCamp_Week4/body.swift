9c618265c7c50c227c6bf10fdc144f2cc5e2c65e
mint3382 <mskim0926@naver.com>
2023-03-13 23:05:41 +0900

    action 삭제

--- CodeStarterCamp_Week4/body.swift
+++ CodeStarterCamp_Week4/body.swift
@@ -60,32 +60,32 @@ struct Activity {
     let action: (BodyCondition) -> Void
 }
 
-let 윗몸일으키기: Activity = Activity(name: "윗몸일으키기", action: { bodycondition in
+let 윗몸일으키기: Activity = Activity(name: "윗몸일으키기") { bodyCondition in
     print("<<\(윗몸일으키기.name)을(를) 시작합니다.>>")
-    bodycondition.upperBodyStrength += Int.random(in: 10...20)
-    bodycondition.fatigability += Int.random(in: 10...20)
-    bodycondition.conditionCheck()
-})
+    bodyCondition.upperBodyStrength += Int.random(in: 10...20)
+    bodyCondition.fatigability += Int.random(in: 10...20)
+    bodyCondition.conditionCheck()
+}
 
-let 스쿼트: Activity = Activity(name: "스쿼트", action: { bodyCondition in
+let 스쿼트: Activity = Activity(name: "스쿼트") { bodyCondition in
     print("<<\(스쿼트.name)을(를) 시작합니다.>>")
     bodyCondition.lowerBodyStrength += Int.random(in: 20...30)
     bodyCondition.fatigability += Int.random(in: 10...20)
     bodyCondition.conditionCheck()
-})
+}
 
-let 오래달리기: Activity = Activity(name: "오래달리기", action: { bodyCondition in
+let 오래달리기: Activity = Activity(name: "오래달리기") { bodyCondition in
     print("<<\(오래달리기.name)을(를) 시작합니다.>>")
     bodyCondition.upperBodyStrength += Int.random(in: 5...10)
     bodyCondition.lowerBodyStrength += Int.random(in: 5...10)
     bodyCondition.muscularEndurance += Int.random(in: 20...30)
     bodyCondition.fatigability += Int.random(in: 20...30)
     bodyCondition.conditionCheck()
-})
+}
 
-let 동적휴식: Activity = Activity(name: "동적휴식", action: { bodyCondition in
+let 동적휴식: Activity = Activity(name: "동적휴식") { bodyCondition in
     print("<<\(동적휴식.name)을(를) 시작합니다.>>")
     bodyCondition.fatigability -= Int.random(in: 5...10)
     bodyCondition.conditionCheck()
-})
+}
 
