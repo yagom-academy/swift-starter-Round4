eb48da7d8e2631518017c1a29a6310fe63c0962c
Hoon94 <dleognsdl1@naver.com>
2023-03-13 23:12:23 +0900

    Step 1: 활동을 만들어 진행해보자

--- CodeStarterCamp_Week4/Step1.swift
+++ CodeStarterCamp_Week4/Step1.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+class BodyCondition {
+    var upperBodyStrength: Int = 100 {
+        didSet(oldValue) {
+            print("상체근력이 \(upperBodyStrength - oldValue) 상승합니다.")
+        }
+    }
+    var lowerBodyStrength: Int = 80 {
+        didSet(oldValue) {
+            print("하체근력이 \(lowerBodyStrength - oldValue) 상승합니다.")
+        }
+    }
+    var muscularEndurance: Int = 80 {
+        didSet(oldValue) {
+            print("근지구력이 \(muscularEndurance - oldValue) 상승합니다.")
+        }
+    }
+    var fatigability: Int = 0 {
+        didSet(oldValue) {
+            if fatigability >= oldValue {
+                print("피로도가 \(fatigability - oldValue) 상승합니다.")
+            } else {
+                print("피로도가 \(oldValue - fatigability) 감소합니다.")
+            }
+        }
+    }
+    
+    func printMeasuredBodyCondition() {
+        print("현재의 컨디션은 다음과 같습니다.")
+        print("상체근력: \(self.upperBodyStrength)")
+        print("하체근력: \(self.lowerBodyStrength)")
+        print("근지구력: \(self.muscularEndurance)")
+        print("피로도: \(self.fatigability)")
+        print("--------------")
+    }
+}
+
+struct Activity {
+    var name: String
+    let action: (BodyCondition) -> Void
+}
--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,5 +8,40 @@
 
 import Foundation
 
-print("Hello, World!")
+var bodyCondition: BodyCondition = BodyCondition()
 
+let sitUp: Activity = Activity(name: "윗몸일으키기", action: { bodyCondition in
+    print("<<윗몸일으키기을(를) 시작합니다.>>")
+    bodyCondition.upperBodyStrength += Int.random(in: 10...20)
+    bodyCondition.fatigability += Int.random(in: 10...20)
+    print("--------------")
+})
+
+let squat: Activity = Activity(name: "스쿼트", action: { bodyCondition in
+    print("<<스쿼트을(를) 시작합니다.>>")
+    bodyCondition.lowerBodyStrength += Int.random(in: 20...30)
+    bodyCondition.fatigability += Int.random(in: 10...20)
+    print("--------------")
+})
+
+let longRun: Activity = Activity(name: "오래달리기", action: { bodyCondition in
+    print("<<오래달리기을(를) 시작합니다.>>")
+    bodyCondition.muscularEndurance += Int.random(in: 20...30)
+    bodyCondition.upperBodyStrength += Int.random(in: 5...10)
+    bodyCondition.lowerBodyStrength += Int.random(in: 5...10)
+    bodyCondition.fatigability += Int.random(in: 20...30)
+    print("--------------")
+})
+
+let dynamicBreak: Activity = Activity(name: "동적휴식", action: { bodyCondition in
+    print("<<동적휴식을(를) 시작합니다.>>")
+    bodyCondition.fatigability += Int.random(in: -10...(-5))
+    print("--------------")
+})
+
+bodyCondition.printMeasuredBodyCondition()
+sitUp.action(bodyCondition)
+bodyCondition.printMeasuredBodyCondition()
+sitUp.action(bodyCondition)
+squat.action(bodyCondition)
+bodyCondition.printMeasuredBodyCondition()
