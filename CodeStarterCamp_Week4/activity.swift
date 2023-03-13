58c4c13a8423c53868779df3f35516666e41d0e9
idinaloq <idinaloq@icloud.com>
2023-03-14 00:09:32 +0900

    week4_step1

--- CodeStarterCamp_Week4/activity.swift
+++ CodeStarterCamp_Week4/activity.swift
@@ -14,18 +14,21 @@ struct Activity {
 
 //윗몸일으키기
 let 윗몸일으키기: Activity = Activity(name: "윗몸일으키기", action: { BodyCondition in
+    print("<<윗몸일으키기를 시작합니다>>")
     BodyCondition.상체근력 += Int.random(in: 10...20)
     BodyCondition.피로도 += Int.random(in: 10...20)
 })
 
 //스쿼트
 let 스쿼트: Activity = Activity(name: "스쿼트", action: { BodyCondition in
+    print("<<스쿼트를 시작합니다>>")
     BodyCondition.하체근력 += Int.random(in: 20...30)
     BodyCondition.피로도 += Int.random(in: 10...20)
 })
 
 //오래달리기
 let 오래달리기: Activity = Activity(name: "오래달리기", action: { BodyCondition in
+    print("<<오래달리기를 시작합니다>>")
     BodyCondition.근지구력 += Int.random(in: 20...30)
     BodyCondition.상체근력 += Int.random(in: 5...10)
     BodyCondition.하체근력 += Int.random(in: 5...10)
@@ -34,11 +37,13 @@ let 오래달리기: Activity = Activity(name: "오래달리기", action: { Body
 
 //동적휴식
 let 동적휴식: Activity = Activity(name: "동적휴식", action: { BodyCondition in
+    print("<<동적휴식을 시작합니다>>")
     BodyCondition.피로도 -= Int.random(in: 5...10)
 })
 
 //종합운동
 let 종합운동: Activity = Activity(name: "종합운동", action: { BodyCondition in
+    print("<<종합운동을 시작합니다>>")
     BodyCondition.상체근력 += Int.random(in: 15...30)
     BodyCondition.하체근력 += Int.random(in: 25...40)
     BodyCondition.근지구력 += Int.random(in: 20...30)
