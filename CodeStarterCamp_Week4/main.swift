a926378c7d19cf75a9a4303ddb26dcd79f87cbe6
idinaloq <idinaloq@icloud.com>
2023-03-14 00:00:14 +0900

    week4_step1

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -12,11 +12,15 @@ var idinaloq = BodyCondition(상체근력: 10, 하체근력: 20, 근지구력: 3
 
 윗몸일으키기.action(idinaloq)
 idinaloq.checkBodyCondition()
+
 스쿼트.action(idinaloq)
 idinaloq.checkBodyCondition()
+
 오래달리기.action(idinaloq)
 idinaloq.checkBodyCondition()
+
 동적휴식.action(idinaloq)
 idinaloq.checkBodyCondition()
+
 종합운동.action(idinaloq)
 idinaloq.checkBodyCondition()
