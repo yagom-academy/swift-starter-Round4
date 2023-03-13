dc37f5a238a73c9d2c8a54e109a66e70472b025d
Hoon94 <dleognsdl1@naver.com>
2023-03-14 00:39:11 +0900

    closure 축약

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -10,34 +10,34 @@ import Foundation
 
 var bodyCondition: BodyCondition = BodyCondition()
 
-let sitUp: Activity = Activity(name: "윗몸일으키기", action: { bodyCondition in
+let sitUp: Activity = Activity(name: "윗몸일으키기") {
     print("<<윗몸일으키기을(를) 시작합니다.>>")
-    bodyCondition.upperBodyStrength += Int.random(in: 10...20)
-    bodyCondition.fatigability += Int.random(in: 10...20)
+    $0.upperBodyStrength += Int.random(in: 10...20)
+    $0.fatigability += Int.random(in: 10...20)
     print("--------------")
-})
+}
 
-let squat: Activity = Activity(name: "스쿼트", action: { bodyCondition in
+let squat: Activity = Activity(name: "스쿼트") {
     print("<<스쿼트을(를) 시작합니다.>>")
-    bodyCondition.lowerBodyStrength += Int.random(in: 20...30)
-    bodyCondition.fatigability += Int.random(in: 10...20)
+    $0.lowerBodyStrength += Int.random(in: 20...30)
+    $0.fatigability += Int.random(in: 10...20)
     print("--------------")
-})
+}
 
-let longRun: Activity = Activity(name: "오래달리기", action: { bodyCondition in
+let longRun: Activity = Activity(name: "오래달리기") {
     print("<<오래달리기을(를) 시작합니다.>>")
-    bodyCondition.muscularEndurance += Int.random(in: 20...30)
-    bodyCondition.upperBodyStrength += Int.random(in: 5...10)
-    bodyCondition.lowerBodyStrength += Int.random(in: 5...10)
-    bodyCondition.fatigability += Int.random(in: 20...30)
+    $0.muscularEndurance += Int.random(in: 20...30)
+    $0.upperBodyStrength += Int.random(in: 5...10)
+    $0.lowerBodyStrength += Int.random(in: 5...10)
+    $0.fatigability += Int.random(in: 20...30)
     print("--------------")
-})
+}
 
-let dynamicBreak: Activity = Activity(name: "동적휴식", action: { bodyCondition in
+let dynamicBreak: Activity = Activity(name: "동적휴식") {
     print("<<동적휴식을(를) 시작합니다.>>")
-    bodyCondition.fatigability += Int.random(in: -10...(-5))
+    $0.fatigability += Int.random(in: -10...(-5))
     print("--------------")
-})
+}
 
 bodyCondition.printMeasuredBodyCondition()
 sitUp.action(bodyCondition)
