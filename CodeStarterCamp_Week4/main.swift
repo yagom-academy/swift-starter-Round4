699940d30bd87be4894239ba869783bf512359e8
eve <rlawldms1385@gmail.com>
2023-03-13 15:18:59 +0900

    Edit Step1

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -10,4 +10,4 @@ import Foundation
 
 /* Step 1: 활동을 만들어 진행해보자! */
 var bodyCondition = BodyCondition(upperBodyStrength: 100, lowerBodyStrength: 80, muscularEndurance: 80, fatigue: 0)
-doExercise(bodyCondition, activities: [sitUp, dynamicRest])
+doExercise(bodyCondition, activities: [sitUp])
