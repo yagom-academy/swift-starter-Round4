459fc1c556dcca91b564750e5ba5c8a3d90d0798
mint3382 <mskim0926@naver.com>
2023-03-13 13:37:10 +0900

    클로저 내부 내용 구현

--- CodeStarterCamp_Week4/body.swift
+++ CodeStarterCamp_Week4/body.swift
@@ -23,11 +23,13 @@ class BodyCondition {
     func conditionCheck() {
         print(
         """
+        --------------
         현재의 컨디션은 다음과 같습니다.
         상체근력: \(upperBodyStrength)
         하체근력: \(lowerBodyStrength)
         근지구력: \(muscularEndurance)
         피로도: \(fatigability)
+        --------------
         """
         )
     }
@@ -38,17 +40,67 @@ struct Activity {
     let action: (BodyCondition) -> Void
 }
 
-let 윗몸일으키기: Activity = Activity(name: "윗몸일으키기", action: { bodyCondition in
-    self.upperBodyStrength += random(in: (10...20))
-    self.fatigability += random(in: 10...20)
+
+let 윗몸일으키기: Activity = Activity(name: "윗몸일으키기", action: { BodyCondition -> Void in
+    let upperBodyCount = Int.random(in: 10...20)
+    let fatigabilityCount = Int.random(in: 10...20)
+    BodyCondition.upperBodyStrength += upperBodyCount
+    BodyCondition.fatigability += fatigabilityCount
     print(
     """
-    <<\(Activity.name)을(를) 시작합니다.>>
-    상체근력이 상승합니다.
-    피로도가 상승합니다.
+    <<\(윗몸일으키기.name)을(를) 시작합니다.>>
+    상체근력이 \(upperBodyCount)만큼 상승합니다.
+    피로도가 \(fatigabilityCount)만큼 상승합니다.
     """
     )
+    BodyCondition.conditionCheck()
 })
 
+let 스쿼트: Activity = Activity(name: "스쿼트", action: { BodyCondition -> Void in
+    let lowerBodyCount = Int.random(in: 20...30)
+    let fatigabilityCount = Int.random(in: 10...20)
+    BodyCondition.lowerBodyStrength += lowerBodyCount
+    BodyCondition.fatigability += fatigabilityCount
+    print(
+    """
+    <<\(스쿼트.name)을(를) 시작합니다.>>
+    하체근력이 \(lowerBodyCount)만큼 상승합니다.
+    피로도가 \(fatigabilityCount)만큼 상승합니다.
+    """
+    )
+    BodyCondition.conditionCheck()
+})
 
+let 오래달리기: Activity = Activity(name: "오래달리기", action: { BodyCondition -> Void in
+    let upperBodyCount = Int.random(in: 5...10)
+    let lowerBodyCount = Int.random(in: 5...10)
+    let muscularEnduranceCount = Int.random(in: 20...30)
+    let fatigabilityCount = Int.random(in: 20...30)
+    BodyCondition.upperBodyStrength += upperBodyCount
+    BodyCondition.lowerBodyStrength += lowerBodyCount
+    BodyCondition.muscularEndurance += muscularEnduranceCount
+    BodyCondition.fatigability += fatigabilityCount
+    print(
+    """
+    <<\(오래달리기.name)을(를) 시작합니다.>>
+    상체근력이 \(upperBodyCount)만큼 상승합니다.
+    하체근력이 \(lowerBodyCount)만큼 상승합니다.
+    근지구력이 \(muscularEnduranceCount)만큼 상승합니다.
+    피로도가 \(fatigabilityCount)만큼 상승합니다.
+    """
+    )
+    BodyCondition.conditionCheck()
+})
+
+let 동적휴식: Activity = Activity(name: "동적휴식", action: { BodyCondition -> Void in
+    let fatigabilityCount = Int.random(in: 5...10)
+    BodyCondition.fatigability -= fatigabilityCount
+    print(
+    """
+    <<\(동적휴식.name)을(를) 시작합니다.>>
+    피로도가 \(fatigabilityCount)만큼 줄어듭니다.
+    """
+    )
+    BodyCondition.conditionCheck()
+})
 
--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,5 +8,9 @@
 
 import Foundation
 
-var mint = BodyCondition(upperBodyStrength: 100, lowerBodyStrength: 100, muscularEndurance: 150, fatigability: 0)
+var mint2 = BodyCondition(upperBodyStrength: 100, lowerBodyStrength: 100, muscularEndurance: 150, fatigability: 0)
 
+윗몸일으키기.action(mint2)
+스쿼트.action(mint2)
+오래달리기.action(mint2)
+동적휴식.action(mint2)
