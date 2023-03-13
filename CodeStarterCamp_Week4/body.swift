eb632f19d2f6bef215d0d2589352b9eb58b73eb9
mint3382 <mskim0926@naver.com>
2023-03-13 22:17:12 +0900

    didSet 사용해서 반복 줄여줌

--- CodeStarterCamp_Week4/body.swift
+++ CodeStarterCamp_Week4/body.swift
@@ -8,10 +8,30 @@
 import Foundation
 
 class BodyCondition {
-    var upperBodyStrength: Int
-    var lowerBodyStrength: Int
-    var muscularEndurance: Int
-    var fatigability: Int
+    var upperBodyStrength: Int {
+        didSet {
+            print("상체근력이 \(upperBodyStrength - oldValue)만큼 상승합니다.")
+        }
+    }
+    var lowerBodyStrength: Int {
+        didSet {
+            print("하체근력이 \(lowerBodyStrength - oldValue)만큼 상승합니다.")
+        }
+    }
+    var muscularEndurance: Int {
+        didSet {
+            print("근지구력이 \(muscularEndurance - oldValue)만큼 상승합니다.")
+        }
+    }
+    var fatigability: Int {
+        didSet {
+            if fatigability > oldValue {
+                print("피로도가 \(fatigability - oldValue)만큼 상승합니다.")
+            } else {
+                print("피로도가 \(oldValue - fatigability)만큼 줄어듭니다.")
+            }
+        }
+    }
     
     init(upperBodyStrength: Int, lowerBodyStrength: Int, muscularEndurance: Int, fatigability: Int) {
         self.upperBodyStrength = upperBodyStrength
@@ -40,71 +60,32 @@ struct Activity {
     let action: (BodyCondition) -> Void
 }
 
-
 let 윗몸일으키기: Activity = Activity(name: "윗몸일으키기", action: { bodycondition in
-    let upperBodyCount = Int.random(in: 10...20)
-    let fatigabilityCount = Int.random(in: 10...20)
-    
-    bodycondition.upperBodyStrength += upperBodyCount
-    bodycondition.fatigability += fatigabilityCount
-    print(
-    """
-    <<\(윗몸일으키기.name)을(를) 시작합니다.>>
-    상체근력이 \(upperBodyCount)만큼 상승합니다.
-    피로도가 \(fatigabilityCount)만큼 상승합니다.
-    """
-    )
+    print("<<\(윗몸일으키기.name)을(를) 시작합니다.>>")
+    bodycondition.upperBodyStrength += Int.random(in: 10...20)
+    bodycondition.fatigability += Int.random(in: 10...20)
     bodycondition.conditionCheck()
 })
 
 let 스쿼트: Activity = Activity(name: "스쿼트", action: { bodyCondition in
-    let lowerBodyCount = Int.random(in: 20...30)
-    let fatigabilityCount = Int.random(in: 10...20)
-    
-    bodyCondition.lowerBodyStrength += lowerBodyCount
-    bodyCondition.fatigability += fatigabilityCount
-    print(
-    """
-    <<\(스쿼트.name)을(를) 시작합니다.>>
-    하체근력이 \(lowerBodyCount)만큼 상승합니다.
-    피로도가 \(fatigabilityCount)만큼 상승합니다.
-    """
-    )
+    print("<<\(스쿼트.name)을(를) 시작합니다.>>")
+    bodyCondition.lowerBodyStrength += Int.random(in: 20...30)
+    bodyCondition.fatigability += Int.random(in: 10...20)
     bodyCondition.conditionCheck()
 })
 
 let 오래달리기: Activity = Activity(name: "오래달리기", action: { bodyCondition in
-    let upperBodyCount = Int.random(in: 5...10)
-    let lowerBodyCount = Int.random(in: 5...10)
-    let muscularEnduranceCount = Int.random(in: 20...30)
-    let fatigabilityCount = Int.random(in: 20...30)
-    
-    bodyCondition.upperBodyStrength += upperBodyCount
-    bodyCondition.lowerBodyStrength += lowerBodyCount
-    bodyCondition.muscularEndurance += muscularEnduranceCount
-    bodyCondition.fatigability += fatigabilityCount
-    print(
-    """
-    <<\(오래달리기.name)을(를) 시작합니다.>>
-    상체근력이 \(upperBodyCount)만큼 상승합니다.
-    하체근력이 \(lowerBodyCount)만큼 상승합니다.
-    근지구력이 \(muscularEnduranceCount)만큼 상승합니다.
-    피로도가 \(fatigabilityCount)만큼 상승합니다.
-    """
-    )
+    print("<<\(오래달리기.name)을(를) 시작합니다.>>")
+    bodyCondition.upperBodyStrength += Int.random(in: 5...10)
+    bodyCondition.lowerBodyStrength += Int.random(in: 5...10)
+    bodyCondition.muscularEndurance += Int.random(in: 20...30)
+    bodyCondition.fatigability += Int.random(in: 20...30)
     bodyCondition.conditionCheck()
 })
 
 let 동적휴식: Activity = Activity(name: "동적휴식", action: { bodyCondition in
-    let fatigabilityCount = Int.random(in: 5...10)
-    
-    bodyCondition.fatigability -= fatigabilityCount
-    print(
-    """
-    <<\(동적휴식.name)을(를) 시작합니다.>>
-    피로도가 \(fatigabilityCount)만큼 줄어듭니다.
-    """
-    )
+    print("<<\(동적휴식.name)을(를) 시작합니다.>>")
+    bodyCondition.fatigability -= Int.random(in: 5...10)
     bodyCondition.conditionCheck()
 })
 
