000dfb0e56116f876109edf1f2790b5a856c2dc5
MaryJo-github <jotjdal0215@gmail.com>
2023-03-14 20:58:00 +0900

    chore: 연습코드 호출부 제거

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,11 +8,6 @@
 
 import Foundation
 
-// MARK: - practice
-//closure()
-//higherOrderFunction()
-
-// MARK: - step1
 let maryCondition = BodyCondition(upperBody: 0, lowerBody: 0, endurance: 0, fatigue: 0)
 
 sitUp.action(maryCondition)
