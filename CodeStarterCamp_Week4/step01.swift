1411b01913dc271a0077b518a58016435c0d7a42
karen <karenrodanthe@icloud.com>
2023-03-16 16:19:28 +0900

    230316 week4-STEP1 미션 피드백 후 수정, 개행 정리 및 오래달리기 상태변화 추가

--- CodeStarterCamp_Week4/step01.swift
+++ CodeStarterCamp_Week4/step01.swift
@@ -10,7 +10,6 @@ import Foundation
 struct Activity {
     let name: String
     let action: (BodyCondition) -> Void
-    
 }
 
 class BodyCondition {
@@ -36,7 +35,7 @@ class BodyCondition {
     
 }
 
-let sitUp:Activity = Activity(name: "윗몸일으키기", action: {bodyCondition in
+let sitUp: Activity = Activity(name: "윗몸일으키기", action: { bodyCondition in
     print("<<윗몸일으키기을(를) 시작합니다.>>")
     bodyCondition.upperBodyStrength += Int.random(in:10...20)
     bodyCondition.degreeOfFatigue += Int.random(in:10...20)
@@ -45,7 +44,7 @@ let sitUp:Activity = Activity(name: "윗몸일으키기", action: {bodyCondition
     bodyCondition.printCondition()
 })
 
-let squats:Activity = Activity(name: "스쿼트", action: {bodyCondition in
+let squats: Activity = Activity(name: "스쿼트", action: { bodyCondition in
     print("<<스쿼트을(를) 시작합니다.>>")
     bodyCondition.lowerBodyMuscleStrength += Int.random(in:20...30)
     bodyCondition.degreeOfFatigue += Int.random(in:10...20)
@@ -54,15 +53,19 @@ let squats:Activity = Activity(name: "스쿼트", action: {bodyCondition in
     bodyCondition.printCondition()
 })
 
-let longRun:Activity = Activity(name: "오래달리기", action: {bodyCondition in
+let longRun: Activity = Activity(name: "오래달리기", action: { bodyCondition in
     print("<<오래달리기을(를) 시작합니다.>>")
     bodyCondition.muscularEndurance += Int.random(in:20...30)
     bodyCondition.upperBodyStrength += Int.random(in:5...10)
     bodyCondition.lowerBodyMuscleStrength += Int.random(in:5...10)
     bodyCondition.degreeOfFatigue += Int.random(in:10...20)
+    print("근지구력이 \(bodyCondition.muscularEndurance) 상승합니다.")
+    print("상체근력이 \(bodyCondition.upperBodyStrength) 상승합니다.")
+    print("하체근력이 \(bodyCondition.lowerBodyMuscleStrength) 상승합니다.")
+    print("피로도가 \(bodyCondition.degreeOfFatigue) 상승합니다.")
 })
 
-let dynamicResting:Activity = Activity(name: "동적휴식", action: {bodyCondition in
+let dynamicResting: Activity = Activity(name: "동적휴식", action: { bodyCondition in
     print("<<동적휴식을(를) 시작합니다.>>")
     bodyCondition.degreeOfFatigue -= Int.random(in:5...10)
     print("피로도가 \(bodyCondition.degreeOfFatigue) 하락합니다.")
