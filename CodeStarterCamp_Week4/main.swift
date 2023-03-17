a41db369fb706cb953e2d8e1320bab7fafe3205b
bubblecocoa <bubblecocoa@gmail.com>
2023-03-17 09:28:47 +0900

    Style: 후행클로저 사용

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,32 +8,32 @@
 
 import Foundation
 
-let sitUp: Activity = Activity(name: "윗몸일으키기", action: {
+let sitUp: Activity = Activity(name: "윗몸일으키기") {
     $0.upperBodyStrength += Int.random(in: 10...20)
     $0.fatigue += Int.random(in: 10...20)
-})
+}
 
-let squat: Activity = Activity(name: "스쿼트", action: {
+let squat: Activity = Activity(name: "스쿼트") {
     $0.lowerBodyStrength += Int.random(in: 20...30)
     $0.fatigue += Int.random(in: 10...20)
-})
+}
 
-let longRun: Activity = Activity(name: "오래달리기", action: {
+let longRun: Activity = Activity(name: "오래달리기") {
     $0.muscularEndurance += Int.random(in: 20...30)
     $0.upperBodyStrength += Int.random(in: 5...10)
     $0.lowerBodyStrength += Int.random(in: 5...10)
     $0.fatigue += Int.random(in: 20...30)
-})
+}
 
-let dynamicRest: Activity = Activity(name: "동적휴식", action: {
+let dynamicRest: Activity = Activity(name: "동적휴식") {
     $0.fatigue -= Int.random(in: 5...10)
-})
+}
 
-let hangBoard: Activity = Activity(name: "행보드", action: {
+let hangBoard: Activity = Activity(name: "행보드") {
     $0.upperBodyStrength += Int.random(in: 10...20)
     $0.muscularEndurance += Int.random(in: 10...20)
     $0.fatigue += Int.random(in: 10...20)
-})
+}
 
 var bmoCondition = BodyCondition(
     upperBodyStrength: 20,
