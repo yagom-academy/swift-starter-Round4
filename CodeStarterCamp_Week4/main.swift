e9cd6bc21dc43e8656856b53d860ae992b3b4bb6
bubblecocoa <bubblecocoa@gmail.com>
2023-03-21 02:21:18 +0900

    Style: Activity 인스턴스 코드 이동

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,40 +8,12 @@
 
 import Foundation
 
-let sitUp: Activity = Activity(name: "윗몸일으키기") {
-    $0.upperBodyStrength += Int.random(in: 10...20)
-    $0.fatigue += Int.random(in: 10...20)
-}
-
-let squat: Activity = Activity(name: "스쿼트") {
-    $0.lowerBodyStrength += Int.random(in: 20...30)
-    $0.fatigue += Int.random(in: 10...20)
-}
-
-let longRun: Activity = Activity(name: "오래달리기") {
-    $0.muscularEndurance += Int.random(in: 20...30)
-    $0.upperBodyStrength += Int.random(in: 5...10)
-    $0.lowerBodyStrength += Int.random(in: 5...10)
-    $0.fatigue += Int.random(in: 20...30)
-}
-
-let dynamicRest: Activity = Activity(name: "동적휴식") {
-    $0.fatigue -= Int.random(in: 5...10)
-}
-
-let hangBoard: Activity = Activity(name: "행보드") {
-    $0.upperBodyStrength += Int.random(in: 10...20)
-    $0.muscularEndurance += Int.random(in: 10...20)
-    $0.fatigue += Int.random(in: 10...20)
-}
-
 var bmoCondition = BodyCondition(
     upperBodyStrength: 20,
     lowerBodyStrength: 10,
     muscularEndurance: 30,
     fatigue: 15
 )
-
 let activities: [Activity] = [sitUp, dynamicRest, hangBoard, dynamicRest]
 let routine = Routine(name: "비모루틴", activities: activities)
 let manager = RoutineManager()
