948e0a18d14ca213c2b212724ce50216e27f5f06
yagom <jin@seohyejin-ui-MacBookAir.local>
2023-03-21 06:54:28 +0900

    Week4 [STEP2] Whales

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -20,12 +20,14 @@ struct Routine {
     
     func inputRoutineCount() throws -> Int {
         print("루틴을 몇 번 반복할까요? :", terminator: " ")
-        let count: Int? = Int(readLine() ?? "nil")
-        let count2: Int = count ?? 0
-        guard count2 > 0 else {
+        
+        guard let countString: String = readLine(), let count: Int = Int(countString) else {
             throw ProgramError.notNumber
         }
-        return count2
+        guard count > 0 else {
+            throw ProgramError.notNumber
+        }
+        return count
     }
 
     func executeRoutine(count: Int, for bodyCondition: BodyCondition) throws {
@@ -35,28 +37,29 @@ struct Routine {
             activities[0].action(bodyCondition)
             activities[3].action(bodyCondition)
             activities[1].action(bodyCondition)
+            
             guard bodyCondition.degreeOfFatigue < 100 else {
-                
                 throw ProgramError.overActivity
             }
             round += 1
         }
         bodyCondition.checkBodyCondition()
     }
+    
+    func startRoutine(_ routine: Routine, by bodyCondition: BodyCondition) {
+        do {
+            try routine.executeRoutine(count: routine.inputRoutineCount(), for: bodyCondition)
+        } catch ProgramError.notNumber {
+            print("잘못된 입력 형식입니다. 다시 입력해주세요.")
+        } catch ProgramError.overActivity {
+            print("피로도가 100 이상입니다. 루틴을 중단합니다.")
+            bodyCondition.checkBodyCondition()
+        } catch {
+            print(error)
+        }
+    }
 }
 
-let bodyCondition: BodyCondition = BodyCondition(upper: 10, lower: 15, muscular: 20, fatigue: 0)
+let whalesBodyCondition: BodyCondition = BodyCondition(upper: 10, lower: 15, muscular: 20, fatigue: 0)
 let newRoutine: Routine = Routine(name: "hellRoutine")
-
-do {
-    try newRoutine.executeRoutine(count: newRoutine.inputRoutineCount(), for: bodyCondition)
-} catch ProgramError.notNumber {
-    print("잘못된 입력 형식입니다. 다시 입력해주세요.")
-} catch ProgramError.overActivity { // 여기서 잡아보자
-    print("피로도가 100 이상입니다. 루틴을 중단합니다.")
-    bodyCondition.checkBodyCondition()
-} catch {
-    print(error)
-}
-
-
+newRoutine.startRoutine(newRoutine, by: whalesBodyCondition)
