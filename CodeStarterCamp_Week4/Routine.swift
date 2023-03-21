bb5c1846427a1ab2fd6944cf86359ec6f292726e
Charles <kch1759@naver.com>
2023-03-21 14:04:49 +0900

    Step2 수정

--- CodeStarterCamp_Week4/Routine.swift
+++ CodeStarterCamp_Week4/Routine.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 struct Routine {
-    var routineName: String
-    var activities: [Activity]
+    let routineName: String
+    let activities: [Activity]
     
     
     func writeNumberOfRoutine() throws -> Int {
@@ -18,7 +18,19 @@ struct Routine {
             throw RoutineInputError.unexpectedInputValue
         }
         
+        guard routineTimes >= 0 else {
+            throw RoutineInputError.unexpectedInputValue
+        }
+        
         return routineTimes
+    }
+    
+    
+    private func doRoutine(with bodyCondition: BodyCondition) throws {
+        for activity in activities {
+            activity.doExercise(with: bodyCondition)
+            try bodyCondition.checkFatigability()
+        }
     }
     
     
@@ -31,24 +43,18 @@ struct Routine {
             
             for time in 1 ... times {
                 print("\(time) 번째 \(self.routineName)을(를) 시작합니다.")
-                for activity in activities {
-                    activity.doExercise(with: bodyCondition)
-                    do {
-                        try bodyCondition.checkFatigability()
-                    } catch RoutineInputError.overflowFatigue {
-                        print("피로도가 100 이상입니다. 루틴을 중단합니다")
-                        break
-                    }
-                }
+                try doRoutine(with: bodyCondition)
             }
             bodyCondition.checkCondition()
         } catch RoutineInputError.unexpectedInputValue {
             print("잘못된 입력 형식입니다. 다시 입력해주세요.")
         } catch RoutineInputError.otherException {
             print("예기치 않은 오류")
+        } catch RoutineInputError.overflowFatigue {
+            print("피로도가 100이상입니다. 루틴을 중단합니다.")
+            bodyCondition.checkCondition()
         } catch {
             print(error)
         }
-
     }
 }
