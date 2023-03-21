ef2d088f6c995e36ce8829d537953b8d6da6dd3d
yagom <jin@seohyejin-ui-MacBookAir.local>
2023-03-21 19:06:52 +0900

    Week4 [STEP2] Whales

--- CodeStarterCamp_Week4/Activity.swift
+++ CodeStarterCamp_Week4/Activity.swift
@@ -14,23 +14,23 @@ struct Activity {
 
 let sitUp: Activity = Activity(name: "윗몸일으키기", action: { bodyCondition in
     print("<<\(sitUp.name)을(를) 시작합니다>>")
-    bodyCondition.upperBodyMuscleStrength += 13
-    bodyCondition.degreeOfFatigue += 18
+    bodyCondition.upperBodyMuscleStrength += Int.random(in: 20...30)
+    bodyCondition.degreeOfFatigue += Int.random(in: 10...20)
 })
 let squat: Activity = Activity(name: "스쿼트", action: { bodyCondition in
     print("<<\(squat.name)을(를) 시작합니다>>")
-    bodyCondition.lowerBodyMuscleStrength += 20
-    bodyCondition.degreeOfFatigue += 25
+    bodyCondition.lowerBodyMuscleStrength += Int.random(in: 20...30)
+    bodyCondition.degreeOfFatigue += Int.random(in: 10...20)
 })
 let marathon: Activity = Activity(name: "오래달리기", action: { bodyCondition in
     print("<<\(marathon.name)을(를) 시작합니다>>")
-    bodyCondition.upperBodyMuscleStrength += 7
-    bodyCondition.lowerBodyMuscleStrength += 9
-    bodyCondition.muscularEndurance += 23
-    bodyCondition.degreeOfFatigue += 30
+    bodyCondition.upperBodyMuscleStrength += Int.random(in: 5...10)
+    bodyCondition.lowerBodyMuscleStrength += Int.random(in: 5...10)
+    bodyCondition.muscularEndurance += Int.random(in: 20...30)
+    bodyCondition.degreeOfFatigue += Int.random(in: 20...30)
 })
 let dynamicResting: Activity = Activity(name: "동적휴식", action: { bodyCondition in
     print("<<\(dynamicResting.name)을(를) 시작합니다>>")
-    bodyCondition.degreeOfFatigue -= 7
+    bodyCondition.degreeOfFatigue -= Int.random(in: 5...10)
 })
 
--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum ProgramError: Error {
+enum ActivityRoutineProgramError: Error {
     case notNumber
     case overActivity
 }
@@ -22,16 +22,17 @@ struct Routine {
         print("루틴을 몇 번 반복할까요? :", terminator: " ")
         
         guard let countString: String = readLine(), let count: Int = Int(countString) else {
-            throw ProgramError.notNumber
+            throw ActivityRoutineProgramError.notNumber
         }
         guard count > 0 else {
-            throw ProgramError.notNumber
+            throw ActivityRoutineProgramError.notNumber
         }
         return count
     }
 
     func executeRoutine(count: Int, for bodyCondition: BodyCondition) throws {
         var round: Int = 1
+        
         while round <= count {
             print("\(ordinalNumber[round-1]) 번째 \(self.name)을(를) 시작합니다.")
             activities[0].action(bodyCondition)
@@ -39,19 +40,24 @@ struct Routine {
             activities[1].action(bodyCondition)
             
             guard bodyCondition.degreeOfFatigue < 100 else {
-                throw ProgramError.overActivity
+                throw ActivityRoutineProgramError.overActivity
             }
             round += 1
         }
         bodyCondition.checkBodyCondition()
     }
+}
+
+struct Person {
+    var name: String
+    let bodyCondition: BodyCondition
     
     func startRoutine(_ routine: Routine, by bodyCondition: BodyCondition) {
         do {
             try routine.executeRoutine(count: routine.inputRoutineCount(), for: bodyCondition)
-        } catch ProgramError.notNumber {
+        } catch ActivityRoutineProgramError.notNumber {
             print("잘못된 입력 형식입니다. 다시 입력해주세요.")
-        } catch ProgramError.overActivity {
+        } catch ActivityRoutineProgramError.overActivity {
             print("피로도가 100 이상입니다. 루틴을 중단합니다.")
             bodyCondition.checkBodyCondition()
         } catch {
@@ -60,6 +66,7 @@ struct Routine {
     }
 }
 
-let whalesBodyCondition: BodyCondition = BodyCondition(upper: 10, lower: 15, muscular: 20, fatigue: 0)
+let whales: Person = Person(name: "Whales", bodyCondition: BodyCondition(upper: 10, lower: 15, muscular: 20, fatigue: 15))
 let newRoutine: Routine = Routine(name: "hellRoutine")
-newRoutine.startRoutine(newRoutine, by: whalesBodyCondition)
+
+whales.startRoutine(newRoutine, by: whales.bodyCondition)
