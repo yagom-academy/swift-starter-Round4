b0dd7db6e0e8c126568774e878e27ecf49b8fbc9
Hayeon Kim <qoocrab@gmail.com>
2023-03-23 15:00:30 +0900

    Feat: 4주차 미션 - Step 1 - 피드백 반영

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -7,8 +7,4 @@ import Foundation
 
 let person: BodyCondition = BodyCondition(upperBodyStrengthValue: 0, lowerBodyStrengthValue: 0, muscularEnduranceValue: 0, fatigueLevelValue: 0)
 
-performSitUps.perform()
-performSitUps.action(person)
-print("----------------")
-person.showBodyCondition()
-print("----------------")
+performSitUps.start(bodyCondition: person)
