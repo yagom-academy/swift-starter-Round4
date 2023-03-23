aa24793341be394b93680dc2b980c13857e830f7
deanidh <deanidh1011@gmail.com>
2023-03-23 20:24:33 +0900

    feat: activity의 action에 간략화
    Activity 타입에 start 함수 추가 및 간략화

--- CodeStarterCamp_Week4/Activity.swift
+++ CodeStarterCamp_Week4/Activity.swift
@@ -8,6 +8,12 @@
 import Foundation
 
 struct Activity {
-        let name: String
-        let action: (BodyCondition) -> Void
+    let name: String
+    let action: (BodyCondition) -> Void
+    
+    func start(bodyCondition: BodyCondition) {
+        print("<<\(name)을(를) 시작합니다>>")
+        action(bodyCondition)
+        bodyCondition.checkBodyCondition()
+    }
 }
--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -9,35 +9,26 @@
 import Foundation
 
 let sitUp: Activity = Activity(name: "윗몸일으키기", action: { bodyCondition in
-    print("<<윗몸일으키기를 시작합니다>>")
     bodyCondition.upperBodyStrength += Int.random(in: 10...20)
     bodyCondition.fatigue += Int.random(in: 10...20)
-    bodyCondition.checkBodyCondition()
 })
 
 let squat: Activity = Activity(name: "스쿼트", action: { bodyCondition in
-    print("<<스쿼트를 시작합니다>>")
     bodyCondition.lowerBodyStrength += Int.random(in: 20...30)
     bodyCondition.fatigue += Int.random(in: 10...20)
-    bodyCondition.checkBodyCondition()
 })
 
 let running: Activity = Activity(name: "오래달리기", action: { bodyCondition in
-    print("<<오래달리기를 시작합니다>>")
     bodyCondition.muscleEndurance += Int.random(in: 20...30)
     bodyCondition.upperBodyStrength += Int.random(in: 5...10)
     bodyCondition.lowerBodyStrength += Int.random(in: 5...10)
     bodyCondition.fatigue += Int.random(in: 20...30)
-    bodyCondition.checkBodyCondition()
 })
 
 let rest: Activity = Activity(name: "동적휴식", action: { bodyCondition in
-    print("<<동적휴식을 시작합니다>>")
     bodyCondition.fatigue -= Int.random(in: 5...10)
-    bodyCondition.checkBodyCondition()
 })
 
 var dean: BodyCondition = BodyCondition(upperBodyStrength: 20, lowerBodyStrength: 20, muscleEndurance: 30, fatigue: 10)
 
-squat.action(dean)
-running.action(dean)
+squat.start(bodyCondition: dean)
