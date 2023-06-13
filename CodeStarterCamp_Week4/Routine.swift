0e2cc28cbf6a0f8c60f3354eacae04efc3515511
JIWON1923 <zest1923@gmail.com>
2023-06-13 22:21:00 +0900

    함수 위치 수정

--- CodeStarterCamp_Week4/Routine.swift
+++ CodeStarterCamp_Week4/Routine.swift
@@ -17,7 +17,7 @@ struct Routine {
     var activities: [Activity]
 
 
-    func inputCycle() throws -> Int {
+    private func inputCycle() throws -> Int {
         print("루틴을 몇 번 반복할까요?")
         let input = readLine()
 
@@ -31,12 +31,28 @@ struct Routine {
         return cycle
     }
 
-    func executeRoutine(bodyCondition: BodyCondition) throws {
+    private func executeRoutine(bodyCondition: BodyCondition) throws {
         for activity in activities {
             activity.action(bodyCondition)
             guard bodyCondition.fatigueLevel < 100 else {
                 throw RoutineError.overwork
             }
+        }
+    }
+    
+    func startRoutine() {
+        do {
+            let cycle = try inputCycle()
+            for _ in 0..<cycle {
+                try executeRoutine(bodyCondition: myBodyCondition)
+            }
+        } catch RoutineError.inputError {
+            print("잘못된 입력 형식입니다. 다시 입력해주세요.")
+            startRoutine()
+        } catch RoutineError.overwork {
+            print("피로도가 100 이상입니다. 루틴을 중단합니다.")
+        } catch {
+            print("알 수 없는 에러가 발생했습니다.")
         }
     }
 }
