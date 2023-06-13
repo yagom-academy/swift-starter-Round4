b189120373f8f9103cdb4c0a37362cc1d07e4d37
JIWON1923 <zest1923@gmail.com>
2023-06-13 22:04:24 +0900

    [Refactor] action 기본 동작 추가

--- CodeStarterCamp_Week4/Activity.swift
+++ CodeStarterCamp_Week4/Activity.swift
@@ -10,4 +10,13 @@ import Foundation
 struct Activity {
     let name: String
     let action: (BodyCondition) -> Void
+    
+    init(name: String, action: @escaping (BodyCondition) -> Void) {
+        self.name = name
+        self.action = { bodyCondition in
+            print("\(name)을(를) 시작합니다.")
+            action(bodyCondition)
+            bodyCondition.showMyBodyCondition()
+        }
+    }
 }
