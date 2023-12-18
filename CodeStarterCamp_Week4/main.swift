a48afbc31732d8dc5526e23233752b99b6b5c5f4
Nat <cestbonciel88@knou.ac.kr>
2023-12-19 00:25:56 +0900

    Add: Round4 STEP1 Nat

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+// MARK: - Round4 [STEP1] 활동을 만들어 진행해보자
+
+var bodyCondition = BodyCondition(upperBodyStrength: 0, lowerBodyStrength: 0, muscularEndurance: 0, fatigue: 0)
+
+let sitUp: Activity = Activity(name: .sitUp) { bodyCondition in
+    bodyCondition.performActivity(.sitUp)
+}
+
+
+sitUp.action(bodyCondition)
+bodyCondition.printCurrentCondition()
+
+
+let squat: Activity = Activity(name: .squat) { bodyCondition in
+    bodyCondition.performActivity(.squat)
+}
+
+squat.action(bodyCondition)
+bodyCondition.printCurrentCondition()
+
+let marathon: Activity = Activity(name: .longDistanceRunning) { bodyCondition in
+    bodyCondition.performActivity(.longDistanceRunning)
+}
+
+marathon.action(bodyCondition)
+bodyCondition.printCurrentCondition()
+
+let coolingDown: Activity = Activity(name: .dynamicRest) { bodyCondition in
+    bodyCondition.performActivity(.dynamicRest)
+}
+
+coolingDown.action(bodyCondition)
+bodyCondition.printCurrentCondition()
+
+let doingYoga: Activity = Activity(name: .yoga) { bodyCondition in
+    bodyCondition.performActivity(.yoga)
+}
+
+doingYoga.action(bodyCondition)
+bodyCondition.printCurrentCondition()
